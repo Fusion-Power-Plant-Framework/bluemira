a3d86255893bea38444dc42c523f1c0007d4d603
Dan Short <dan.short@ukaea.uk>
2021-07-29 17:35:13 +0100

    First pass at a reactor
    Uses BLUEPRINT geometries to generate relevant faces and plots them.
    A few updates needed in separate PRs:
    - Handle mixed faces that aren't just in x-z plane
    - Add plotting for Components
    - Disable typechecking

--- bluemira/utilities/plot_tools.py
+++ bluemira/utilities/plot_tools.py
@@ -31,6 +31,8 @@ from matplotlib.patches import Patch, PathPatch
 from mpl_toolkits.mplot3d.art3d import PathPatch3D
 from mpl_toolkits.mplot3d import Axes3D
 import imageio
+
+from bluemira.base.components import Component, GroupingComponent
 from bluemira.base.constants import GREEK_ALPHABET, GREEK_ALPHABET_CAPS
 from bluemira.base.file import get_bluemira_path
 
@@ -220,3 +222,26 @@ class BluemiraPathPatch3D(PathPatch3D):
         Transfer the key getattr to underlying PathPatch object.
         """
         return getattr(self._patch2d, key)
+
+
+def plot_component(component: Component, axis=None):
+    """
+    Plot the Component on the provided axis.
+
+    If the Component is a GroupingComponent then all child components with shapes will be
+    plotted.
+    """
+    from bluemira.geometry.plotting import plot_face
+
+    if axis is None:
+        axis = Plot3D()
+
+    if isinstance(component, GroupingComponent):
+        for child in component.children:
+            plot_component(child, axis=axis)
+        return
+
+    if hasattr(component, "shape"):
+        plot_face(component.shape, axis=axis)
+    else:
+        raise ValueError(f"Could not plot component {component}")
