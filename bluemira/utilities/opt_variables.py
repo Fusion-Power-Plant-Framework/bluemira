552f4dd7cae8427064571a33f1cd1e1b4a4ff7e8
Matti <matti.coleman@ukaea.uk>
2021-09-10 16:15:30 +0200

    initial pass

---
+++ bluemira/utilities/opt_variables.py
@@ -0,0 +1,329 @@
+# bluemira is an integrated inter-disciplinary design tool for future fusion
+# reactors. It incorporates several modules, some of which rely on other
+# codes, to carry out a range of typical conceptual fusion reactor design
+# activities.
+#
+# Copyright (C) 2021 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh, J. Morris,
+#                    D. Short
+#
+# bluemira is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# bluemira is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
+
+"""
+Optimisation variable class.
+"""
+from dataclasses import dataclass, field, InitVar
+from typing import Optional
+import numpy as np
+import matplotlib.pyplot as plt
+
+from bluemira.base.error import BluemiraError
+
+# TODO: This should live in opt_tools.py, but there would be a clash incoming so I leave it here for now
+# TODO: Error class also probably exists in opt_tools or elsewhere
+
+
+class OptUtilitiesError(BluemiraError):
+    """
+    Error for optimisation utilities
+    """
+
+    pass
+
+
+def normalise_value(value, lower_bound, upper_bound):
+    """
+    Normalise a value [0 -> 1].
+
+    Parameters
+    ----------
+    value: float
+        Value to normalise
+    lower_bound: float
+        Lower bound at which to normalise
+    upper_bound: float
+        Upper bound at which to normalise
+
+    Returns
+    -------
+    v_norm: float
+        Normalised value [0 -> 1]
+    """
+    return (value - lower_bound) / (upper_bound - lower_bound)
+
+
+def denormalise_value(v_norm, lower_bound, upper_bound):
+    """
+    Denormalise a value from [0 -> 1] w.r.t bounds.
+
+    Parameters
+    ----------
+    v_norm: float
+        Normalised value
+    lower_bound: float
+        Lower bound at which to denormalise
+    upper_bound: float
+        Upper bound at which to denormalise
+
+    Returns
+    -------
+    value: float
+        Denormalised value w.r.t. bounds
+    """
+    return lower_bound + v_norm * (upper_bound - lower_bound)
+
+
+@dataclass
+class BoundedVariable:
+    """
+    A bounded variable.
+
+    Parameters
+    ----------
+    name: str
+        Name of the variable
+    value: float
+        Value of the variable
+    lower_bound: float
+        Lower bound of the variable
+    upper_bound: float
+        Upper bound of the variable
+    fixed: bool
+        Whether or not the variable is to be held constant
+    """
+
+    _value: float = field(init=False, repr=False)
+    name: str
+    value: float
+    lower_bound: float = 0
+    upper_bound: float = 0
+    fixed: bool = False
+    _initialised: bool = field(init=False, repr=False, default=False)
+
+    def __post_init__(self):
+        if self.value < self.lower_bound:
+            raise OptUtilitiesError(
+                "Cannot initialise variable: its value is out of its bounds."
+            )
+        if self.value > self.upper_bound:
+            raise OptUtilitiesError(
+                "Cannot initialise variable: its value is out of its bounds."
+            )
+
+        if self.lower_bound > self.upper_bound:
+            raise OptUtilitiesError("Lower bound is higher than upper bound.")
+
+        self._initialised = True
+
+    def fix(self, value: float):
+        """
+        Fix the variable at a specified value. Ignores bounds.
+
+        Parameters
+        ----------
+        value: float
+            Value at which to fix the variable.
+        """
+        self.fixed = True
+        if value is not None:
+            self._value = value
+
+    @property
+    def value(self) -> float:
+        """
+        The value of the variable.
+        """
+        return self._value
+
+    @value.setter
+    def value(self, value: float):
+        """
+        Set the value of the variable, enforcing bounds.
+        """
+        if self._initialised:
+            if value < self.lower_bound:
+                value = self.lower_bound
+
+            elif value > self.upper_bound:
+                value = self.upper_bound
+
+        self._value = value
+
+    @property
+    def normalised_value(self) -> float:
+        """
+        The normalised value of the variable.
+        """
+        return normalise_value(self.value, self.lower_bound, self.upper_bound)
+
+
+class OptVariables:
+    """
+    A set of ordered variables to facilitate optimisation using normalised values.
+
+    Parameters
+    ----------
+    variables: List[BoundedVariable]
+        Set of variables to use
+    """
+
+    def __init__(self, variables):
+        self._var_dict = {v.name: v for v in variables}
+
+    def add_variable(self, variable):
+        """
+        Add a variable to the set.
+
+        Parameters
+        ----------
+        variable: BoundedVariable
+            Variable to add to the set.
+        """
+        if variable.name in self._var_dict:
+            raise OptUtilitiesError(f"Variable {variable.name} already in OptVariables.")
+
+        self._var_dict[variable.name] = variable
+
+    def remove_variable(self, name):
+        """
+        Remove a variable from the set.
+
+        Parameters
+        ----------
+        name: str
+            Name of the variable to remove
+        """
+        self._check_presence(name)
+
+        del self._var_dict[name]
+
+    def adjust_variable(self, name, value=None, lower_bound=None, upper_bound=None):
+        """
+        Adjust a variable in the set.
+
+        Parameters
+        ----------
+        name: str
+            Name of the variable to adjust
+        value: float
+            Value of the variable to set
+        lower_bound: float
+            Value of the lower bound to set
+        upper_bound: float
+            Value of the upper to set
+        """
+        self._check_presence(name)
+
+        if lower_bound:
+            self._var_dict[name].lower_bound = lower_bound
+        if upper_bound:
+            self._var_dict[name].upper_bound = upper_bound
+        if value:
+            self._var_dict[name].value = value
+
+    def fix_variable(self, name, value=None):
+        """
+        Fix a variable in the set, removing it from optimisation but preserving a
+        constant value.
+
+        Parameters
+        ----------
+        name: str
+            Name of the variable to fix
+        value: Optional[float]
+            Value at which to fix the variable (will default to present value)
+        """
+        self._check_presence(name)
+
+        self._var_dict[name].fix(value=value)
+
+    def get_normalised_values(self):
+        """
+        Get the normalised values of all free variables.
+
+        Returns
+        -------
+        x_norm: np.ndarray
+            Array of normalised values
+        """
+        return np.array(
+            [v.normalised_value for v in self._var_dict.values() if not v.fixed]
+        )
+
+    def set_values_from_norm(self, x_norm):
+        """
+        Set values from a normalised vector.
+
+        Parameters
+        ----------
+        x_norm: np.ndarray
+            Array of normalised values
+        """
+        if len(x_norm) != self.n_free_variables:
+            raise OptUtilitiesError(
+                f"Number of normalised variables {len(x_norm)} != {self.n_free_variables}."
+            )
+
+        for name, v_norm in zip(self._opt_vars, x_norm):
+            variable = self._var_dict[name]
+            value = denormalise_value(v_norm, variable.lower_bound, variable.upper_bound)
+            variable.value = value
+
+    @property
+    def values(self):
+        """
+        All un-normalised values of the variable set (including fixed variable values).
+        """
+        return np.array([v.value for v in self._var_dict.values()])
+
+    @property
+    def n_free_variables(self) -> int:
+        return len(self._opt_vars)
+
+    @property
+    def _opt_vars(self):
+        return [v.name for v in self._var_dict.values() if not v.fixed]
+
+    def _check_presence(self, name):
+        if not name in self._var_dict.keys():
+            raise OptUtilitiesError(f"Variable {name} not in OptVariables.")
+
+    def plot(self):
+        """
+        Plot the OptVariables.
+        """
+        f, ax = plt.subplots()
+        left_labels = [
+            f"{v.name}: {v.lower_bound:.2f} " for v in self._var_dict.values()
+        ]
+        right_labels = [f"{v.upper_bound:.2f}" for v in self._var_dict.values()]
+        y_pos = np.arange(len(left_labels))
+
+        x_norm = [
+            v.normalised_value if not v.fixed else 0.5 for v in self._var_dict.values()
+        ]
+        colors = ["r" if v.fixed else "lightblue" for v in self._var_dict.values()]
+
+        values = [f"{v:.2f}" for v in self.values]
+        ax2 = ax.twinx()
+        ax.barh(y_pos, x_norm, color=colors)
+        ax2.barh(y_pos, x_norm, color=colors)
+        ax.text(x_norm, y_pos, values)
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(left_labels)
+        ax.invert_yaxis()
+
+        ax2.set_yticks(y_pos)
+        ax2.set_yticklabels(right_labels)
+        ax.set_xlim([-0.1, 1.1])
+        ax.set_xlabel("$x_{norm}$")
---
+++ tests/bluemira/utilities/test_opt_variables.py
@@ -0,0 +1,97 @@
+# bluemira is an integrated inter-disciplinary design tool for future fusion
+# reactors. It incorporates several modules, some of which rely on other
+# codes, to carry out a range of typical conceptual fusion reactor design
+# activities.
+#
+# Copyright (C) 2021 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh, J. Morris,
+#                    D. Short
+#
+# bluemira is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# bluemira is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
+
+import pytest
+import numpy as np
+
+from bluemira.utilities.opt_variables import (
+    BoundedVariable,
+    OptUtilitiesError,
+    OptVariables,
+)
+
+
+class TestBoundedVariable:
+    def test_initialisation(self):
+        v1 = BoundedVariable("a", 2, 0, 3)
+        assert v1.name == "a"
+        assert v1.value == 2
+        assert v1.lower_bound == 0
+        assert v1.upper_bound == 3
+
+        v2 = BoundedVariable("b", 0, -1, 1)
+        assert v2.name == "b"
+        assert v2.value == 0
+        assert v2.lower_bound == -1
+        assert v2.upper_bound == 1
+
+        with pytest.raises(OptUtilitiesError):
+            v3 = BoundedVariable("a", 2, 2.5, 3)
+
+    def test_value_setting(self):
+        v1 = BoundedVariable("a", 2, 0, 3)
+        v1.value = 4
+        assert v1.value == v1.upper_bound
+        v1.value = -1
+        assert v1.value == v1.lower_bound
+
+    def test_normalised_value(self):
+        v1 = BoundedVariable("a", 2, 0, 4)
+        assert v1.normalised_value == 0.5
+
+
+class TestOptVariables:
+    @classmethod
+    def setup_class(cls):
+        v1 = BoundedVariable("a", 2, 0, 3)
+        v2 = BoundedVariable("b", 0, -1, 1)
+        v3 = BoundedVariable("c", -1, -10, 10)
+        cls.vars = OptVariables([v1, v2, v3])
+
+    def test_init(self):
+        assert self.vars.n_free_variables == 3
+        assert len(self.vars.values) == 3
+        assert np.allclose(self.vars.values, np.array([2, 0, -1]))
+
+    def test_add(self):
+        v4 = BoundedVariable("d", 4, -4, 6, fixed=False)
+        v5 = BoundedVariable("e", 1, -1, 2, fixed=True)
+        self.vars.add_variable(v4)
+        self.vars.add_variable(v5)
+        assert self.vars.n_free_variables == 4
+        assert len(self.vars.values) == 5
+        assert np.allclose(self.vars.values, np.array([2, 0, -1, 4, 1]))
+
+    def test_remove(self):
+        self.vars.remove_variable("c")
+        assert self.vars.n_free_variables == 3
+        assert len(self.vars.values) == 4
+        assert np.allclose(self.vars.values, np.array([2, 0, 4, 1]))
+
+    def test_fix(self):
+        self.vars.fix_variable("a", value=100)
+        assert self.vars.values[0] == 100
+        assert self.vars.n_free_variables == 2
+        assert np.allclose(self.vars.values, np.array([100, 0, 4, 1]))
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
