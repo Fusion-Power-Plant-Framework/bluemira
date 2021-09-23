24c1d52bbd07ea60210b97d6fd99d7a296e43672
Dan Short <dan.short@ukaea.uk>
2021-09-23 17:43:00 +0100

    Make usage consistent with docs

--- BLUEPRINT/codes/process/run.py
+++ BLUEPRINT/codes/process/run.py
@@ -94,7 +94,7 @@ class Run:
         Default, the value specified by DEFAULT_INDAT.
     params_to_update: list
         A list of parameter names compatible with the ParameterFrame class.
-        If provided, parameters included in this list will be set up use BLUEPRINT
+        If provided, parameters included in this list will be set to use BLUEPRINT
         values. All other parameters with a PROCESS mapping will be set to use
         PROCESS values. If None, the boolean which determines which value to use will
         be left unmodified for each parameter.
@@ -121,10 +121,16 @@ class Run:
         reactor,
         run_dir=None,
         template_indat=None,
+        params_to_update=None,
     ):
         self.reactor = reactor
         self.param_list = self.reactor.params.get_parameter_list()
-        self.params_to_update = self.reactor.build_config.get("params_to_update", None)
+        if params_to_update is not None:
+            self.params_to_update = params_to_update
+        else:
+            self.params_to_update = self.reactor.build_config.get(
+                "params_to_update", None
+            )
         if run_dir is not None:
             self.run_dir = run_dir
         else:
