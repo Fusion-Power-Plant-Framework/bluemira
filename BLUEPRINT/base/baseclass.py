1145edf176bd08fd7ff0f65d655d89cc335411e7
Matti <matti.coleman@ukaea.uk>
2021-10-01 17:16:36 +0200

    eradicate

--- BLUEPRINT/base/baseclass.py
+++ BLUEPRINT/base/baseclass.py
@@ -29,7 +29,6 @@ from typing import Type, Union
 from bluemira.base.parameter import ParameterFrame
 from bluemira.base.error import BluemiraError
 
-from BLUEPRINT.base.typebase import TypeBase
 from BLUEPRINT.base.error import SystemsError
 
 
