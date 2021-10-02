163fa43039a572f282a557ffcfc0fefff1140166
Dario_V <dario86.vaccaro@gmail.com>
2021-10-02 11:22:17 +0100

    Minor change in input

--- tests/BLUEPRINT/systems/test_firstwall.py
+++ tests/BLUEPRINT/systems/test_firstwall.py
@@ -293,7 +293,12 @@ class TestFirstWallDN:
         flux_loops = self.firstwall.pick_flux_loops()
         inner_strike, outer_strike = self.firstwall.find_strike_points(flux_loops)
         tangent = self.firstwall.get_tangent_vector(outer_strike, flux_loops[0])
-        tar_out = self.firstwall.make_divertor_target(outer_strike, tangent)
+        tar_out = self.firstwall.make_divertor_target(
+            outer_strike,
+            tangent,
+            vertical_target=self.inputs["div_vertical_outer_target"],
+            outer_target=True,
+        )
         assert tar_out[0][0] > self.firstwall.points["x_point"]["x"]
         assert tar_out[0][0] < tar_out[1][0]
 
@@ -302,7 +307,10 @@ class TestFirstWallDN:
         inner_strike, outer_strike = self.firstwall.find_strike_points(flux_loops)
         tangent = self.firstwall.get_tangent_vector(inner_strike, flux_loops[1])
         tar_in = self.firstwall.make_divertor_target(
-            inner_strike, tangent, vertical_target=False, outer_target=False
+            inner_strike,
+            tangent,
+            vertical_target=self.inputs["div_vertical_inner_target"],
+            outer_target=False,
         )
         assert tar_in[0][0] < self.firstwall.points["x_point"]["x"]
         assert tar_in[0][0] > tar_in[1][0]
