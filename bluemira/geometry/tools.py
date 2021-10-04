242e5a556f9aa1b0f5e37f50823eb3c3d0af9c39
ivanmaione <maione.ivan@gmail.com>
2021-10-04 16:58:58 +0200

    reviewed documentation

--- bluemira/geometry/tools.py
+++ bluemira/geometry/tools.py
@@ -201,7 +201,7 @@ def extrude_shape(shape: BluemiraGeo, vec: tuple, label=None) -> BluemiraSolid:
 
 
 def distance_to(geo1: BluemiraGeo, geo2: BluemiraGeo):
-    """Calculate the distance between two BluemiraWires.
+    """Calculate the distance between two BluemiraGeos.
 
     Parameters
     ----------
@@ -215,8 +215,9 @@ def distance_to(geo1: BluemiraGeo, geo2: BluemiraGeo):
     output:
         a tuple of two -> (dist, vectors)
         dist is the minimum distance (float value)
-        vectors is a list of tuple of geo1's and geo2's points
-        with the found minimum distance
+        vectors is a list of tuples corresponding to the nearest points
+        between geo1 and geo2. The distance between those points
+        is the minimum distance given by dist.
     """
     shape1 = geo1._shape
     shape2 = geo2._shape
