b555fcbac74d1a1d206b042d16f9bcca5ceefba2
ivanmaione <maione.ivan@gmail.com>
2021-10-08 17:15:41 +0200

    initial commit. Still work in progress. Some changes in the architecture could be possible.

---
+++ bluemira/plotting/plotter.py
@@ -0,0 +1,214 @@
+# bluemira is an integrated inter-disciplinary design tool for future fusion
+# reactors. It incorporates several modules, some of which rely on other
+# codes, to carry out a range of typical conceptual fusion reactor design
+# activities.
+#
+# Copyright (C) 2021 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh, J. Morris,
+#                    D. Short
+#
+# bluemira is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# bluemira is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
+
+from abc import ABC, abstractmethod
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from bluemira.geometry.wire import BluemiraWire
+from bluemira.geometry.face import BluemiraFace
+
+
+DEFAULT = {}
+DEFAULT["plot_flag"] = {"poptions": True, "woptions": True, "foptions": True}
+DEFAULT["poptions"] = {"s": 30, "facecolors": "blue", "edgecolors": "black"}
+DEFAULT["woptions"] = {"color": "black", "linewidth": "2"}
+DEFAULT["foptions"] = {"color": "red"}
+
+
+class Plot3D(Axes3D):
+    """
+    Cheap and cheerful
+    """
+
+    def __init__(self):
+        fig = plt.figure(figsize=[14, 14])
+        super().__init__(fig)
+        # \n to offset labels from axes
+        self.set_xlabel("\n\nx [m]")
+        self.set_ylabel("\n\ny [m]")
+        self.set_zlabel("\n\nz [m]")
+
+
+class Plottable:
+    def __init__(self, plotter=None, object=None):
+        self._plotter = plotter
+        self._plottable_object = object
+
+    def plot(self, **kwargs):
+        self._plotter.plot(self._plottable_object, **kwargs)
+
+
+class BasePlotter(ABC):
+    """
+    Base utility plotting class
+    """
+
+    def __init__(self, **kwargs):
+        self.data = []
+        if kwargs:
+            for k in kwargs:
+                if k in self.options:
+                    self.options[k] = kwargs[k]
+
+    @abstractmethod
+    def plot(self, data, ax=None, *argv, **kwargs):
+        pass
+
+
+class PointsPlotter(BasePlotter):
+    """
+    Base utility plotting class for points
+    """
+
+    def __init__(self, ax=None, **kwargs):
+        self.options = DEFAULT
+        super().__init__(**kwargs)
+
+    def plot(self, points, ax=None, show: bool = False, block: bool = False):
+        if not self.options["plot_flag"]["poptions"]:
+            return ax
+
+        if not self.options["poptions"]:
+            self.ax = ax
+        else:
+            if ax is None:
+                self.ax = Plot3D()
+            else:
+                self.ax = ax
+
+            self.ax.scatter(*points, **self.options["poptions"])
+            self.data = points
+
+            if show:
+                plt.gca().set_aspect("auto")
+                plt.show(block=block)
+        return self.ax
+
+
+class WirePlotter(BasePlotter):
+    """
+    Base utility plotting class for bluemira wires
+    """
+
+    def __init__(self, **kwargs):
+        self.options = DEFAULT
+        super().__init__(**kwargs)
+
+    def plot(
+        self,
+        wire,
+        ax=None,
+        show: bool = False,
+        block: bool = False,
+        ndiscr=100,
+        byedges=True,
+    ):
+
+        if (
+            not self.options["plot_flag"]["poptions"]
+            and not self.options["plot_flag"]["woptions"]
+        ):
+            return ax
+
+        if not self.options["poptions"] and not self.options["woptions"]:
+            return ax
+        else:
+            if ax is None:
+                self.ax = Plot3D()
+            else:
+                self.ax = ax
+
+            if not isinstance(wire, BluemiraWire):
+                raise ValueError("wire must be a BluemiraWire")
+
+            pointsw = wire.discretize(ndiscr=ndiscr, byedges=byedges).T
+            self.data = pointsw
+
+            if self.options["plot_flag"]["woptions"]:
+                self.ax.plot(*pointsw, **self.options["woptions"])
+
+            if self.options["plot_flag"]["poptions"]:
+                pplotter = PointsPlotter(**self.options)
+                self.ax = pplotter.plot(pointsw, self.ax, show=False)
+
+            if show:
+                plt.gca().set_aspect("auto")
+                plt.show(block=block)
+
+        return self.ax
+
+
+class FacePlotter(BasePlotter):
+    """
+    Base utility plotting class for bluemira faces
+    """
+
+    def __init__(self, **kwargs):
+        self.options = DEFAULT
+        super().__init__(**kwargs)
+
+    def plot(
+        self,
+        face,
+        ax=None,
+        show: bool = False,
+        block: bool = False,
+        ndiscr=100,
+        byedges=True,
+    ):
+
+        if (
+            not self.options["plot_flag"]["poptions"]
+            and not self.options["plot_flag"]["woptions"]
+            and not self.options["plot_flag"]["foptions"]
+        ):
+            return ax
+
+        if (
+            not self.options["poptions"]
+            and not self.options["woptions"]
+            and not self.options["foptions"]
+        ):
+            return ax
+        else:
+            if ax is None:
+                self.ax = Plot3D()
+            else:
+                self.ax = ax
+
+            if not isinstance(face, BluemiraFace):
+                raise ValueError("wire must be a BluemiraFace")
+
+            for boundary in face.boundary:
+                    wplotter = WirePlotter(**self.options)
+                    wplotter.plot(boundary, show=False)
+                    # TODO: self.data should add a None line every time a new
+                    #  boundary is found.
+                    self.data = wplotter.data
+
+            # TODO: implement face filling
+
+            if show:
+                plt.gca().set_aspect("auto")
+                plt.show(block=block)
+
+        return self.ax
---
+++ examples/bluemira/geometry/plotting_tutorial.py
@@ -0,0 +1,47 @@
+# bluemira is an integrated inter-disciplinary design tool for future fusion
+# reactors. It incorporates several modules, some of which rely on other
+# codes, to carry out a range of typical conceptual fusion reactor design
+# activities.
+#
+# Copyright (C) 2021 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh, J. Morris,
+#                    D. Short
+#
+# bluemira is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# bluemira is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
+
+from bluemira.plotting.plotter import PointsPlotter, WirePlotter, FacePlotter
+from bluemira.geometry.parameterisations import PrincetonD
+from bluemira.geometry.face import BluemiraFace
+
+p = PrincetonD()
+p.adjust_variable("x1", 4, lower_bound=3, upper_bound=5)
+p.adjust_variable("x2", 16, lower_bound=10, upper_bound=20)
+p.adjust_variable("dz", 0, lower_bound=0, upper_bound=0)
+
+wire = p.create_shape()
+face = BluemiraFace(wire)
+
+array = p.create_array(n_points=200)
+
+pplotter = PointsPlotter(poptions={"s": 30, "facecolors": "red", "edgecolors": "black"})
+pplotter.plot(array, show=True, block=True)
+
+wplotter = WirePlotter()
+wplotter.plot(wire, show=True, block=True)
+
+wplotter.options["poptions"] = {}
+wplotter.plot(wire, show=True, block=True)
+
+# Todo: FacePlotter need adjustments. It doesn't work.
+fplotter = FacePlotter()
+fplotter.plot(face, show=True, block=True)
