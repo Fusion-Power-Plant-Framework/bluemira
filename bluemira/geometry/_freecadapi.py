2e40bfe09041edfd8801c457597b40ae28753d6f
CoronelBuendia <matti.coleman@gmail.com>
2021-10-12 19:09:01 +0200

    roll back 2 point discr

--- bluemira/geometry/_freecadapi.py
+++ bluemira/geometry/_freecadapi.py
@@ -441,8 +441,7 @@ def discretize(w: Part.Wire, ndiscr: int = 10, dl: float = None):
         raise ValueError("dl must be > 0.")
     else:
         # a dl is calculated for the discretisation of the different edges
-        # NOTE: must discretise to at least two points.
-        ndiscr = max(math.ceil(w.Length / dl), 2)
+        ndiscr = math.ceil(w.Length / dl)
 
     # discretization points array
     output = w.discretize(ndiscr)
