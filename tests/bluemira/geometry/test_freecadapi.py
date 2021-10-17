0a44af2ab5e7b76d10ca90265df0222ee75a703a
ivanmaione <maione.ivan@gmail.com>
2021-10-17 18:31:11 +0200

    new test for discretize and discretize_by_edges

--- tests/bluemira/geometry/test_freecadapi.py
+++ tests/bluemira/geometry/test_freecadapi.py
@@ -152,12 +152,10 @@ class TestFreecadapi:
         wire: Part.Wire = freecadapi.make_polygon(self.square_points, True)
         ndiscr = 10
         points = freecadapi.discretize(wire, ndiscr)
-        print(f"Points = {points}")
         assert len(points) == ndiscr
         length_w = wire.Length
         dl = length_w / float(ndiscr)
         points = freecadapi.discretize(wire, dl=dl)
-        print(f"Points = {points}")
         assert len(points) == ndiscr
 
     def test_discretize_by_edges(self):
@@ -172,3 +170,23 @@ class TestFreecadapi:
         dl = 0.4
         points = freecadapi.discretize_by_edges(wire, ndiscr=100, dl=dl)
         print(f"Points = {points}")
+
+    def test_discretize_vs_discretize_by_edges(self):
+        wire1 = freecadapi.make_polygon([[0, 0, 0], [1, 0, 0], [1, 1, 0]])
+        wire2 = freecadapi.make_polygon([[0, 0, 0], [0, 1, 0], [1, 1, 0]])
+        wire2.reverse()
+        wire = Part.Wire([wire1, wire2])
+
+        # ndiscr is chosen in such a way that both discretize and discretize_by_edges
+        # give the same points (so that a direct comparison is possible).
+        points1 = freecadapi.discretize(wire, ndiscr=5)
+        points2 = freecadapi.discretize_by_edges(wire, ndiscr=4)
+
+        # assert that points1 and points2 are the same
+        comparison = True
+        for index in range(len(points1)):
+            for i in range(3):
+                comparison = comparison & (
+                    points1[index][i] - points2[index][i] < D_TOLERANCE
+                )
+        assert comparison
