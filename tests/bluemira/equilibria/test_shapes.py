7c453f05e9dde0820289644c10a8eb5365d724cf
CoronelBuendia <matti.coleman@gmail.com>
2021-10-18 15:10:29 +0200

    A and name

--- tests/bluemira/equilibria/test_shapes.py
+++ tests/bluemira/equilibria/test_shapes.py
@@ -3,7 +3,7 @@
 # codes, to carry out a range of typical conceptual fusion reactor design
 # activities.
 #
-# Copyright (C) 2021 M. Coleman, J. Cook, F. Franza, I. Maione, S. McIntosh, J. Morris,
+# Copyright (C) 2021 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh, J. Morris,
 #                    D. Short
 #
 # bluemira is free software; you can redistribute it and/or
@@ -177,7 +177,7 @@ class TestJohner:
             plt.show()
 
 
-class TestJohner:
+class TestJohnerCAD:
     def test_segments(self):
         p = JohnerLCFS()
         wire = p.create_shape()
