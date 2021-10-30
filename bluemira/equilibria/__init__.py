410b6f1869ff20252dc26c7c0ab49278c4dd5b6d
alexanderianblair <alexander.blair@ukaea.uk>
2021-10-30 03:09:36 +0100

    Add nested Coilser optimiser for faster convergence

--- bluemira/equilibria/__init__.py
+++ bluemira/equilibria/__init__.py
@@ -43,6 +43,7 @@ from .optimiser import (
     BoundedCurrentOptimiser,
     PositionOptimiser,
     CoilsetOptimiser,
+    NestedCoilsetOptimiser,
 )
 from .find import find_flux_surfs, find_LCFS_separatrix, find_OX_points
 from .equilibrium import Equilibrium, Breakdown
