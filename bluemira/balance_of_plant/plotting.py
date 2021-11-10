a2a2a5b4c7226c8547febb0d93e661b959b56cd5
CoronelBuendia <matti.coleman@gmail.com>
2021-11-10 19:31:48 +0100

    flake8

--- bluemira/balance_of_plant/plotting.py
+++ bluemira/balance_of_plant/plotting.py
@@ -19,6 +19,10 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
 
+"""
+Plotting for balance of plant
+"""
+
 import numpy as np
 from copy import deepcopy
 import matplotlib.pyplot as plt
