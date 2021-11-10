da8122423951302e59a0bc1c593c78dcc9922a05
ivanmaione <maione.ivan@gmail.com>
2021-11-10 11:48:50 +0100

    Added Meshable class

--- bluemira/geometry/base.py
+++ bluemira/geometry/base.py
@@ -36,13 +36,38 @@ from bluemira.geometry.bound_box import BoundingBox
 
 import copy
 
+import bluemira.mesh.meshing as meshing
+
+
+class GeoMeshable(meshing.Meshable):
+    """
+    Extended Meshable class for BluemiraGeo objects.
+    """
+    def remove_mesh_options(self, recursive=False):
+        """Remove mesh options for this object"""
+        super().remove_mesh_options()
+        if hasattr(self, 'boundary'):
+            for obj in self.boundary:
+                if isinstance(obj, GeoMeshable):
+                    obj.remove_mesh_options(recursive=True)
+
+    def print_mesh_options(self, recursive=True):
+        # Todo: improve the output of this function
+        output = []
+        output.append(self.mesh_options)
+        if hasattr(self, 'boundary'):
+            for obj in self.boundary:
+                if isinstance(obj, GeoMeshable):
+                    output.append(obj.print_mesh_options(True))
+        return output
+
 
 class _Orientation(enum.Enum):
     FORWARD = "Forward"
     REVERSED = "Reversed"
 
 
-class BluemiraGeo(ABC):
+class BluemiraGeo(ABC, GeoMeshable):
     """Base abstract class for geometry
 
     Parameters
@@ -61,6 +86,7 @@ class BluemiraGeo(ABC):
         label: str = "",
         boundary_classes=None,
     ):
+        super().__init__()
         self._boundary_classes = boundary_classes
         self.boundary = boundary
         self.label = label
