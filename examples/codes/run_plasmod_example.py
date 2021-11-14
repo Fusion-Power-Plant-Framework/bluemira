c5b819fbe2459714c4533be94b962335f3ac26c0
ivanmaione <maione.ivan@gmail.com>
2021-11-14 16:45:28 +0100

    example to run plasmod (mock test)

---
+++ examples/codes/run_plasmod_example.py
@@ -0,0 +1,31 @@
+# bluemira is an integrated inter-disciplinary design tool for future fusion
+# reactors. It incorporates several modules, some of which rely on other
+# codes, to carry out a range of typical conceptual fusion reactor design
+# activities.
+#
+# Copyright (C) 2021 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh, J. Morris,
+#                    D. Short
+#
+# bluemira is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# bluemira is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
+
+"""
+Test for plasmod run
+"""
+from bluemira.codes.plasmod import plasmodapi
+
+plasmod_solver = plasmodapi.PlasmodSolver()
+plasmod_solver.set_runmode('MOCK')
+plasmod_solver.run()
+
+print(f"FFprime: {plasmod_solver._out_params._FFprime}")
