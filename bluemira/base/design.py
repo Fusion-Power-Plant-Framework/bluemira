c72896e332de8460394d9e41716e8e863848f804
Dan Short <dan.short@ukaea.uk>
2021-11-16 17:50:46 +0000

    Update api

--- bluemira/base/design.py
+++ bluemira/base/design.py
@@ -72,7 +72,7 @@ class Design:
         Parameters are transferred onto the Design after each step.
         """
         for builder in self._builders:
-            build_result = builder.build(self._params)
+            build_result = builder(self._params)
             for result in build_result:
                 self._component_manager.insert_at_path(result[0], result[1])
             self._params.update_kw_parameters(
