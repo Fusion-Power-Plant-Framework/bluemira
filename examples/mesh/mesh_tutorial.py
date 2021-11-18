9260a3b8d9064322933f69c1ac949b78c6800ffa
ivanmaione <maione.ivan@gmail.com>
2021-11-18 16:54:00 +0100

    updated mesh tutorial

--- examples/mesh/mesh_tutorial.py
+++ examples/mesh/mesh_tutorial.py
@@ -32,3 +32,57 @@ from bluemira.equilibria.shapes import JohnerLCFS
 
 # %%
 p = JohnerLCFS()
+
+# %%
+
+from bluemira.equilibria.shapes import JohnerLCFS
+#import bluemira.display as display
+from bluemira.mesh import meshing
+from bluemira.geometry.face import BluemiraFace
+import bluemira.mesh.msh2xdmf as msh2xdmf
+import dolfin
+# %%[markdown]
+
+# Creation of a simple geometry
+
+# %%
+
+p = JohnerLCFS()
+lcfs = p.create_shape(label="LCFS")
+lcfs.mesh_options = {'lcar': 0.3, 'physical_group': 'LCFS'}
+face = BluemiraFace(lcfs, label="plasma_surface")
+face.mesh_options = {"lcar": 0.1, "physical_group": "surface"}
+#display.plot_2d(face)
+
+# %%[markdown]
+
+# Mesh creation
+
+# %%
+m = meshing.Mesh()
+buffer = m(face)
+print(m.get_gmsh_dict(buffer))
+
+# %%[markdown]
+
+# Convert the me in xdmf for reading in fenics
+
+# %%
+
+msh2xdmf.msh2xdmf("Mesh.msh", dim=2, directory=".")
+
+mesh, boundaries, subdomains, labels = msh2xdmf.import_mesh(
+    prefix="Mesh",
+    dim=2,
+    directory=".",
+    subdomains=True,
+)
+# # %%[markdown]
+
+# # Plot the mesh
+
+# # %%
+# # If the mesh is made by 3D points, the plot with dolfin doesn't work
+# dolfin.plot(mesh)
+
+print(mesh.coordinates())
