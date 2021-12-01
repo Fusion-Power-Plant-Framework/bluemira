e4749d4ebee341eb2c87c6fbd6ed0fb7f4e8af06
Dan Short <dan.short@ukaea.uk>
2021-12-01 13:11:02 +0000

    Move config extraction into EU-DEMO example

--- examples/design/EU-DEMO/EUDEMO_reactor.py
+++ examples/design/EU-DEMO/EUDEMO_reactor.py
@@ -23,17 +23,85 @@
 Perform the EU-DEMO reactor design.
 """
 
-from bluemira.builders.EUDEMO.config import params, build_config
+import json
+
+from bluemira.base.config import Configuration
+from bluemira.base.file import get_bluemira_root
+from bluemira.base.parameter import ParameterError
+
 from bluemira.builders.EUDEMO.reactor import EUDEMO
 
 from bluemira.codes import plot_PROCESS
 
-# If you have PROCESS installed then change these to enable PROCESS runs.
+# First define the configuration for the run.
+
+params = {}
+for param in Configuration.params:
+    params[param[0]] = {}
+    params[param[0]]["name"] = param[1]
+    params[param[0]]["value"] = param[2]
+    params[param[0]]["unit"] = param[3]
+    params[param[0]]["source"] = param[5]
+    if param[4] is not None:
+        params[param[0]]["description"] = param[4]
+    if len(param) == 7:
+        params[param[0]]["mapping"] = {
+            key: value.to_dict() for key, value in param[6].items()
+        }
+
+params = dict(sorted(params.items()))
+
+with open(f"{get_bluemira_root()}/examples/design/EU-DEMO/template.json", "w") as fh:
+    json.dump(params, fh, indent=2, ensure_ascii=False)
+
+config = {
+    "Name": "EU-DEMO",
+    "tau_flattop": 6900,
+    "n_TF": 18,
+    "fw_psi_n": 1.06,
+    "tk_tf_front_ib": 0.05,
+    "tk_bb_ib": 0.755,
+    "tk_bb_ob": 1.275,
+    "g_tf_pf": 0.05,
+    "C_Ejima": 0.3,
+    "eta_nb": 0.4,
+    "LPangle": -15,
+    "w_g_support": 1.5,
+}
+
+for key, val in config.items():
+    if isinstance(val, dict):
+        for attr, attr_val in val.items():
+            if attr in ["name", "unit"]:
+                raise ParameterError(f"Cannot set {attr} in parameter configuration.")
+            params[key][attr] = attr_val
+    else:
+        params[key]["value"] = val
+
+with open(f"{get_bluemira_root()}/examples/design/EU-DEMO/params.json", "w") as fh:
+    json.dump(config, fh, indent=2, ensure_ascii=False)
+
+build_config = {
+    "reference_data_root": "!BM_ROOT!/data",
+    "generated_data_root": "!BM_ROOT!/generated_data",
+    "process_mode": "mock",
+}
+
+with open(f"{get_bluemira_root()}/examples/design/EU-DEMO/build_config.json", "w") as fh:
+    json.dump(build_config, fh, indent=2, ensure_ascii=False)
+
+# If you have PROCESS installed then change these to enable a PROCESS run or to read
+# an existing PROCESS output.
+
 # build_config["process_mode"] = "run"
 # build_config["process_mode"] = "read"
 
+# Create the Reactor and run the design.
+
 reactor = EUDEMO(params, build_config)
 reactor.run()
+
+# Display the PROCESS radial build.
 
 if build_config["process_mode"] == "run":
     plot_PROCESS(reactor.file_manager.generated_data_dirs["systems_code"])
