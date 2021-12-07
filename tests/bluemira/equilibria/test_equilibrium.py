9836d038c84eea7da6025088f7617ed9e7bf2d26
CoronelBuendia <matti.coleman@gmail.com>
2021-12-07 12:04:41 +0100

    Equilibrium read/write bug-fix (#503)
    * add test
    
    * fix test but I dont like the look of q
    
    * remove comment
    
    * parametrize and remove wasted compute

--- tests/bluemira/equilibria/test_equilibrium.py
+++ tests/bluemira/equilibria/test_equilibrium.py
@@ -32,6 +32,7 @@ from bluemira.equilibria.grid import Grid
 from bluemira.equilibria.profiles import DoublePowerFunc
 from bluemira.equilibria.equilibrium import Equilibrium
 from bluemira.equilibria.run import AbInitioEquilibriumProblem
+from bluemira.utilities.tools import compare_dicts
 
 
 class TestFields:
@@ -249,6 +250,25 @@ class TestEquilibrium:
             assert eq_q.call_count == 1
             assert "qpsi" in res
             assert np.all(res["qpsi"] == 0)  # array is all zeros
+
+
+class TestEqReadWrite:
+    @pytest.mark.parametrize("qpsi_calcmode", [0, 1])
+    def test_read_write(self, qpsi_calcmode):
+        data_path = get_bluemira_path("bluemira/equilibria/test_data", subfolder="tests")
+        file_name = "eqref_OOB.json"
+        file_path = os.sep.join([data_path, file_name])
+
+        new_file_name = "eqref_OOB_temp1.json"
+        new_file_path = os.sep.join([data_path, new_file_name])
+        eq = Equilibrium.from_eqdsk(file_path)
+        eq.to_eqdsk(directory=data_path, filename=new_file_name)
+        d1 = eq.to_dict(qpsi_calcmode=qpsi_calcmode)
+
+        eq2 = Equilibrium.from_eqdsk(new_file_path)
+        d2 = eq2.to_dict(qpsi_calcmode=qpsi_calcmode)
+        os.remove(new_file_path)
+        assert compare_dicts(d1, d2)
 
 
 if __name__ == "__main__":
