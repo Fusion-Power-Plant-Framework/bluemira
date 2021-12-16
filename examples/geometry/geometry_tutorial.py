13b7ab6b6ea614f66f41f964b3e64bab773d6ec2
Matti <matti.coleman@ukaea.uk>
2021-12-16 19:42:39 +0100

    intro and cylinder

--- examples/geometry/geometry_tutorial.py
+++ examples/geometry/geometry_tutorial.py
@@ -20,10 +20,8 @@
 # License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
 
 """
-Some examples of using bluemira geometry objects.
+A geometry tutorial for users.
 """
-
-import bluemira.geometry as geo
 
 # %% [markdown]
 
@@ -34,7 +32,50 @@ import bluemira.geometry as geo
 # or another. Puns intended.
 
 # There a few basic concepts you need to familiarise yourself with:
-# * Basic types: [BluemiraWire, BluemiraFace, BluemiraShell, BluemiraSolid]
+# * Basic objects: [`BluemiraWire`, `BluemiraFace`, `BluemiraShell`, `BluemiraSolid`]
 # * Matryoshka structure
 # * Geometry creation
 # * Geometry operations
+
+## Imports
+
+# Let's start out by importing all the basic objects, and some typical tools
+
+# %%
+
+from bluemira.geometry.wire import BluemiraWire
+from bluemira.geometry.face import BluemiraFace
+from bluemira.geometry.shell import BluemiraShell
+from bluemira.geometry.solid import BluemiraSolid
+from bluemira.geometry.tools import (
+    make_circle,
+    make_polygon,
+    make_bspline,
+    revolve_shape,
+    extrude_shape,
+    sweep_shape,
+    boolean_cut,
+    boolean_fuse,
+)
+
+# %%[markdown]
+
+## Make a cylinder
+
+# There are many ways to make a cylinder, but perhaps the simplest way is as follows:
+# * Make a circular Wire
+# * Make a Face from that Wire
+# * Extrude that Face along a vector, to make a Solid
+
+# %%
+
+circle_wire = make_circle(
+    radius=5,
+    center=(0, 0, 0),
+    axis=(0, 0, 1),
+    start_angle=0,
+    end_angle=360,
+    label="my_wire",
+)
+circle_face = BluemiraFace(circle_wire, label="my_face")
+cylinder = extrude_shape(circle_face, vec=(0, 0, 10), label="my_solid")
