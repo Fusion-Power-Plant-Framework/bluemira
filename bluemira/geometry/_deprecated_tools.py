ce0bf5dcd87f5b3389b136f0f4caa17ea13435cb
je-cook <81617086+je-cook@users.noreply.github.com>
2022-01-10 16:53:31 +0000

    CI precommit (#576)
    * ignore ipynb version
    
    * CI additions
    
    * precommit fixes
    
    * 🔥 remove old files

--- bluemira/geometry/_deprecated_tools.py
+++ bluemira/geometry/_deprecated_tools.py
@@ -37,8 +37,8 @@ from bluemira.base.look_and_feel import bluemira_warn
 from bluemira.codes import _freecadapi as cadapi
 from bluemira.geometry.bound_box import BoundingBox
 from bluemira.geometry.constants import CROSS_P_TOL, DOT_P_TOL
+from bluemira.geometry.coordinates import _validate_coordinates, get_area
 from bluemira.geometry.error import GeometryError
-from bluemira.geometry.coordinates import get_area, _validate_coordinates
 from bluemira.geometry.face import BluemiraFace
 from bluemira.geometry.wire import BluemiraWire
 from bluemira.utilities.tools import flatten_iterable
