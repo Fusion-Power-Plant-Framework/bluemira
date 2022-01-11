3aa5517d8e518d88a0e8255d1b56391aeec24c71
CoronelBuendia <matti.coleman@gmail.com>
2022-01-11 13:44:47 +0100

    more skeleton and prep for PF

--- bluemira/builders/thermal_shield.py
+++ bluemira/builders/thermal_shield.py
@@ -26,10 +26,14 @@ Built-in build steps for making a parameterised thermal shield.
 from typing import List
 
 import numpy as np
+from scipy.spatial import ConvexHull
 
 import bluemira.utilities.plot_tools as bm_plot_tools
 from bluemira.base.builder import BuildConfig, Builder
-from bluemira.base.components import Component
+from bluemira.base.components import Component, PhysicalComponent
+from bluemira.display.palettes import BLUE_PALETTE
+from bluemira.geometry.face import BluemiraFace
+from bluemira.geometry.tools import boolean_fuse, make_polygon, offset_wire
 
 
 class ThermalShieldBuilder(Builder):
@@ -38,15 +42,24 @@ class ThermalShieldBuilder(Builder):
     """
 
     _required_params: List[str] = [
-        None,
+        "tk_ts",
+        "g_ts_pf",
+        "g_ts_tf",
+        "g_vv_ts",
     ]
+
+    def run(self, pf_coils_xz_kozs, tf_xz_koz=None, vv_xz_koz=None):
+        self._pf_coils = pf_coils_xz_kozs
+        self._tf_koz = tf_xz_koz
+        self._vv_koz = vv_xz_koz
 
     def build(self, label: str = "Thermal Shield", **kwargs) -> Component:
         """
-        Build the PF Coils component.
+        Build the thermal shield component.
+
         Returns
         -------
-        component: PFCoilsComponent
+        component: Component
             The Component built by this builder.
         """
         super().build(**kwargs)
@@ -61,16 +74,59 @@ class ThermalShieldBuilder(Builder):
         """
         Build the x-z components of the thermal shield.
         """
-        pass
+        # Cryostat thermal shield
+        pf_xz = self._pf_coils
+        x, z = [], []
+        for coil in pf_xz:
+            bound_box = coil.bounding_box
+            xc = 0.5 * (bound_box.x_max - bound_box.x_min)
+            zc = 0.5 * (bound_box.z_max - bound_box.z_min)
+            dx = bound_box.x_max - xc
+            dz = abs(bound_box.z_max - zc)
+            z_sign = -1 if zc < 0 else 1
+
+            x.append(xc + dx)
+            z.append(zc + z_sign * dz)
+
+        # Project extrema onto axis (might be bad for NT)
+        x.extend([0, 0])
+        z.extend([np.min(z), np.max(z)])
+
+        hull = ConvexHull(np.array([x, z]))
+        wire = make_polygon([hull.vertices[0], 0, hull.vertices[1]])
+        wire = offset_wire(wire, self.params.g_ts_pf, open_wire=True)
+        o_wire = offset_wire(wire, self.params.tk_ts, open_wire=True)
+        # TODO: Add boolean union of TF exclusion zone
+
+        cts = BluemiraFace(wire)
+        cryostat_ts = PhysicalComponent("Cryostat TS", cts)
+        cryostat_ts.plot_options.face_options["color"] = BLUE_PALETTE["TS"][0]
+
+        component = Component("xz", children=[cts])
+        bm_plot_tools.set_component_plane(component, "xz")
+        return component
 
     def build_xy(self):
         """
         Build the x-y components of the thermal shield.
         """
-        pass
+        # Cryostat thermal shield
+        cts = None
+        cryostat_ts = PhysicalComponent("Cryostat TS", cts)
+        cryostat_ts.plot_options.face_options["color"] = BLUE_PALETTE["TS"][0]
+
+        component = Component("xy", children=[cts])
+        bm_plot_tools.set_component_plane(component, "xy")
+        return component
 
     def build_xyz(self):
         """
         Build the x-y-z components of the thermal shield.
         """
-        pass
+        # Cryostat thermal shield
+        cts = None
+        cryostat_ts = PhysicalComponent("Cryostat TS", cts)
+        cryostat_ts.display_cad_options.color = BLUE_PALETTE["TS"][0]
+
+        component = Component("xyz", children=[cts])
+        return component
