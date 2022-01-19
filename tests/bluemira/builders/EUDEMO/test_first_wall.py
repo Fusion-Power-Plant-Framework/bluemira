ad2e855e0739836489b99d7baa23c5befbb2709d
Harry Saunders <harry.saunders@ukaea.uk>
2022-01-19 12:20:30 +0000

    Add basic builder for EUDEMO first wall
    The wall is currently a simple PolySpline in the xz plane, with no
    optimisations implemented yet.

---
+++ tests/bluemira/builders/EUDEMO/test_first_wall.py
@@ -0,0 +1,71 @@
+# bluemira is an integrated inter-disciplinary design tool for future fusion
+# reactors. It incorporates several modules, some of which rely on other
+# codes, to carry out a range of typical conceptual fusion reactor design
+# activities.
+#
+# Copyright (C) 2021 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh, J. Morris,
+#                    D. Short
+#
+# bluemira is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# bluemira is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
+import copy
+
+from bluemira.builders.EUDEMO.first_wall import FirstWallBuilder
+
+
+class TestFirstWallBuilder:
+
+    _default_variables_map = {
+        "x1": {  # ib radius
+            "value": "r_fw_ib_in",
+            "fixed": True,
+        },
+        "x2": {  # ob radius
+            "value": "r_fw_ob_in",
+            "lower_bound": 10.0,
+        },
+    }
+
+    _default_config = {
+        "param_class": "bluemira.builders.EUDEMO.first_wall::FirstWallPolySpline",
+        "variables_map": _default_variables_map,
+        "runmode": "mock",
+        "name": "First Wall",
+    }
+
+    _params = {
+        "Name": "First Wall Example",
+        "plasma_type": "SN",
+        "R_0": (9.0, "Input"),
+        "kappa_95": (1.6, "Input"),
+        "r_fw_ib_in": (5.8, "Input"),
+        "r_fw_ob_in": (12.1, "Input"),
+        "A": (3.1, "Input"),
+    }
+
+    def test_builder_has_name_from_config(self):
+        config = copy.deepcopy(self._default_config)
+        config["name"] = "New name"
+
+        builder = FirstWallBuilder(self._params, build_config=config)
+
+        assert builder.name == "New name"
+
+    def test_built_component_contains_physical_component_in_xz(self):
+        builder = FirstWallBuilder(self._params, build_config=self._default_config)
+
+        component = builder(self._params)
+
+        xy_component = component.get_component("xz", first=False)
+        assert len(xy_component) == 1
+        assert len(xy_component[0].get_component("first_wall", first=False)) == 1
