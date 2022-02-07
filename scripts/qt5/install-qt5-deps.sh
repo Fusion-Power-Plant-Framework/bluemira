3a312e7eeb86739e08af4a87b8f22e6e713613ee
Dan Short <dan.short@ukaea.uk>
2022-02-07 21:15:08 +0000

    Update scripts

---
+++ scripts/qt5/install-qt5-deps.sh
@@ -0,0 +1,3 @@
+apt-get install -y build-essential perl python git '^libxcb.*-dev' libx11-xcb-dev \
+                   libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev \
+                   libxkbcommon-x11-dev libclang-6.0-dev llvm-6.0
