16504c9c72c6573b3eda04f964b1390179b5c08d
Dan Short <dan.short@ukaea.uk>
2022-02-10 14:40:01 +0000

    Remove libcoin dependency

--- scripts/freecad/install-freecad-deps.sh
+++ scripts/freecad/install-freecad-deps.sh
@@ -4,7 +4,7 @@ apt-get install -y \
     cmake cmake-gui libboost-date-time-dev libboost-dev libboost-filesystem-dev \
     libboost-graph-dev libboost-iostreams-dev libboost-program-options-dev \
     libboost-python-dev libboost-regex-dev libboost-serialization-dev \
-    libboost-thread-dev libcoin-dev libeigen3-dev libgts-bin libgts-dev libkdtree++-dev \
+    libboost-thread-dev libeigen3-dev libgts-bin libgts-dev libkdtree++-dev \
     libmedc-dev libocct-data-exchange-dev libocct-ocaf-dev libocct-visualization-dev \
     libopencv-dev libproj-dev libspnav-dev libvtk7-dev libx11-dev libxerces-c-dev \
     libzipios++-dev occt-draw python3-dev libclang-dev llvm
