6de9b84377e7317f1b20ac979e11f80381aa9396
Dan Short <dan.short@ukaea.uk>
2022-02-10 14:40:25 +0000

    Align formatting

--- scripts/qt5/install-qt5-deps.sh
+++ scripts/qt5/install-qt5-deps.sh
@@ -1,3 +1,4 @@
-apt-get install -y build-essential perl python git '^libxcb.*-dev' libx11-xcb-dev \
-                   libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev \
-                   libxkbcommon-x11-dev libclang-6.0-dev llvm-6.0
+apt-get install -y \
+    build-essential perl python git '^libxcb.*-dev' libx11-xcb-dev \
+    libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev \
+    libxkbcommon-x11-dev libclang-6.0-dev llvm-6.0
