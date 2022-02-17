signs will still be wrong, but COCOS

bluemira/equilibria/profiles.py
@@ -674,13 +674,13 @@ class CustomProfile(Profile):
         self.psisep = psisep
         self.psiax = psiax
         psi_norm = np.clip((psi - psiax) / (psisep - psiax), 0.0, 1.0)
-        jtor = np.abs(x * self._pprime(psi_norm) + self._ffprime(psi_norm) / (x * MU_0))
+        jtor = x * self._pprime(psi_norm) + self._ffprime(psi_norm) / (x * MU_0)
         if mask is not None:
             jtor *= mask
         if self.Ip is not None:
             # This is a simple way to prescribe the plasma current
             Ip = self.int2d(jtor)
-            self.scale = abs(self.Ip / Ip)
+            self.scale = self.Ip / Ip
             jtor *= self.scale
         return jtor
 