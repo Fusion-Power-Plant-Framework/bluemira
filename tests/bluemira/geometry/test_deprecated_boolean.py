f5ee6b6c185f3ac37e5b98fda76ba842373076e7
James <james.cook1@ukaea.uk>
2022-02-23 14:16:25 +0000

    💩 definitely not a hack

--- tests/bluemira/geometry/test_deprecated_boolean.py
+++ tests/bluemira/geometry/test_deprecated_boolean.py
@@ -107,7 +107,7 @@ class TestBooleanSimple:
 class TestBooleanOpen:
     @pytest.mark.longrun
     def test_openclip(self):
-        fp = get_bluemira_path("geometry/test_data", subfolder="tests")
+        fp = get_bluemira_path("bluemira/geometry/test_data", subfolder="tests")
         fn = os.sep.join([fp, "loopcut_data.pkl"])
         with open(fn, "rb") as f:
             data = pickle.load(f)  # noqa :S301
@@ -215,7 +215,7 @@ class TestBooleanShell:
 class TestCleanloop:
     @pytest.mark.longrun
     def test_hanging_chad(self):
-        fp = get_bluemira_path("geometry/test_data", subfolder="tests")
+        fp = get_bluemira_path("bluemira/geometry/test_data", subfolder="tests")
         fn = os.sep.join([fp, "classic_hanging_chad.json"])
         loop = Loop.from_file(fn)
         f, ax = plt.subplots()
