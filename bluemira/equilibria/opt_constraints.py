73874704c5059c54c0fe81152ea40bad6dd91cbb
CoronelBuendia <matti.coleman@gmail.com>
2022-03-16 13:15:26 +0100

    add force constraint

--- bluemira/equilibria/opt_constraints.py
+++ bluemira/equilibria/opt_constraints.py
@@ -54,6 +54,8 @@ optimisaiton algorithms, but will need to be updated or approximated for use
 in derivative based algorithms, such as those utilising gradient descent.
 """  # noqa (W505)
 
+import numpy as np
+
 
 def objective_constraint(constraint, vector, grad, objective_function, maximum_fom=1.0):
     """
@@ -92,4 +94,90 @@ def current_midplane_constraint(constraint, vector, grad, eq, radius, inboard=Tr
         constraint[:] = radius - min(lcfs.x)
     else:
         constraint[:] = max(lcfs.x) - radius
+    return constraint
+
+
+def coil_force_constraints(
+    constraint, vector, grad, eq, n_PF, n_CS, PF_Fz_max, CS_Fz_sum, CS_Fz_sep, scale
+):
+    """
+    Current optimisation force constraints
+
+    Parameters
+    ----------
+    constraint: np.ndarray
+        Constraint array (modified in place)
+    vector: np.ndarray
+        Current vector
+    grad: np.ndarray
+        Constraint Jacobian (modified in place)
+    eq: Equilibrium
+        Equilibrium object with which to calculate constraints
+    n_PF: int
+        Number of PF coils
+    n_CS: int
+        Number of CS coils
+    PF_Fz_max: float
+        Maximum vertical force on each PF coil [MN]
+    CS_Fz_sum: float
+        Maximum total vertical force on the CS stack [MN]
+    CS_Fz_sep: float
+        Maximum vertical separation force in the CS stack [MN]
+    scale: float
+        Current scale with which to calculate the constraints
+
+    Returns
+    -------
+    constraint: np.ndarray
+        Updated constraint vector
+
+    Note
+    ----
+    Modifies (in place):
+        constraint
+        grad
+    """
+    # get coil force and jacobian
+    F, dF = eq.force_field.calc_force(vector * scale)  # noqa :N803
+    F /= scale  # Scale down to MN
+    # dF /= self.scale
+
+    # calculate constraint jacobian
+    if grad.size > 0:
+        # PFz lower bound
+        grad[:n_PF] = -dF[:n_PF, :, 1]
+        # PFz upper bound
+        grad[n_PF : 2 * n_PF] = dF[:n_PF, :, 1]
+
+        if n_CS != 0:
+            # CS sum lower
+            grad[2 * n_PF] = -np.sum(dF[n_PF:, :, 1], axis=0)
+            # CS sum upper
+            grad[2 * n_PF + 1] = np.sum(dF[n_PF:, :, 1], axis=0)
+            for j in range(n_CS - 1):  # evaluate each gap in CS stack
+                # CS separation constraint Jacobians
+                f_up = np.sum(dF[n_PF : n_PF + j + 1, :, 1], axis=0)
+                f_down = np.sum(dF[n_PF + j + 1 :, :, 1], axis=0)
+                grad[2 * n_PF + 2 + j] = f_up - f_down
+
+    # vertical force on PF coils
+    pf_fz = F[:n_PF, 1]
+    # PFz lower bound
+    constraint[:n_PF] = -PF_Fz_max - pf_fz
+    # PFz upper bound
+    constraint[n_PF : 2 * n_PF] = pf_fz - PF_Fz_max
+
+    if n_CS != 0:
+        # vertical force on CS coils
+        cs_fz = F[n_PF:, 1]
+        # vertical force on CS stack
+        cs_z_sum = np.sum(cs_fz)
+        # CSsum lower bound
+        constraint[2 * n_PF] = -CS_Fz_sum - cs_z_sum
+        # CSsum upper bound
+        constraint[2 * n_PF + 1] = cs_z_sum - CS_Fz_sum
+        for j in range(n_CS - 1):  # evaluate each gap in CS stack
+            # CS seperation constraints
+            f_sep = np.sum(cs_fz[: j + 1]) - np.sum(cs_fz[j + 1 :])
+            constraint[2 * n_PF + 2 + j] = f_sep - CS_Fz_sep
     return constraint
