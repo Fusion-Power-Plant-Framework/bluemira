23dd92bb140c22e7656522f18d2ab998fa1e0898
James <james.cook1@ukaea.uk>
2022-03-21 17:09:31 +0000

    🎨 Problem Settings

--- bluemira/codes/process/setup.py
+++ bluemira/codes/process/setup.py
@@ -58,6 +58,11 @@ class Setup(interface.Setup):
     Setup Task for process
     """
 
+    def __init__(self, parent, *args, problem_settings=None, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        self._problem_settings = problem_settings if problem_settings is not None else {}
+
     def _run(self):
         """
         Write the IN.DAT file and store in the main PROCESS folder
@@ -104,8 +109,10 @@ class Setup(interface.Setup):
             )
 
         if use_bp_inputs is True:
-            _inputs = self.get_new_inputs(remapper=update_obsolete_vars)
+            _inputs = self._get_new_inputs(remapper=update_obsolete_vars)
             for key, value in _inputs.items():
+                writer.add_parameter(key, value)
+            for key, value in self._problem_settings.items():
                 writer.add_parameter(key, value)
 
         self._validate_models(writer)
