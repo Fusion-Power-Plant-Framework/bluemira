a5746f55f63b4923a4cc716377c23b05c7105407
CoronelBuendia <matti.coleman@gmail.com>
2022-03-23 12:18:48 +0100

    OMG freecad why doesn't 1e-8 work?

--- tests/bluemira/geometry/test_wire.py
+++ tests/bluemira/geometry/test_wire.py
@@ -200,13 +200,13 @@ class TestWireParameterAt(ValueParameterBase):
         assert np.isclose(self.mixed.parameter_at([1, 0, -2]), (1 + np.pi) / (2 + np.pi))
         assert np.isclose(self.mixed.parameter_at([2, 0, -2]), 1.0)
 
-    @pytest.mark.parametrize("tolerance", [1e-5, 1e-8, 1e-17])
+    @pytest.mark.parametrize("tolerance", [1e-5, 1e-7, 1e-17])
     def test_tolerance(self, tolerance):
         line = make_polygon([[0, 0, 0], [1, 0, 0]])
         alpha = line.parameter_at([-tolerance, 0, 0], tolerance=tolerance)
         assert np.isclose(alpha, 0.0)
 
-    @pytest.mark.parametrize("tolerance", [1e-5, 1e-8, 1e-16])
+    @pytest.mark.parametrize("tolerance", [1e-5, 1e-7, 1e-16])
     def test_error(self, tolerance):
         with pytest.raises(GeometryError):
             line = make_polygon([[0, 0, 0], [1, 0, 0]])
