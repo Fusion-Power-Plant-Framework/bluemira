e40ee2d3c2c8b92e820835b230e9f5bc85a7f59d
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-03-31 11:22:02 +0100

    Use param attribute, not getter method in reactor

--- bluemira/builders/EUDEMO/reactor.py
+++ bluemira/builders/EUDEMO/reactor.py
@@ -291,7 +291,7 @@ class EUDEMOReactor(Reactor):
         bluemira_print(f"Completed design stage: {name}")
         # TODO(hsaunders1904): this is not functional at the moment,
         # need to check parameter access is correct
-        return build_ivc_xz_shapes(component, self._params.get_param("c_rm").value)
+        return build_ivc_xz_shapes(component, self._params.c_rm.value)
 
     def build_cryostat(self, component_tree: Component):
         """
