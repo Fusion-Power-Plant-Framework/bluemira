2dcb86aade933854d307bc11aa6c0c386f5a044c
CoronelBuendia <matti.coleman@gmail.com>
2022-03-31 18:10:04 +0200

    put back run

--- examples/design/EU-DEMO/EUDEMO_reactor.py
+++ examples/design/EU-DEMO/EUDEMO_reactor.py
@@ -222,7 +222,7 @@ build_config = {
         "runmode": "read",  # ["run", "read", "mock"]
     },
     "TF Coils": {
-        "runmode": "read",  # ["run", "read", "mock"]
+        "runmode": "run",  # ["run", "read", "mock"]
         "param_class": "TripleArc",
         "variables_map": {
             "x1": {
