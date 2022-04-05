ccde978aa067ad15fb1cd6fd029575e4d4421529
CoronelBuendia <matti.coleman@gmail.com>
2022-04-05 14:45:47 +0200

    whoops

--- tests/BLUEPRINT/systems/test_physicstoolbox.py
+++ tests/BLUEPRINT/systems/test_physicstoolbox.py
@@ -57,13 +57,3 @@ class TestTE:
         kappa = 1.85
         te = IPB98y2(i_p, b_t, p_sep, n19, r_0, a, kappa)
         assert np.isclose(te, 3.7, rtol=1e-3), f"{te:2f}"
-
-
-class TestKappaLaw:
-    def test_kappa(self):
-        k95 = estimate_kappa95(3.6, 0.3)
-        assert np.isclose(k95, 1.58, rtol=5e-2)
-        k95 = estimate_kappa95(3.1, 0.3)
-        assert np.isclose(k95, 1.67, rtol=5e-2)
-        k95 = estimate_kappa95(2.6, 0.3)
-        assert np.isclose(k95, 1.725, rtol=5e-2)
