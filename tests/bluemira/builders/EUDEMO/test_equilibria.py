83fd986ca197c7fc9642e428e386b07a2869ec28
CoronelBuendia <matti.coleman@gmail.com>
2022-04-05 14:56:26 +0200

    why did flake8 let me commit?

--- tests/bluemira/builders/EUDEMO/test_equilibria.py
+++ tests/bluemira/builders/EUDEMO/test_equilibria.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
 
+import numpy as np
 import pytest
 
 from bluemira.builders.EUDEMO.equilibria import estimate_kappa95
