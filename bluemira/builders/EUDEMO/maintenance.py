a50e5127dade4839145f4d87fc946fee68aaee11
CoronelBuendia <matti.coleman@gmail.com>
2022-04-05 17:07:33 +0200

    works with COBYLA

--- bluemira/builders/EUDEMO/maintenance.py
+++ bluemira/builders/EUDEMO/maintenance.py
@@ -19,22 +19,128 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
 
+from typing import List
+
+import numpy as np
+
+from bluemira.base.constants import EPS
+from bluemira.geometry.face import BluemiraFace
 from bluemira.geometry.parameterisations import GeometryParameterisation
-from bluemira.geometry.tools import boolean_cut, make_polygon
+from bluemira.geometry.placement import BluemiraPlacement
+from bluemira.geometry.tools import boolean_cut, make_polygon, slice_shape
 from bluemira.utilities.opt_problems import (
     OptimisationConstraint,
     OptimisationObjective,
     OptimisationProblem,
 )
-
-
-class UpperPortCut(GeometryParameterisation):
-    def __init__(
-        self,
-    ):
-        pass
+from bluemira.utilities.opt_variables import BoundedVariable, OptVariables
+from bluemira.utilities.optimiser import Optimiser, approx_derivative
 
 
 class UpperPortOP(OptimisationProblem):
-    def __init__():
-        pass
+    def __init__(
+        self, parameterisation, optimiser: Optimiser, breeding_blanket_xz: BluemiraFace
+    ):
+
+        objective = OptimisationObjective(self.minimise_port_size, f_objective_args={})
+
+        box = breeding_blanket_xz.bounding_box
+        r_ib_min = box.x_min
+        r_ob_max = box.x_max
+
+        constraints = [
+            OptimisationConstraint(
+                self.constrain_blanket_cut,
+                f_constraint_args={
+                    "bb": breeding_blanket_xz,
+                    "c_rm": 0.02,
+                    "r_ib_min": r_ib_min,
+                    "r_ob_max": r_ob_max,
+                },
+                tolerance=1e-6 * np.ones(3),
+            )
+        ]
+        super().__init__(parameterisation, optimiser, objective, constraints)
+        lower_bounds = [r_ib_min + 0.5, 9, r_ib_min + 1, 0]
+        upper_bounds = [9, r_ob_max - 0.5, r_ob_max - 1, 30]
+        self.set_up_optimiser(4, bounds=[lower_bounds, upper_bounds])
+
+    @staticmethod
+    def minimise_port_size(vector, grad):
+        ri, ro, ci, gamma = vector
+        value = ro - ri
+        if grad.size > 0:
+            grad[0] = -1
+            grad[1] = 1
+            grad[2] = 0
+            grad[3] = 0
+        return value
+
+    @staticmethod
+    def get_outer_cut_point(vector, bb):
+        ri, ro, ci, gamma = vector
+        cut_plane = BluemiraPlacement.from_3_points([ci, 0, 0], [ci, 0, 1], [ci, 1, 1])
+        # Get the first intersection with the vertical inner cut plane
+        intersections = slice_shape(bb.boundary[0], cut_plane)
+        intersections = intersections[intersections[:, -1] > 0.0]
+        intersection = sorted(intersections, key=lambda x: x[-1])[0]
+
+        x, y, z = intersection
+        x2 = x - np.sin(np.deg2rad(gamma))
+        y2 = y
+        z2 = z + np.cos(np.deg2rad(gamma))
+        angled_cut_plane = BluemiraPlacement.from_3_points(
+            intersection, [x2, y2, z2], [x, y + 1, z]
+        )
+        intersections = slice_shape(bb.boundary[0], angled_cut_plane)
+        intersections = intersections[intersections[:, -1] > z + EPS]
+        intersection = sorted(intersections, key=lambda x: x[-1])[0]
+        co, _, _ = intersection
+        return co
+
+    @staticmethod
+    def calculate_constraints(vector, bb, c_rm, r_ib_min, r_ob_max):
+        ri, ro, ci, gamma = vector
+
+        co = UpperPortOP.get_outer_cut_point(vector, bb)
+
+        return [
+            (r_ob_max - ci + 2 * c_rm) - (ro - co),
+            (ci - r_ib_min + 2 * c_rm) - (ro - ri),
+            (ro - ci + 2 * c_rm) - (ci - r_ib_min),
+        ]
+
+    @staticmethod
+    def constrain_blanket_cut(constraint, vector, grad, bb, c_rm, r_ib_min, r_ob_max):
+
+        constraint[0] = UpperPortOP.calculate_constraints(
+            vector, bb, c_rm, r_ib_min, r_ob_max
+        )
+
+        if grad.size > 0:
+            grad[:] = approx_derivative(
+                UpperPortOP.calculate_constraints,
+                vector,
+                f0=constraint,
+                args=(bb, c_rm, r_ib_min, r_ob_max),
+            )
+
+        return constraint
+
+    def optimise(self, x0):
+        return self.opt.optimise(x0)
+
+
+if __name__ == "__main__":
+
+    bb = make_polygon(
+        {"x": [5, 6, 6, 11, 11, 12, 12, 5], "y": 0, "z": [-5, -5, 5, 5, -5, -5, 6, 6]},
+        closed=True,
+    )
+    bb = BluemiraFace(bb)
+    optimiser = Optimiser("SLSQP", opt_conditions={"max_eval": 1000, "ftol_rel": 1e-8})
+
+    parameterisation = np.array([])
+    design_problem = UpperPortOP(parameterisation, optimiser, bb)
+
+    result = design_problem.optimise([7, 10, 9, 0])
