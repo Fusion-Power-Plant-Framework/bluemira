1d3fb7fadeec11b3b0f1128d724b5fa1d6a4e854
CoronelBuendia <matti.coleman@gmail.com>
2022-04-08 12:57:11 +0200

    put back read

--- examples/design/EU-DEMO/EUDEMO_reactor.py
+++ examples/design/EU-DEMO/EUDEMO_reactor.py
@@ -222,7 +222,7 @@ build_config = {
         "runmode": "mock",  # ["run", "read", "mock"]
     },
     "Plasma": {
-        "runmode": "run",  # ["run", "read", "mock"]
+        "runmode": "read",  # ["run", "read", "mock"]
     },
     "TF Coils": {
         "runmode": "run",  # ["run", "read", "mock"]
