b745c4aa384edcb46486a8c41adae743f4148f47
CoronelBuendia <matti.coleman@gmail.com>
2022-04-14 10:30:34 +0200

    put back process mapping

--- bluemira/codes/process/mapping.py
+++ bluemira/codes/process/mapping.py
@@ -22,14 +22,12 @@
 """
 PROCESS mappings
 """
-from bluemira.base.parameter import ParameterMapping
 from bluemira.codes.utilities import Model, create_mapping
 
 
 class CurrentDriveEfficiencyModel(Model):
     """
     Switch for current drive efficiency model:
-
     1 - Fenstermacher Lower Hybrid
     2 - Ion Cyclotron current drive
     3 - Fenstermacher ECH
@@ -41,7 +39,6 @@ class CurrentDriveEfficiencyModel(Model):
     10 - ECRH user input gamma
     11 - ECRH "HARE" model (E. Poli, Physics of Plasmas 2019)
     12 - EBW user scaling input. Scaling (S. Freethy)
-
     PROCESS variable name: "iefrf"
     """
 
@@ -61,11 +58,9 @@ class CurrentDriveEfficiencyModel(Model):
 class TFCoilConductorTechnology(Model):
     """
     Switch for TF coil conductor model:
-
     0 - copper
     1 - superconductor
     2 - Cryogenic aluminium
-
     PROCESS variable name: "i_tf_sup"
     """
 
@@ -75,95 +70,23 @@ class TFCoilConductorTechnology(Model):
 
 
 IN_mappings = {
-    "P_el_net": ParameterMapping("pnetelin", False, True),
-    "P_el_net_process": ParameterMapping("pnetelmw", True, False),
-    "tau_flattop": ParameterMapping("tburn", False, False),
-    "n_TF": ParameterMapping("n_tf", False, True),
-    "TF_ripple_limit": ParameterMapping("ripmax", False, True),
-    "A": ParameterMapping("aspect", True, True),
-    "R_0": ParameterMapping("rmajor", True, False),
-    "B_0": ParameterMapping("bt", True, False),
-    "q_95": ParameterMapping("q95", False, False),
-    "kappa_95": ParameterMapping("kappa95", True, False),
-    "kappa": ParameterMapping("kappa", True, False),
-    "delta_95": ParameterMapping("triang95", True, False),
-    "delta": ParameterMapping("triang", True, False),
-    "T_e": ParameterMapping("te", False, False),
-    "Z_eff": ParameterMapping("zeff", False, False),
-    "V_p": ParameterMapping("vol", False, False),
-    "l_i": ParameterMapping("rli", False, False),
-    "I_p": ParameterMapping("plascur/1d6", True, False),
-    "P_fus": ParameterMapping("powfmw", True, False),
-    "P_fus_DT": ParameterMapping("pdt", True, False),
-    "P_fus_DD": ParameterMapping("pdd", True, False),
-    "H_star": ParameterMapping("hfact", True, False),
-    "P_sep": ParameterMapping("pdivt", True, False),
-    "P_rad_core": ParameterMapping("pcoreradmw", True, False),
-    "P_rad_edge": ParameterMapping("pedgeradmw", True, False),
-    "P_rad": ParameterMapping("pradmw", True, False),
-    "P_line": ParameterMapping("plinepv*vol", True, False),
-    "P_sync": ParameterMapping("psyncpv*vol", True, False),
-    "P_brehms": ParameterMapping("pbrempv*vol", True, False),
-    "f_bs": ParameterMapping("bootipf", True, False),
-    "beta_p": ParameterMapping("betap", True, False),
-    "beta": ParameterMapping("beta", True, False),
-    "tau_e": ParameterMapping("taueff", True, False),
-    "v_burn": ParameterMapping("vburn", True, False),
-    "C_Ejima": ParameterMapping("gamma", False, True),
-    "f_ni": ParameterMapping("faccd", False, False),
-    "e_nbi": ParameterMapping("enbeam", False, True),
-    "P_hcd_ss": ParameterMapping("pinjalw", False, True),
-    "eta_nb": ParameterMapping("etanbi", False, True),
-    "e_mult": ParameterMapping("emult", False, True),
-    "bb_t_inlet": ParameterMapping("t_in_bb", False, True),
-    "bb_t_outlet": ParameterMapping("t_out_bb", False, True),
-    # TODO: What about water?
-    "bb_p_outlet": ParameterMapping("p_he", False, True),
-    # TODO: PROCESS uses dP as an input, not outlet-inlet
-    "bb_pump_eta_el": ParameterMapping("etahtp", False, True),
-    "bb_pump_eta_isen": ParameterMapping("etaiso", False, True),
-    "tk_bb_ib": ParameterMapping("blnkith", True, True),
-    "tk_bb_ob": ParameterMapping("blnkoth", True, True),
-    "tk_sh_in": ParameterMapping("shldith", True, True),
-    "tk_sh_out": ParameterMapping("shldoth", False, True),
-    "tk_sh_top": ParameterMapping("shldtth", False, True),
-    "tk_sh_bot": ParameterMapping("shldlth", False, True),
-    "tk_vv_in": ParameterMapping("d_vv_in", True, True),
-    "tk_vv_out": ParameterMapping("d_vv_out", False, True),
-    "tk_vv_top": ParameterMapping("d_vv_top", False, True),
-    "tk_vv_bot": ParameterMapping("d_vv_bot", False, True),
-    "tk_sol_ib": ParameterMapping("scrapli", True, True),
-    "tk_sol_ob": ParameterMapping("scraplo", True, True),
-    "tk_ts": ParameterMapping("thshield", True, True),
-    "tk_cr_vv": ParameterMapping("ddwex", False, True),
-    "tk_fw_in": ParameterMapping("fwith", True, False),
-    "tk_fw_out": ParameterMapping("fwoth", True, False),
-    "tk_tf_inboard": ParameterMapping("tfcth", True, False),
-    "tk_tf_outboard": ParameterMapping("tfthko", False, False),
-    "tk_tf_nose": ParameterMapping("thkcas", True, False),
-    "tk_tf_wp": ParameterMapping("dr_tf_wp", True, False),
-    "tk_tf_front_ib": ParameterMapping("casthi", False, True),
-    "tk_tf_side": ParameterMapping("casths", False, True),
-    "tf_wp_depth": ParameterMapping("wwp1", False, False),
-    "tk_cs": ParameterMapping("ohcth", True, False),
-    "sigma_tf_max": ParameterMapping("alstrtf", False, False),
-    "h_cp_top": ParameterMapping("h_cp_top", False, False),
-    "h_tf_max_in": ParameterMapping("hmax", False, False),
-    "r_cp_top": ParameterMapping("r_cp_top", True, False),
-    "r_cs_in": ParameterMapping("bore", True, False),
-    "r_tf_in": ParameterMapping("rtfin", True, False),
-    "r_tf_inboard_out": ParameterMapping("r_tf_inboard_out", False, False),
-    "r_tf_in_centre": ParameterMapping("r_tf_inboard_mid", True, False),
-    "r_ts_ib_in": ParameterMapping("r_ts_ib_in", True, False),
-    "r_vv_ib_in": ParameterMapping("r_vv_ib_in", True, False),
-    "r_fw_ib_in": ParameterMapping("r_fw_ib_in", True, False),
-    "r_fw_ob_in": ParameterMapping("r_fw_ob_in", True, False),
-    "r_vv_ob_in": ParameterMapping("r_vv_ob_in", True, False),
-    "r_tf_out_centre": ParameterMapping("r_tf_outboard_mid", True, False),
-    "g_vv_ts": ParameterMapping("gapds", True, False),
-    "g_cs_tf": ParameterMapping("gapoh", True, True),
-    "g_ts_tf": ParameterMapping("tftsgap", True, True),
-    "g_vv_bb": ParameterMapping("vvblgap", True, True),
+    "P_el_net": ("pnetelin", "MW"),
+    "n_TF": ("n_tf", "dimensionless"),
+    "TF_ripple_limit": ("ripmax", "%"),
+    "C_Ejima": ("gamma", "dimensionless"),
+    "e_nbi": ("enbeam", "keV"),
+    "P_hcd_ss": ("pinjalw", "MW"),
+    "eta_nb": ("etanbi", "dimensionless"),
+    "bb_e_mult": ("emult", "dimensionless"),
+    "tk_sh_out": ("shldoth", "m"),
+    "tk_sh_top": ("shldtth", "m"),
+    "tk_sh_bot": ("shldlth", "m"),
+    "tk_vv_out": ("d_vv_out", "m"),
+    "tk_vv_top": ("d_vv_top", "m"),
+    "tk_vv_bot": ("d_vv_bot", "m"),
+    "tk_cr_vv": ("ddwex", "m"),
+    "tk_tf_front_ib": ("casthi", "m"),
+    "tk_tf_side": ("casths", "m"),
 }
 
 OUT_mappings = {
