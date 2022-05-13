6fd546180a553449c99727e353e5251899c74189
James <james.cook1@ukaea.uk>
2022-05-13 14:48:10 +0100

    🎨 more robust checks

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -11,24 +11,23 @@ fi
 BM_INSTALLED=$(pip list | grep bluemira | awk 'NR==1 {print $1}')
 [ "$BM_INSTALLED" = "bluemira" ] || { echo "bluemira not found in the current python environment, exiting" && exit 1; }
 
-# check we are in the bluemira git repo
-[ $(git rev-parse --is-inside-work-tree) = "true" ] ||  { echo "Not in bluemira git repo, exiting" && exit 1; }
+readonly BLUEMIRA_ROOT="$(realpath "$(dirname "$0")"/..)"
+echo 'Bluemira directory is '$BLUEMIRA_ROOT
 
-CURRENT_REPO=$(git remote get-url origin)
-REPO_GIT_URL="git@github.com:Fusion-Power-Plant-Framework/bluemira.git"
-REPO_HTTPS_URL="https://github.com/Fusion-Power-Plant-Framework/bluemira.git"
-[ "$CURRENT_REPO" = "$REPO_GIT_URL" ] || [ "$CURRENT_REPO" = "$REPO_HTTPS_URL" ] || { echo "Not in bluemira git repo, exiting" && exit 1; }
-
+OLD_DIR=$(pwd)
+cd $BLUEMIRA_ROOT
 # check that the requirements files havent changed
-git diff --exit-code requirements.txt || { echo "requirements.txt modified on this branch, exiting" && exit 1; }
-git diff --exit-code requirements-develop.txt || { echo "requirements-develop.txt modified on this branch, exiting" && exit 1; }
+git diff --exit-code $BLUEMIRA_ROOT"/requirements.txt" || { cd $OLD_DIR && echo "requirements.txt modified on this branch, exiting" && exit 1; }
+git diff --exit-code requirements-develop.txt || { cd $OLD_DIR && echo "requirements-develop.txt modified on this branch, exiting" && exit 1; }
 
 # checkout dependencies from develop
 git checkout $REQ_BRANCH requirements.txt requirements-develop.txt
 
 # install dependencies
-pip install -r requirements.txt || { echo "pip update failed" && exit 1; }
-pip install -r requirements-develop.txt || { echo "pip update failed" && exit 1; }
+pip install -r requirements.txt || { cd $OLD_DIR &&  echo "pip update failed" && exit 1; }
+pip install -r requirements-develop.txt || { cd $OLD_DIR &&  echo "pip update failed" && exit 1; }
 
 # revert checkout of dependencies files
 git checkout $(git rev-parse --abbrev-ref HEAD) requirements.txt requirements-develop.txt
+
+cd $OLD_DIR
