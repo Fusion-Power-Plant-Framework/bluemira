f19a608f1dbceec35fb884cc41ed15834c745873
James <james.cook1@ukaea.uk>
2022-05-16 08:15:51 +0100

    🚧 separate qt5 steps

--- scripts/qt5/install-qt5-deps.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-apt-get install -y \
-    build-essential perl python git '^libxcb.*-dev' libx11-xcb-dev \
-    libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev \
-    libxkbcommon-x11-dev libclang-6.0-dev llvm-6.0
