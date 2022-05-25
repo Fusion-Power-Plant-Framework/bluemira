40531355a57680a0eb8fce37b0fe932a16b3ab65
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-05-25 15:58:17 +0100

    Add docstring to version_checker script

--- scripts/version_checker.py
+++ scripts/version_checker.py
@@ -1,7 +1,12 @@
+"""
+Validate an input version string using the packaging.version module, and
+print the formatted version.
+"""
+
 from sys import argv
 
 from packaging import version
 
 v = version.Version(argv[1])
 
-print(f"{v.__str__()[1:] if v.__str__().startswith('v') else v.__str__()}")
+print(f"{str(v)[1:] if str(v).startswith('v') else v}")
