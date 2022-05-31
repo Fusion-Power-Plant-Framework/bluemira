c776c578ab2b2477ce09253b04610aa318582479
James <james.cook1@ukaea.uk>
2022-05-31 16:27:30 +0100

    🐛 missed two

--- setup.py
+++ setup.py
@@ -61,8 +61,8 @@ prominence = [
 ]
 
 pinned = [
-    "nlopt==2.7.0",
-    "numba==0.55.1",
+    "nlopt==2.7.1",
+    "numba==0.55.2",
     "numba-scipy==0.3.0",
     "numpy==1.22.4",
     "matplotlib==3.5.2",
