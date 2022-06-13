34afdb9928f60922d13397691ea4571254279065
ivanmaione <maione.ivan@gmail.com>
2022-06-13 17:58:03 +0200

    added fixed boundary equilibrium solver with plasmod

---
+++ bluemira/equilibria/fem_fixed_boundary/equilibrium.py
@@ -0,0 +1,253 @@
+# bluemira is an integrated inter-disciplinary design tool for future fusion
+# reactors. It incorporates several modules, some of which rely on other
+# codes, to carry out a range of typical conceptual fusion reactor design
+# activities.
+#
+# Copyright (C) 2021 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh, J. Morris,
+#                    D. Short
+#
+# bluemira is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# bluemira is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
+
+"""Fixed boundary equilibrium class"""
+import numpy as np
+
+from bluemira.base.look_and_feel import bluemira_debug
+from bluemira.equilibria.fem_fixed_boundary.fem_magnetostatic_2D import (
+    FemGradShafranovFixedBoundary,
+)
+from bluemira.equilibria.fem_fixed_boundary.transport_solver import (
+    PlasmodTransportSolver,
+)
+from bluemira.equilibria.fem_fixed_boundary.utilities import (
+    calculate_plasma_shape_params,
+    plot_profile,
+)
+from bluemira.mesh import meshing
+from bluemira.mesh.tools import import_mesh, msh_to_xdmf
+
+
+def solve_plasmod_fixed_boundary(
+    builder_plasma,
+    plasmod_options,
+    gs_options,
+    delta95_t,
+    kappa95_t,
+    lcar_coarse=0.15,
+    lcar_fine=0.05,
+    niter_max=30,
+    iter_err_max=1e-5,
+    theta=0.8,
+    gs_i_theta=0.5,
+):
+    """
+    Solve the plasma fixed boundary problem using delta95 and kappa95 as target
+    values and iterating on plasmod to have consistency with pprime and ffprime.
+
+    Paramters:
+        builder_plasma: bluemira.base.builder
+            plasma poloidal cross section builder object
+        plasmod_options: dict
+            set of options used to set up and run plasmod
+        gs_options: dict
+            set of options used to set up and run the FemGradShafranovFixedBoundary
+        delta95_t: float
+            target value for delta at 95%
+        kappa95_t: float
+            target value for kappa at 95%
+        lcar_coarse: float
+            value of the characteristic length used to generate the mesh to solve the
+            Grad-Shafranov problem
+        lcar_fine: float
+            value of the characteristic length used to extrapolate the isoflux for the
+            calculation of kappa and delta at 95%
+        niter_max: int
+            maximum number of iteration between Grad-Shafranov and Plasmod
+        iter_err_max: float
+            convergence maximum error to stop the iteration
+        theta: float
+            iteration relaxing factor
+        gs_i_theta: float
+            FemGradShafranovFixedBoundary iteration relaxing factor
+
+    Returns
+    -------
+        None
+        This function directly modifies the parameters of builder_plasma
+    """
+    niter = 0
+    delta_95 = delta95_t
+    kappa_95 = kappa95_t
+
+    while niter < niter_max:
+        # source string to be used in changed parameters
+        source = f"from equilibrium iteration {niter}"
+
+        # build the plasma
+        # - xz - plasma toroidal cross section
+        # - xyz - to get the volume
+        plasma = builder_plasma.build_xz().get_component("xz").get_component("LCFS")
+        plasma_volume = (
+            builder_plasma.build_xyz()
+            .get_component("xyz")
+            .get_component("LCFS")
+            .shape.volume
+        )
+
+        plasma.plot_options.show_faces = False
+        plasma.plot_2d(show=True)
+
+        kappa_u = builder_plasma.params.get_param("kappa_u")
+        kappa_l = builder_plasma.params.get_param("kappa_l")
+        delta_u = builder_plasma.params.get_param("delta_u")
+        delta_l = builder_plasma.params.get_param("delta_l")
+
+        kappa = (kappa_u + kappa_l) / 2
+        delta = (delta_u + delta_l) / 2
+
+        print(f"kappa_u: {kappa_u}, delta: {delta_u}")
+        print(f"kappa_l: {kappa_l}, delta: {delta_l}")
+        print(f"kappa: {kappa}, delta: {delta}")
+        print(f"volume: {plasma_volume}")
+
+        # initialize plasmod solver
+        # - V_p is set equal to plasma volume
+        plasmod_options["params"].set_parameter("V_p", plasma_volume, "m^3", source)
+
+        plasmod_options["params"].set_parameter("kappa", kappa, "dimensionless", source)
+        plasmod_options["params"].set_parameter("delta", delta, "dimensionless", source)
+        plasmod_options["params"].set_parameter(
+            "kappa_95", kappa_95, "dimensionless", source
+        )
+        plasmod_options["params"].set_parameter(
+            "delta_95", delta_95, "dimensionless", source
+        )
+
+        plasmod_solver = PlasmodTransportSolver(
+            params=plasmod_options["params"],
+            build_config=plasmod_options["build_config"],
+        )
+
+        plot_profile(
+            plasmod_solver.x, plasmod_solver.pprime(plasmod_solver.x), "pprime", "-"
+        )
+        plot_profile(
+            plasmod_solver.x, plasmod_solver.ffprime(plasmod_solver.x), "ffrime", "-"
+        )
+
+        # generate mesh for the Grad-Shafranov solver
+        plasma.shape.boundary[0].mesh_options = {
+            "lcar": lcar_coarse,
+            "physical_group": "lcfs",
+        }
+        plasma.shape.mesh_options = {
+            "lcar": lcar_coarse,
+            "physical_group": "plasma_face",
+        }
+
+        m = meshing.Mesh()
+        m(plasma)
+
+        msh_to_xdmf("Mesh.msh", dimensions=(0, 2), directory=".", verbose=True)
+
+        mesh, boundaries, subdomains, labels = import_mesh(
+            "Mesh",
+            directory=".",
+            subdomains=True,
+        )
+
+        # initialize the Grad-Shafranov solver
+        p_order = gs_options["p_order"]
+        gs_solver = FemGradShafranovFixedBoundary(mesh, p_order=p_order)
+
+        print("Solving Grad-Shafranov...")
+        # solve the Grad-Shafranov equation
+        gs_solver.solve(
+            plasmod_solver.pprime,
+            plasmod_solver.ffprime,
+            plasmod_solver.I_p,
+            tol=gs_options["tol"],
+            max_iter=gs_options["max_iter"],
+            i_theta=gs_i_theta,
+            verbose_plot=gs_options["verbose_plot"],
+        )
+
+        # create the finer mesh to calculate the isofluxes
+        plasma.shape.boundary[0].mesh_options = {
+            "lcar": lcar_fine,
+            "physical_group": "lcfs",
+        }
+        plasma.shape.mesh_options = {"lcar": lcar_fine, "physical_group": "plasma_face"}
+
+        m = meshing.Mesh()
+        m(plasma)
+
+        msh_to_xdmf("Mesh.msh", dimensions=(0, 2), directory=".", verbose=True)
+
+        mesh, boundaries, subdomains, labels = import_mesh(
+            "Mesh",
+            directory=".",
+            subdomains=True,
+        )
+
+        points = mesh.coordinates()
+        x2d_data = np.array([gs_solver.psi_norm_2d(x) for x in points])
+
+        # calculate kappa_95 and delta_95
+        r_geo, kappa_95, delta_95 = calculate_plasma_shape_params(
+            points, x2d_data, [np.sqrt(0.95)]
+        )
+        r_geo, kappa_95, delta_95 = r_geo[0], kappa_95[0], delta_95[0]
+
+        # calculate the iteration error
+        err_delta = abs(delta_95 - delta95_t) / delta95_t
+        err_kappa = abs(kappa_95 - kappa95_t) / kappa95_t
+        iter_err = max(err_delta, err_kappa)
+
+        # calculate the new kappa_u and delta_u
+        print("previous shape parameters")
+        kappa_u_0 = builder_plasma.params.get_param("kappa_u")
+        delta_u_0 = builder_plasma.params.get_param("delta_u")
+
+        print(f"{kappa_u_0}, {delta_u_0}")
+
+        kappa_u = theta * kappa_u_0 * (kappa95_t / kappa_95) + (1 - theta) * kappa_u_0
+        delta_u = theta * delta_u_0 * (delta95_t / delta_95) + (1 - theta) * delta_u_0
+
+        print("recalculated shape parameters")
+        print(f"{kappa_u}, {delta_u}")
+
+        print(" ")
+        print(f"bluemira delta95 = {delta_95}")
+        print(f"target delta95 = {delta95_t}")
+
+        print(f"|Target - bluemira|/Target = {err_delta}")
+
+        print(" ")
+        print(f"bluemira kappa95 = {kappa_95}")
+        print(f"target kappa95 = {kappa95_t}")
+
+        print(f"|Target - bluemira|/Target = {err_kappa}")
+
+        print(f"iter_err: {iter_err}, iter_err_max: {iter_err_max}")
+
+        if iter_err <= iter_err_max:
+            break
+
+        # increase iteration number
+        niter += 1
+
+        # update builder_plasma parameters
+        builder_plasma.params.kappa_u = kappa_u
+        builder_plasma.params.delta_u = delta_u
+        bluemira_debug(f"{builder_plasma.params}")
---
+++ examples/equilibria/fem_fixed_boundary/equilibrium_example.py
@@ -0,0 +1,134 @@
+# bluemira is an integrated inter-disciplinary design tool for future fusion
+# reactors. It incorporates several modules, some of which rely on other
+# codes, to carry out a range of typical conceptual fusion reactor design
+# activities.
+#
+# Copyright (C) 2021 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh, J. Morris,
+#                    D. Short
+#
+# bluemira is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# bluemira is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
+
+"""
+An example that shows how to set up the problem for the fixed boundary equilibrium.
+"""
+
+from bluemira.base.config import Configuration
+from bluemira.base.logs import set_log_level
+from bluemira.builders.plasma import MakeParameterisedPlasma
+from bluemira.equilibria.fem_fixed_boundary.equilibrium import (
+    solve_plasmod_fixed_boundary,
+)
+
+# ------------------------------------------------------------------------------
+set_log_level("INFO")
+# ------------------------------------------------------------------------------
+
+main_params = {
+    "R_0": 8.9830e00,
+    "A": 3.1,
+    "kappa_u": 1.6,
+    "kappa_l": 1.75,
+    "delta_u": 0.33,
+    "delta_l": 0.45,
+    "I_p": 19e6,
+    "B_0": 5.31,
+}
+
+build_config = {
+    "name": "Plasma",
+    "class": "MakeParameterisedPlasma",
+    "param_class": "bluemira.equilibria.shapes::JohnerLCFS",
+    "variables_map": {
+        "r_0": "R_0",
+        "a": "A",
+        "kappa_u": "kappa_u",
+        "kappa_l": "kappa_l",
+        "delta_u": "delta_u",
+        "delta_l": "delta_l",
+    },
+}
+
+Configuration.set_template_parameters([["kappa_u", "kappa_u", "", "dimensionless"]])
+Configuration.set_template_parameters([["kappa_l", "kappa_l", "", "dimensionless"]])
+Configuration.set_template_parameters([["delta_u", "delta_u", "", "dimensionless"]])
+Configuration.set_template_parameters([["delta_l", "delta_l", "", "dimensionless"]])
+Configuration.set_template_parameters([["kappa_95", "kappa_95", "", "dimensionless"]])
+Configuration.set_template_parameters([["delta_95", "delta_95", "", "dimensionless"]])
+
+builder_plasma = MakeParameterisedPlasma(main_params, build_config)
+
+new_params = {
+    "A": main_params["A"],
+    "R_0": main_params["R_0"],
+    "I_p": main_params["I_p"] / 1e6,
+    "B_0": main_params["B_0"],
+    "V_p": -2500,
+    "v_burn": -1.0e6,
+    "kappa_95": 1.652,
+    "delta_95": 0.333,
+    "delta": (main_params["delta_l"] + main_params["delta_u"]) / 2,
+    "kappa": (main_params["kappa_l"] + main_params["kappa_u"]) / 2,
+    "q_95": 3.25,
+    "f_ni": 0,
+}
+
+# plasmod options
+PLASMOD_PATH = "/home/ivan/Desktop/bluemira_project/plasmod/bin/"
+binary = f"{PLASMOD_PATH}plasmod"
+
+plasmod_params = Configuration(new_params)
+
+# Add parameter source
+for param_name in plasmod_params.keys():
+    if param_name in new_params:
+        param = plasmod_params.get_param(param_name)
+        param.source = "Plasmod Example"
+
+problem_settings = {
+    "amin": new_params["R_0"] / new_params["A"],
+    "pfus_req": 2000.0,
+    "pheat_max": 100.0,
+    "q_control": 50.0,
+    "i_impmodel": "PED_FIXED",
+    "i_modeltype": "GYROBOHM_2",
+    "i_equiltype": "q95_sawtooth",
+    "i_pedestal": "SAARELMA",
+}
+
+plasmod_build_config = {
+    "problem_settings": problem_settings,
+    "mode": "run",
+    "binary": binary,
+}
+
+plasmod_options = {"params": plasmod_params, "build_config": plasmod_build_config}
+gs_options = {"p_order": 2, "tol": 1e-4, "max_iter": 30, "verbose_plot": False}
+
+# target values
+delta95_t = 0.333
+kappa95_t = 1.652
+
+solve_plasmod_fixed_boundary(
+    builder_plasma,
+    plasmod_options,
+    gs_options,
+    delta95_t,
+    kappa95_t,
+    lcar_coarse=0.3,
+    lcar_fine=0.05,
+    niter_max=15,
+    iter_err_max=1e-5,
+    theta=1,
+    gs_i_theta=1,
+)
