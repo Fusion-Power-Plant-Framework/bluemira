623e6a6d244593e3bd9f5c3cc7f0c09029497c78
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-06-23 16:52:45 +0100

    Add ability to compare warning reports

--- scripts/format_warning_report.py
+++ scripts/format_warning_report.py
@@ -3,8 +3,9 @@
 import argparse
 import json
 from dataclasses import dataclass
-from typing import Dict, Iterable, List, Set
+from typing import Any, Dict, Iterable, List, Set, Tuple
 
+INDENT_SIZE = 2
 PROG = "format_warning_report"
 WARNINGS_KEY = "warnings"
 
@@ -18,43 +19,92 @@ class Warning:
     lineno: int
 
     def __str__(self) -> str:
-        return (
-            f"On {self.when}:\n"
-            f"  {self.filename}:{self.lineno}: {self.category}: {self.message}"
-        )
+        return f"{self.filename}:{self.lineno}: {self.category}: {self.message}"
 
 
 def parse_args(sys_args: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser(PROG)
     parser.add_argument("report_file", help="path to .report.json to parse")
+    parser.add_argument(
+        "--compare", default=None, help="path to .report.json to compare warnings to"
+    )
     return parser.parse_args(sys_args)
 
 
-def load_json(file_path: str) -> Dict:
+def load_warnings(file_path: str) -> Set[Warning]:
     with open(file_path, "r") as f:
-        return json.load(f)
+        data = json.load(f)
+    return set([Warning(**warning) for warning in data[WARNINGS_KEY]])
 
 
-def parse_warning_list(warning_data: Iterable[Dict]) -> Set[Warning]:
-    # Convert to set to remove duplicates
-    return set([Warning(**warning) for warning in warning_data])
-
-
-def display_warnings(warnings: Set[Warning]):
-    print(f"{PROG}: found {len(warnings)} warnings.")
+def format_warnings_markdown(warnings: Iterable[Warning]):
+    whens: Dict[str, List[Warning]] = {}
     for warning in warnings:
-        print(warning)
+        try:
+            whens[warning.when].append(warning)
+        except KeyError:
+            whens[warning.when] = [warning]
+    markdown = ""
+    for when, warns in whens.items():
+        markdown += f"## On {when}\n\n"
+        for warn in warns:
+            markdown += f"- `{warn}`\n"
+        markdown += "\n"
+    return markdown[:-1]
 
 
-def format_warning_report(sys_args: List[str]) -> int:
+def make_collapsable_md(markdown: str, summary: str):
+    lines = markdown.split("\n")
+    indented_lines = [f"{line}" for line in lines]
+    new_lines = (
+        [
+            "<details>\n",
+            f"{' '*INDENT_SIZE}<summary>{summary}</summary>\n",
+        ]
+        + indented_lines
+        + ["</details>"]
+    )
+    return "\n".join(new_lines)
+
+
+def elements_not_in(head: Iterable[Any], ref: Iterable[Any]) -> List:
+    not_in = []
+    for head_el in head:
+        if head_el not in ref:
+            not_in.append(head_el)
+    return not_in
+
+
+def compare_warnings(
+    head_warnings: Set[Warning], ref_warnings: Set[Warning]
+) -> Tuple[List[Warning], List[Warning]]:
+    new_warnings = elements_not_in(head_warnings, ref_warnings)
+    fixed_warnings = elements_not_in(ref_warnings, head_warnings)
+    return new_warnings, fixed_warnings
+
+
+def format_warning_report(sys_args: List[str]) -> str:
     inputs = parse_args(sys_args)
-    report = load_json(inputs.report_file)
-    warnings = parse_warning_list(report[WARNINGS_KEY])
-    display_warnings(warnings)
-    return 0
+    warnings = load_warnings(inputs.report_file)
+    report = ["# ⚠️ Warning Report\n"]
+    warning_list_md = format_warnings_markdown(warnings)
+    if inputs.compare is not None:
+        ref_warnings = load_warnings(inputs.compare)
+        new, fixed = compare_warnings(warnings, ref_warnings)
+        report.append(f"Found {len(new)} new warnings, {len(fixed)} fixed warnings.\n")
+        if new:
+            warning_list = format_warnings_markdown(new)
+            report.append(make_collapsable_md(warning_list, "New warnings"))
+    else:
+        report.append(f"Found {len(warnings)} warnings.\n")
+    if warnings:
+        report.append(
+            make_collapsable_md(warning_list_md, f"All warnings ({len(warnings)})")
+        )
+    return "\n".join(report)
 
 
 if __name__ == "__main__":
     import sys
 
-    format_warning_report(sys.argv[1:])
+    print(format_warning_report(sys.argv[1:]))
