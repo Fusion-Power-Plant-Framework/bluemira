5ea28a38b1c51da985d611b1f23c7bce19f756fc
James <james.cook1@ukaea.uk>
2022-07-06 11:43:15 +0100

    🚨 Remove mpl test warning about plotting without a gui backend

--- tests/equilibria/fem_fixed_boundary/test_fem_magnetostatic_2D.py
+++ tests/equilibria/fem_fixed_boundary/test_fem_magnetostatic_2D.py
@@ -26,6 +26,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import bluemira.geometry.tools as tools
+import tests
 from bluemira.base.components import Component, PhysicalComponent
 from bluemira.equilibria.fem_fixed_boundary.fem_magnetostatic_2D import (
     FemMagnetostatic2d,
@@ -99,7 +100,9 @@ class TestGetNormal:
             subdomains=True,
         )
         dolfin.plot(mesh)
-        plt.show()
+
+        if tests.PLOTTING:
+            plt.show()
 
         em_solver = FemMagnetostatic2d(mesh, boundaries, 3)
 
