df9f5c86fa88144cee231ba41954f2f5576629cc
James <james.cook1@ukaea.uk>
2022-07-08 10:40:34 +0100

    🔥 remove numba-scipy source install

--- scripts/hacks.sh
+++ scripts/hacks.sh
@@ -2,4 +2,3 @@ set -e
 
 pip install -i https://test.pypi.org/simple/ CoolProp==6.4.2.dev0
 pip install neutronics-material-maker==0.1.11
-pip install -U git+https://github.com/numba/numba-scipy.git
