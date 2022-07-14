6db372f4047e6ec3fa25d0e850ad23dc8d9b7989
CoronelBuendia <matti.coleman@gmail.com>
2022-07-14 13:59:06 +0200

    adjust Polyspline parameters more dynamically

--- examples/design/EU-DEMO/EUDEMO_builders/ivc/wall.py
+++ examples/design/EU-DEMO/EUDEMO_builders/ivc/wall.py
@@ -233,10 +233,37 @@ class WallBuilder(OptimisedShapeBuilder):
         """
         params = super()._derive_shape_params()
         if issubclass(self._param_class, PolySpline):
-            params["height"] = {"value": self._derive_polyspline_height()}
+            params["height"] = self._derive_polyspline_height_values()
+            params["x1"] = self._derive_polyspline_x1_values()
+            params["x2"] = self._derive_polyspline_x2_values()
         return params
 
-    def _derive_polyspline_height(self) -> float:
+    def _derive_polyspline_height_values(self) -> float:
         """Derive the PolySpline height from relevant parameters."""
-        r_minor = self._params.R_0 / self._params.A
-        return (self._params.kappa_95 * r_minor) * 2
+        r_minor = self._params.R_0.value / self._params.A.value
+        ref_height = (self._params.kappa_95.value * r_minor) * 2
+        return {
+            "value": ref_height,
+            "lower_bound": 0.8 * ref_height,
+            "upper_bound": 1.2 * ref_height,
+        }
+
+    def _derive_polyspline_x1_values(self) -> float:
+        """Derive the PolySpline x1 from relevant parameters."""
+        r_minor = self._params.R_0.value / self._params.A.value
+        ref_x1 = self._params.R_0.value - r_minor
+        return {
+            "value": ref_x1,
+            "lower_bound": 0.8 * ref_x1,
+            "upper_bound": 1.2 * ref_x1,
+        }
+
+    def _derive_polyspline_x2_values(self) -> float:
+        """Derive the PolySpline x2 from relevant parameters."""
+        r_minor = self._params.R_0.value / self._params.A.value
+        ref_x2 = self._params.R_0.value + r_minor
+        return {
+            "value": ref_x2,
+            "lower_bound": 0.8 * ref_x2,
+            "upper_bound": 1.2 * ref_x2,
+        }
