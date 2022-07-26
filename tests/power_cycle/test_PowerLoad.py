da57541ed7ba3f37be0c969738fa9992e3699851
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2022-07-26 17:05:11 +0200

    remove imported classes "imported_" names

--- tests/power_cycle/test_PowerLoad.py
+++ tests/power_cycle/test_PowerLoad.py
@@ -1,39 +1,39 @@
-# Import
+# Import general packages
 # import numpy as np
 from pprint import pprint
 
 import matplotlib.pyplot as plt
 
-# from scipy.interpolate import interp1d as imported_interp1d
-from _TIAGO_FILES_.Tools import Tools as imported_tools
+# Import Power Cycle packages
+from bluemira.power_cycle.base import PowerCycleUtilities
+from bluemira.power_cycle.loads import PowerData, PowerLoad
 
-from bluemira.power_cycle.base import PowerCycleUtilities as imported_utilities
-from bluemira.power_cycle.loads import PowerData as imported_class_1
-from bluemira.power_cycle.loads import PowerLoad as imported_class_2
+# from scipy.interpolate import interp1d as imported_interp1d
+
 
 # Header
-imported_tools.print_header("Test PowerLoad")
+PowerCycleUtilities.print_header("Test PowerLoad")
 
 # Test data
 name_1 = "test_1"
 time_1 = [0, 4, 7, 8]
 data_1 = [6, 9, 7, 8]
-load_1 = imported_class_1(name_1, time_1, data_1)
+load_1 = PowerData(name_1, time_1, data_1)
 model_1 = "ramp"
 name_2 = "test_2"
 time_2 = [2, 5, 7, 9, 10]
 data_2 = [2, 2, 2, 4, 4]
-load_2 = imported_class_1(name_2, time_2, data_2)
+load_2 = PowerData(name_2, time_2, data_2)
 model_2 = "step"
 
 # Create instances of PowerLoad
-instance_1 = imported_class_2("Test 1", load_1, model_1)
-instance_2 = imported_class_2("Test 2", load_2, model_2)
+instance_1 = PowerLoad("Test 1", load_1, model_1)
+instance_2 = PowerLoad("Test 2", load_2, model_2)
 
 # Test `_refine_vector` method
-refined_time_1 = imported_class_2._refine_vector(time_1, 3)
+refined_time_1 = PowerLoad._refine_vector(time_1, 3)
 print(refined_time_1)
-refined_time_2 = imported_class_2._refine_vector(time_2, 0)
+refined_time_2 = PowerLoad._refine_vector(time_2, 0)
 print(refined_time_2)
 
 # Test visualization method
@@ -41,7 +41,7 @@ plt.figure()
 plt.grid()
 instance_1.plot(c="r")
 instance_2.plot(c="b")
-imported_utilities.adjust_2d_graph_ranges()
+PowerCycleUtilities.adjust_2d_graph_ranges()
 
 # Test addition method
 plt.figure()
