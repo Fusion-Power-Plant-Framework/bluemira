0b222a56d6aef55c4996969345e1518c3f7e5cbe
CoronelBuendia <matti.coleman@gmail.com>
2022-09-21 15:23:43 +0200

    units

--- bluemira/equilibria/fem_fixed_boundary/utilities.py
+++ bluemira/equilibria/fem_fixed_boundary/utilities.py
@@ -348,7 +348,7 @@ def calculate_plasma_shape_params(
 
 def find_psi_axis(psi_func, mesh):
     """
-    Find the magnetic axis in the poloidal flux map
+    Find the magnetic axis in the poloidal flux map.
     Parameters
     ----------
     psi_func: callable
@@ -358,7 +358,7 @@ def find_psi_axis(psi_func, mesh):
     Returns
     -------
     psi_axis: float
-        Maximum psi in the continuous psi function
+        Maximum psi in the continuous psi function [V.s]
     """
     points = mesh.coordinates()
     psi_array = [psi_func(x) for x in points]
