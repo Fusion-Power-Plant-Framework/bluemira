9d738073c59ee1b2d77c10432a5ed35aeb75cfed
CoronelBuendia <matti.coleman@gmail.com>
2022-09-23 15:05:40 +0200

    fix mysterious warnings again

--- bluemira/equilibria/fem_fixed_boundary/utilities.py
+++ bluemira/equilibria/fem_fixed_boundary/utilities.py
@@ -270,38 +270,7 @@ def find_flux_surface_precise_OLD(psi_norm_func, mesh, psi_norm, xtol=1e-7):
     return x, z
 
 
-def find_flux_surface_no_mesh(psi_norm_func, psi_norm, n_points=100):
-    """ """
-    x_axis, z_axis = find_magnetic_axis(lambda x: -psi_norm_func(x), None)
-    theta = np.linspace(0, 2 * np.pi, n_points - 1, endpoint=False)
-
-    def psi_norm_match(x):
-        return abs(psi_norm_func(x) - psi_norm)
-
-    points = np.zeros((2, n_points), dtype=float)
-    d0 = 0.5
-    for i in range(len(theta)):
-
-        def theta_line(d):
-            return float(x_axis + d * np.cos(theta[i])), float(
-                z_axis + d * np.sin(theta[i])
-            )
-
-        result = scipy.optimize.minimize(
-            lambda d: psi_norm_match(theta_line(d)),
-            x0=d0,
-            bounds=[(0.1, None)],
-            method="SLSQP",
-            options={"disp": False, "ftol": 1e-14, "maxiter": 3000},
-        )
-        points[:, i] = theta_line(result.x)
-        d0 = result.x
-
-    points[:, -1] = points[:, 0]
-    return points
-
-
-def find_flux_surface_precise(psi_norm_func, mesh, psi_norm, n_points=100):
+def find_flux_surface(psi_norm_func, psi_norm, mesh=None, n_points=100):
     """
     Find a flux surface in the psi function precisely.
 
@@ -334,15 +303,15 @@ def find_flux_surface_precise(psi_norm_func, mesh, psi_norm, n_points=100):
     def psi_norm_match(x):
         return abs(psi_norm_func(x) - psi_norm)
 
-    points = np.zeros((2, n_points))
+    theta = np.linspace(0, 2 * np.pi, n_points - 1, endpoint=False, dtype=float)
+    points = np.zeros((2, n_points), dtype=float)
+    distances = np.zeros(n_points)
     for i in range(len(theta)):
-        if i == 0:
-            d_guess = abs(np.max(contour[0, :]) - x_axis) - search_range
-        else:
-            d_guess = np.hypot(points[0, i - 1] - x_axis, points[1, i - 1] - z_axis)
 
         def theta_line(d):
-            return x_axis + d * np.cos(theta[i]), z_axis + d * np.sin(theta[i])
+            return float(x_axis + d * np.cos(theta[i])), float(
+                z_axis + d * np.sin(theta[i])
+            )
 
         result = scipy.optimize.minimize(
             lambda d: psi_norm_match(theta_line(d)),
@@ -355,16 +324,6 @@ def find_flux_surface_precise(psi_norm_func, mesh, psi_norm, n_points=100):
 
     points[:, -1] = points[:, 0]
 
-    mpoints = mesh.coordinates()
-    psi_norm_array = [psi_norm_func(x) for x in mpoints]
-
-    contour = get_tricontours(mpoints[:, 0], mpoints[:, 1], psi_norm_array, psi_norm)[0]
-    f, ax = plt.subplots()
-    ax.plot(*contour.T, color="b", marker="o", linestyle="--")
-    ax.plot(*points, color="r", marker="s")
-    ax.plot(x_axis, z_axis, marker="o", color="g")
-    ax.set_aspect("equal")
-    plt.show()
     return points
 
 
