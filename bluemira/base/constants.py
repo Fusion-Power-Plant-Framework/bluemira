33067f566c1be4b8438e53e8a6495438312838ac
James <james.cook1@ukaea.uk>
2022-10-06 08:11:45 +0100

    🐛 fix gas flow

--- bluemira/base/constants.py
+++ bluemira/base/constants.py
@@ -142,10 +142,12 @@ class BMUnitRegistry(UnitRegistry):
         return self._gas_flow_temperature
 
     @gas_flow_temperature.setter
-    def gas_flow_temperature(self, value: Union[float, Quantity]):
+    def gas_flow_temperature(self, value: Union[float, None, Quantity]):
         self._gas_flow_temperature = (
             value.to("kelvin")
             if isinstance(value, Quantity)
+            else value
+            if value is None
             else self.Quantity(value, "kelvin")
         )
 
