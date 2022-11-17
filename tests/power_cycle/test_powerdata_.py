f6dd85447908c0a05f8a7878e56bb959d5732f28
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2022-11-17 15:51:50 +0100

    Relocate base utilities, test files as lowercase

--- tests/power_cycle/test_powerdata_.py
+++ tests/power_cycle/test_powerdata_.py
@@ -4,11 +4,11 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 
 # Import Power Cycle packages
-from bluemira.power_cycle.base import PowerCycleUtilities
 from bluemira.power_cycle.loads import PowerData
+from bluemira.power_cycle.utilities import adjust_2d_graph_ranges, print_header
 
 # Header
-PowerCycleUtilities.print_header("Test PowerData")
+print_header("Test PowerData")
 
 # Test data
 test_name = "test_powerdata"
@@ -32,7 +32,7 @@ plt.figure()
 plt.grid()
 plot_list = test_instance.plot()
 pprint(plot_list)
-PowerCycleUtilities.adjust_2d_graph_ranges()
+adjust_2d_graph_ranges()
 
 # Show plots
 plt.show()
