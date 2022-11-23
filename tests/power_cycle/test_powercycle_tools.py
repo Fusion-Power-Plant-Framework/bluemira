4c9e7aea8202f68e601945913a1cc36f688f123a
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2022-11-23 11:47:53 +0100

    apply tools test review comments

--- tests/power_cycle/test_powercycle_tools.py
+++ tests/power_cycle/test_powercycle_tools.py
@@ -1,9 +1,9 @@
-from pprint import pprint
+from pprint import pformat
 
 import matplotlib.pyplot as plt
 import pytest
 
-from bluemira.base.look_and_feel import bluemira_print
+from bluemira.base.look_and_feel import bluemira_debug
 from bluemira.power_cycle.tools import (
     _add_dict_entries,
     _join_delimited_values,
@@ -11,14 +11,12 @@ from bluemira.power_cycle.tools import (
     validate_axes,
 )
 
-# Simple vectors for sample plotting
-sample_x = [1, 2]
-sample_y = [3, 4]
 
-bluemira_print("Test Power Cycle Tools")
+def script_title():
+    return "Test Power Cycle Tools"
 
 
-def test_add_dict_entries(visualize=None):
+def test_add_dict_entries():
     old_dict = {
         "value_1": 1,
         "value_2": 2,
@@ -29,68 +27,126 @@ def test_add_dict_entries(visualize=None):
         "value_4": 4,
     }
     new_dict = _add_dict_entries(old_dict, new_entries)
-    if visualize:
-        bluemira_print("Test Power Cycle Tools ('_add_dict_entries')")
-        pprint(old_dict)
-        pprint(new_entries)
-        pprint(new_dict)
+    bluemira_debug(
+        f"""
+        {script_title()} ('_add_dict_entries')
+
+        Old dictionary:
+        {pformat(old_dict)}
+
+        New dictionary entries:
+        {pformat(new_entries)}
+
+        New dictionary:
+        {pformat(new_dict)}
+        """
+    )
     assert isinstance(new_dict, dict)
     assert new_dict == {**old_dict, **new_entries}
 
 
-def test_join_delimited_values(visualize=None):
+def test_join_delimited_values():
     all_arguments = [
         ["value 1", "value 2", "value 3"],
         {"value_1": 1, "value_2": 2, "value_3": 3},
     ]
     for test_argument in all_arguments:
         string_from_argument = _join_delimited_values(test_argument)
-        if visualize:
-            bluemira_print("Test Power Cycle Tools " "('_join_delimited_values')")
-            print(string_from_argument)
+        bluemira_debug(
+            f"""
+            {script_title()} ('_join_delimited_values')
+
+            Generated string:
+            {string_from_argument}
+            """
+        )
     assert isinstance(string_from_argument, str)
 
 
-def test_validate_axes(visualize=None):
+class TestPlottingTools:
+    def setup_method(self):
+        self.sample_x = [1, 2]
+        self.sample_y = [3, 4]
 
-    sample_plot = plt.gca()
-    sample_plot = sample_plot.plot(sample_x, sample_y)
-    not_a_plot = sample_x
+    def teardown_method(self):
+        self.closer_all_plots()
 
-    all_axes = [
-        None,
-        sample_plot,
-        not_a_plot,
-    ]
-    for axes in all_axes:
+    @staticmethod
+    def closer_all_plots():
+        plt.close("all")
 
-        print(str(axes))
-        if (axes is None) or isinstance(axes, plt.Axes):
-            checked_axes = validate_axes(axes)
-            assert isinstance(checked_axes, plt.Axes)
-        else:
-            with pytest.raises(TypeError):
+    @staticmethod
+    def query_axes_limits(ax):
+        all_limits = []
+        all_axes = ["x", "y"]
+        for axis in all_axes:
+            if axis == "x":
+                axis_lim = ax.get_xlim()
+            elif axis == "y":
+                axis_lim = ax.get_ylim()
+            all_limits.append(axis_lim)
+        return all_limits
+
+    # ------------------------------------------------------------------
+    #  TESTS
+    # ------------------------------------------------------------------
+    def test_validate_axes(self):
+
+        sample_plot = plt.gca()
+        sample_plot = sample_plot.plot(self.sample_x, self.sample_y)
+        not_a_plot = self.sample_x
+
+        all_axes = [
+            None,
+            sample_plot,
+            not_a_plot,
+        ]
+        for axes in all_axes:
+            if (axes is None) or isinstance(axes, plt.Axes):
                 checked_axes = validate_axes(axes)
+                assert isinstance(checked_axes, plt.Axes)
+            else:
+                with pytest.raises(TypeError):
+                    checked_axes = validate_axes(axes)
 
-    if not visualize:
-        plt.close("all")
-    else:
-        bluemira_print("Test Power Cycle Tools ('validate_axes')")
+    def test_adjust_2d_graph_ranges(self):
+        all_scales = ["linear", "log"]
+        for scale in all_scales:
 
+            # Assert that output are still axes
+            test_plot = validate_axes()
+            test_plot.plot(self.sample_x, self.sample_y)
+            test_plot.set_xscale(scale)
+            test_plot.set_yscale(scale)
+            old_limits = self.query_axes_limits(test_plot)
+            adjust_2d_graph_ranges(ax=test_plot)
+            new_limits = self.query_axes_limits(test_plot)
+            assert isinstance(test_plot, plt.Axes)
 
-def test_adjust_2d_graph_ranges(visualize=None):
+            # Assert that range of each axes is increased
+            n_axes = len(old_limits)
+            bluemira_debug(
+                f"""
+                {script_title()} ('adjust_2d_graph_ranges')
 
-    all_scales = ["linear", "log"]
-    for scale in all_scales:
-        test_plot = validate_axes()
-        test_plot.plot(sample_x, sample_y)
-        test_plot.set_xscale(scale)
-        test_plot.set_yscale(scale)
-        adjust_2d_graph_ranges(ax=test_plot)
-        assert isinstance(test_plot, plt.Axes)
-        del test_plot
+                Old axes limits:
+                {pformat(old_limits)}
 
-    if not visualize:
-        plt.close("all")
-    else:
-        bluemira_print("Test Power Cycle Tools " "('adjust_2d_graph_ranges')")
+                New axes limits:
+                {pformat(new_limits)}
+                """
+            )
+            for axis_index in range(n_axes):
+                old_axis = old_limits[axis_index]
+                new_axis = new_limits[axis_index]
+
+                old_lower = old_axis[0]
+                new_lower = new_axis[0]
+                assert new_lower < old_lower
+
+                old_upper = old_axis[1]
+                new_upper = new_axis[1]
+                assert old_upper < new_upper
+
+            del test_plot
+            self.closer_all_plots()
