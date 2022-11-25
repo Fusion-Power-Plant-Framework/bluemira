b59a71dd7ce18439dce92a1cf97e4a75c0b3685f
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-11-25 13:35:29 +0000

    EUDEMO reactor starting point (#1658)
    * Port part of EUDEMO build for new architecture
    
    * Remove design/examples directory
    
    * Add TF coil builder to EUDEMO reactor
    
    * Delete accidentally created __init__ file
    
    * Add PF coil tools for eudemo
    
    * Use 'read' mode for TF Coil design
    
    This makes the reactor build much faster whilst I'm debugging other
    things!
    
    * Add designer for EUDEMO PF coils
    
    * Fix 'missing docstring' flake8 warning
    
    * Run eudemo package tests in CI
    
    * Add VV thermal shield to eudemo reactor
    
    * Add template for EUDEMO PF coil builder
    
    * Remove bad unit conversion in EUDEMO equilibrium
    
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>
    
    * Remove another bad unit conversion in EUDEMO equil.
    
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>
    
    * Fix TF coil parameter units in EUDEMO tests
    
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>
    
    * Fix flake8 warnings in EUDEMO PF coils builder
    
    * Add README to eudemo folder
    
    * Remove reference to examples/design in pre-commit
    
    * Fix path to data in EUDEMO ivc test
    
    * Rename eudemo/tests -> eudemo/eudemo_tests
    
    This gets around a module name clash (eudemo's tests and bluemira's
    tests) in pytest.
    
    Also fix a couple of file paths in the EUDEMO tests
    
    * Uncomment code in eudemo tools test
    
    This does cause an error, but it's an error that needs fixing. Better to
    get the error as a reminder to fix it, than to forget about it.
    
    * Fix path in .flake8 config
    
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>

---
+++ eudemo/eudemo_tests/test_blanket.py
@@ -0,0 +1,62 @@
+# bluemira is an integrated inter-disciplinary design tool for future fusion
+# reactors. It incorporates several modules, some of which rely on other
+# codes, to carry out a range of typical conceptual fusion reactor design
+# activities.
+#
+# Copyright (C) 2022 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh, J. Morris,
+#                    D. Short
+#
+# bluemira is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# bluemira is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
+from bluemira.geometry.face import BluemiraFace
+from bluemira.geometry.tools import make_polygon
+from eudemo.blanket import BlanketBuilder
+
+
+class TestDivertorBuilder:
+    @classmethod
+    def setup_class(cls):
+        cls.params = {
+            "n_bb_inboard": {"value": 2, "unit": "m"},
+            "n_bb_outboard": {"value": 3, "unit": "m"},
+            "c_rm": {"value": 0.02, "unit": "m"},
+            "n_TF": {"value": 12, "unit": ""},
+        }
+        cls.silhouette = BluemiraFace(
+            make_polygon(
+                [
+                    [1, 0, -2],
+                    [1, 0, 10],
+                    [5, 0, 10],
+                    [5, 0, -2],
+                    [4, 0, -2],
+                    [4, 0, 9],
+                    [2, 0, 9],
+                    [2, 0, -2],
+                ],
+                closed=True,
+            )
+        )
+
+    def test_components_and_segments(self):
+        builder = BlanketBuilder(self.params, {}, self.silhouette)
+        cryostat_ts = builder.build()
+
+        assert cryostat_ts.component().get_component("xz")
+        assert cryostat_ts.component().get_component("xy")
+
+        xyz = cryostat_ts.component().get_component("xyz")
+        assert xyz
+        assert len(xyz.leaves) == self.params["n_TF"]["value"] * (
+            self.params["n_bb_inboard"]["value"] + self.params["n_bb_outboard"]["value"]
+        )
