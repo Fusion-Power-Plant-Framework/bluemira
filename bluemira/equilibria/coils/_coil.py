🐛 Field calculation fixes

bluemira/equilibria/coils/_coil.py
@@ -37,6 +37,7 @@ import numpy as np
 
 from bluemira.base.constants import MU_0
 from bluemira.base.look_and_feel import bluemira_warn
+from bluemira.equilibria.coils._field import CoilFieldsMixin
 from bluemira.equilibria.constants import I_MIN, NBTI_B_MAX, NBTI_J_MAX, X_TOLERANCE
 from bluemira.equilibria.error import EquilibriaError
 from bluemira.equilibria.file import EQDSKInterface
@@ -121,7 +122,7 @@ class CoilNumber:
         return idx
 
 
-class Coil:
+class Coil(CoilFieldsMixin):
     def __init__(
         self,
         x: float,
@@ -160,6 +161,7 @@ class Coil:
         self._validate_size()
         if not self._flag_sizefix and None in (self.dx, self.dz):
             self._dx, self._dz = 0, 0
+            self._discretise()
 
     def __repr__(self):
         return (
@@ -283,7 +285,7 @@ class Coil:
 
     @current.setter
     def current(self, value: Union[str, CoilType]):
-        self._current = value
+        self._current = float(value)
 
     @j_max.setter
     def j_max(self, value: float):
@@ -330,12 +332,12 @@ class Coil:
         Possible improvement: multiple discretisations for different coils
 
         """
-        weighting = None
         self._quad_x = np.array([self.x])
         self._quad_z = np.array([self.z])
         self._quad_dx = np.array([self.dx])
         self._quad_dz = np.array([self.dz])
         self._quad_weighting = np.ones_like(self._quad_x)
+        self._einsum_str = "...j, ...j -> ..."
 
         if self.discretisation < 1:
             # How fancy do we want the mesh or just smaller rectangles?
@@ -402,7 +404,7 @@ class Coil:
             self._quad_dx = np.ones(x_sc.size) * sc_dx
             self._quad_dz = np.ones(x_sc.size) * sc_dz
 
-            self._quad_weighting = np.ones(x_sc.size)
+            self._quad_weighting = np.ones(x_sc.size) / x_sc.size
 
     def fix_size(self):
         """

bluemira/equilibria/coils/_field.py
@@ -52,7 +52,7 @@ from bluemira.magnetostatics.semianalytic_2d import semianalytic_Bx, semianalyti
 from bluemira.utilities.tools import consec_repeat_elem, is_num
 
 
-class CoilFieldsMixin:
+class CoilGroupFieldsMixin:
 
     __slots__ = (
         "_quad_dx",
@@ -90,8 +90,8 @@ class CoilFieldsMixin:
             greens_psi(
                 self._quad_x[None],
                 self._quad_z[None],
-                x[..., None],
-                z[..., None],
+                x[..., None, None],
+                z[..., None, None],
                 self._quad_dx[None],
                 self._quad_dz[None],
             ),
@@ -194,7 +194,10 @@ class CoilFieldsMixin:
         """
         x, z = np.ascontiguousarray(x), np.ascontiguousarray(z)
 
-        zero_coil_size = np.logical_or(np.isclose(self.dx, 0), np.isclose(self.dz, 0))
+        # if not wrapped in np.array the following if won't work for `Coil`
+        zero_coil_size = np.array(
+            np.logical_or(np.isclose(self.dx, 0), np.isclose(self.dz, 0))
+        )
 
         if False in zero_coil_size:
             # if dx or dz is not 0 and x,z inside coil
@@ -239,7 +242,7 @@ class CoilFieldsMixin:
         response = np.zeros_like(inside, dtype=float)
         for coil, (points, qx, qz, qw, cx, cz, cdx, cdz) in enumerate(
             zip(
-                inside.T,
+                np.moveaxis(inside, -1, 0),
                 self._quad_x,
                 self._quad_z,
                 self._quad_weighting,
@@ -250,15 +253,16 @@ class CoilFieldsMixin:
             )
         ):
             if np.any(~points):
-                response[~points, coil] = np.squeeze(
-                    greens_func(x[~points], z[~points], True, qx, qz, qw)
-                )
-            if np.any(points):
-                response[points, coil] = np.squeeze(
-                    semianalytic_func(x[points], z[points], True, cx, cz, cdx, cdz)
+                response[~points, coil] = greens_func(
+                    x[~points], z[~points], True, qx, qz, qw
                 )
 
-        return response
+            if np.any(points):
+                response[points, coil] = semianalytic_func(
+                    x[points], z[points], True, cx, cz, cdx, cdz
+                )
+
+        return np.squeeze(response)
 
     def _points_inside_coil(
         self,
@@ -334,15 +338,17 @@ class CoilFieldsMixin:
             _quad_z = self._quad_z
             _quad_weight = self._quad_weighting
 
-        return np.einsum(
-            self._einsum_str,
-            greens(
-                _quad_x[None],
-                _quad_z[None],
-                x[..., None],
-                z[..., None],
-            ),
-            _quad_weight[None],
+        return np.squeeze(
+            np.einsum(
+                self._einsum_str,
+                greens(
+                    _quad_x[None],
+                    _quad_z[None],
+                    x[..., None, None],
+                    z[..., None, None],
+                ),
+                _quad_weight[None],
+            )
         )
 
     def _unit_Bx_greens(
@@ -449,13 +455,15 @@ class CoilFieldsMixin:
             coil_dx = self.dx
             coil_dz = self.dz
 
-        return semianalytic(
-            coil_x[None],
-            coil_z[None],
-            x[..., None],
-            z[..., None],
-            d_xc=coil_dx[None],
-            d_zc=coil_dz[None],
+        return np.squeeze(
+            semianalytic(
+                coil_x[None],
+                coil_z[None],
+                x[..., None],
+                z[..., None],
+                d_xc=coil_dx[None],
+                d_zc=coil_dz[None],
+            )
         )
 
     def _unit_Bx_analytical(
@@ -577,22 +585,109 @@ class CoilFieldsMixin:
         return 2 * np.pi * pos[0] * np.array([Bz, -Bx]).T  # 1 cross B
 
 
-def get_max_current(dx, dz, j_max):
-    """
-    Get the maximum current in a coil cross-sectional area
+class CoilFieldsMixin(CoilGroupFieldsMixin):
+    def _points_inside_coil(
+        self,
+        x: Union[float, np.array],
+        z: Union[float, np.array],
+        *,
+        atol: float = X_TOLERANCE,
+    ):
+        """
+        Determine which points lie inside or on the coil boundary.
 
-    Parameters
-    ----------
-    dx: float
-        Coil half-width [m]
-    dz: float
-        Coil half-height [m]
-    j_max: float
-        Coil current density [A/m^2]
+        Parameters
+        ----------
+        x: Union[float, np.array]
+            The x coordinates to check
+        z: Union[float, np.array]
+            The z coordinates to check
+        atol: Optional[float]
+            Add an offset, to ensure points very near the edge are counted as
+            being on the edge of a coil
 
-    Returns
-    -------
-    max_current: float
-        Maximum current [A]
-    """
-    return abs(j_max * (4 * dx * dz))
+        Returns
+        -------
+        inside: np.array(dtype=bool)
+            The Boolean array of point indices inside/outside the coil boundary
+        """
+        x, z = np.ascontiguousarray(x)[..., None], np.ascontiguousarray(z)[..., None]
+
+        x_min, x_max = (
+            self.x - self.dx - atol,
+            self.x + self.dx + atol,
+        )
+        z_min, z_max = (
+            self.z - self.dz - atol,
+            self.z + self.dz + atol,
+        )
+        return (x >= x_min) & (x <= x_max) & (z >= z_min) & (z <= z_max)
+
+    def _combined_control(self, inside, x, z, greens_func, semianalytic_func):
+        """
+        Combine semianalytic and greens function calculation of magnetic field
+
+        Used for situation where there are calculation points both inside and
+        outside the coil boundaries.
+
+        Parameters
+        ----------
+        inside: np.ndarray[bool]
+            array of if the point is inside a coil
+        x,z: Union[float, int, np.array]
+            Points to calculate field at
+        greens_func: Callable
+            greens function
+        semianalytic_func: Callable
+            semianalytic function
+
+        Returns
+        -------
+        response: np.ndarray
+
+        """
+        response = np.zeros(inside.shape[:-1])
+        points = inside[:, :, 0]
+
+        if np.any(~points):
+            response[~points] = greens_func(x[~points], z[~points])
+
+        if np.any(points):
+            response[points] = semianalytic_func(x[points], z[points])
+
+        return response
+
+    def _unit_B_analytical(
+        self,
+        semianalytic,
+        x,
+        z,
+        *args,
+        **kwargs,
+    ):
+        """
+        Calculate radial magnetic field Bx response at (x, z) due to a unit
+        current using semi-analytic method.
+
+        Parameters
+        ----------
+        semianalytic: Callable
+            semianalytic function
+        x,z: Union[float, int, np.array]
+            Points to calculate field at
+
+        Returns
+        -------
+        response: np.ndarray
+
+        """
+        return super()._unit_B_analytical(
+            semianalytic,
+            x,
+            z,
+            split=True,
+            coil_x=np.array([self.x]),
+            coil_z=np.array([self.z]),
+            coil_dx=np.array([self.dx]),
+            coil_dz=np.array([self.dz]),
+        )

bluemira/equilibria/coils/_grouping.py
@@ -40,7 +40,7 @@ import numpy as np
 from bluemira.base.constants import MU_0
 from bluemira.base.look_and_feel import bluemira_warn
 from bluemira.equilibria.coils._coil import CoilType
-from bluemira.equilibria.coils._field import CoilFieldsMixin
+from bluemira.equilibria.coils._field import CoilGroupFieldsMixin
 from bluemira.equilibria.constants import I_MIN, NBTI_B_MAX, NBTI_J_MAX, X_TOLERANCE
 from bluemira.equilibria.error import EquilibriaError
 from bluemira.equilibria.file import EQDSKInterface
@@ -52,13 +52,13 @@ from bluemira.magnetostatics.greens import (
     greens_psi,
 )
 from bluemira.magnetostatics.semianalytic_2d import semianalytic_Bx, semianalytic_Bz
-from bluemira.utilities.tools import flatten_iterable, is_num
+from bluemira.utilities.tools import flatten_iterable, is_num, yintercept
 
 # from scipy.interpolate import RectBivariateSpline
 
 
-class CoilGroup(CoilFieldsMixin):
-    def __init__(self, *coils: Union[Coil, CoilGroup[Coil]]) -> None:
+class CoilGroup(CoilGroupFieldsMixin):
+    def __init__(self, *coils: Union[Coil, CoilGroup[Coil]]):
         self._coils = coils
         self._pad_discretisation(self.__list_getter("_quad_x"))
 
@@ -160,7 +160,7 @@ class CoilGroup(CoilFieldsMixin):
         self._pad_size = max_len - all_len
 
         self._einsum_str = (
-            "..., ...j -> ..." if all(self._pad_size == 0) else "...j, ...j -> ..."
+            "...j, ...ij -> ...i" if len(_to_pad) == 1 else "...ij, ...ij -> ...i"
         )
 
     def assign_material(
@@ -226,11 +226,19 @@ class CoilGroup(CoilFieldsMixin):
 
     @property
     def x_boundary(self) -> np.ndarray:
-        return self.__getter("x_boundary")
+        xb = self.__getter("x_boundary")
+        if self.n_coils > 1:
+            return xb.reshape(-1, 4)
+        else:
+            return xb
 
     @property
     def z_boundary(self) -> np.ndarray:
-        return self.__getter("z_boundary")
+        zb = self.__getter("z_boundary")
+        if self.n_coils > 1:
+            return zb.reshape(-1, 4)
+        else:
+            return zb
 
     @property
     def _current_radius(self) -> np.ndarray:

tests/equilibria/test_coils.py
@@ -40,6 +40,33 @@ from bluemira.equilibria.error import EquilibriaError
 from bluemira.equilibria.grid import Grid
 
 
+def callable_tester(f_callable, coils=1):
+    """
+    Checks that all different field calls (with different inputs) behave
+    as expected
+    """
+    # This should go without a hitch...
+    value = f_callable(8, 0)
+    v2 = f_callable(np.array(8), np.array(0))
+    v3 = f_callable(np.array([8]), np.array([0]))
+    assert np.allclose(v2, value)
+    assert np.allclose(v3, value)
+
+    # Now let's check iterables (X = 4 or 20 is off-grid)
+    # (Z = -10 or 10 off-grid)
+    x = np.array([4, 8, 20, 4, 8, 20, 4, 8, 20])
+    z = np.array([0, 0, 0, 10, 10, 10, -10, -10, 10])
+
+    b = np.zeros((len(z), coils))
+
+    b1 = f_callable(x, z)
+
+    for i, (xi, zi) in enumerate(zip(x, z)):
+        b[i] = f_callable(xi, zi)
+
+    assert np.allclose(b.flat, b1.flat)
+
+
 class TestCoil:
     @classmethod
     def setup_class(cls):
@@ -48,8 +75,8 @@ class TestCoil:
         cls.cs_coil = Coil(x=4, z=4, current=10e6, ctype="CS", j_max=NBTI_J_MAX)
         cls.no_coil = Coil(x=4, z=4, current=10e6, ctype="NONE", j_max=NBTI_J_MAX)
 
-    @classmethod
-    def teardown_cls(cls):
+    def teardown_method(self):
+        plt.show()
         plt.close("all")
 
     def test_name(self):
@@ -84,7 +111,7 @@ class TestCoil:
         assert np.round(abs(c.Bx(0.001, z) - Bx), 4) == 0
         assert np.round(abs(c.Bz(0.001, z) - Bz), 5) == 0
         psi_single = c.psi(15, 15)
-        c.discretise(0.1)
+        c.discretisation = 0.1
         assert np.round(abs(c.Bx(0.001, z) - Bx), 4) == 0
         assert np.round(abs(c.Bz(0.001, z) - Bz), 3) == 0
         psi_multi = c.psi(15, 15)
@@ -100,10 +127,10 @@ class TestCoil:
         x, z = np.meshgrid(x_1_d, z_1_d, indexing="ij")
         c = Coil(x=4, z=0, current=1591550, dx=0.3, dz=1)
 
-        gbx = c.control_Bx(x, z)
-        gbz = c.control_Bz(x, z)
+        gbx = c.unit_Bx(x, z)
+        gbz = c.unit_Bz(x, z)
         _ = np.sqrt(gbx**2 + gbz**2)
-        _ = c.control_psi(x, z)
+        _ = c.unit_psi(x, z)
 
         _, ax = plt.subplots()
         cc = ax.contourf(x, z, gbx)
@@ -113,12 +140,12 @@ class TestCoil:
         ax.set_xlim([2, 6])
         ax.set_ylim([-3, 3])
 
-        c.discretise(0.1)
+        c.discretisation = 0.1
 
-        gbxn = c.control_Bx(x, z)
-        _ = c.control_Bz(x, z)
+        gbxn = c.unit_Bx(x, z)
+        _ = c.unit_Bz(x, z)
         _ = np.sqrt(gbx**2 + gbz**2)
-        _ = c.control_psi(x, z)
+        _ = c.unit_psi(x, z)
 
         _, ax = plt.subplots()
         c = ax.contourf(x, z, gbxn)
@@ -126,46 +153,18 @@ class TestCoil:
         ax.set_aspect("equal")
         ax.set_xlim([2, 6])
         ax.set_ylim([-3, 3])
-        plt.show()
-
-    @staticmethod
-    def callable_tester(f_callable):
-        """
-        Checks that all different field calls (with different inputs) behave
-        as expected
-        """
-        # This should go without a hitch...
-        value = f_callable(8, 0)
-        v2 = f_callable(np.array(8), np.array(0))
-        v3 = f_callable(np.array([8]), np.array([0]))
-        assert np.isclose(v2, value)
-        assert np.isclose(v3, value)
-
-        # Now let's check iterables (X = 4 or 20 is off-grid)
-        # (Z = -10 or 10 off-grid)
-        x = np.array([4, 8, 20, 4, 8, 20, 4, 8, 20])
-        z = np.array([0, 0, 0, 10, 10, 10, -10, -10, 10])
-
-        b = np.zeros(len(z))
-
-        for i, (xi, zi) in enumerate(zip(x, z)):
-            b[i] = f_callable(xi, zi)
-
-        b1 = f_callable(x, z)
-
-        assert np.allclose(b, b1.flat)
 
     def test_bx(self):
-        self.callable_tester(self.coil.Bx)
+        callable_tester(self.coil.Bx)
 
     def test_bz(self):
-        self.callable_tester(self.coil.Bz)
+        callable_tester(self.coil.Bz)
 
     def test_bp(self):
-        self.callable_tester(self.coil.Bp)
+        callable_tester(self.coil.Bp)
 
     def test_psi(self):
-        self.callable_tester(self.coil.psi)
+        callable_tester(self.coil.psi)
 
     def test_point_in_coil(self):
         coil = Coil(x=4, z=4, current=10, dx=1, dz=2)
@@ -183,7 +182,6 @@ class TestCoil:
         assert np.alltrue(coil._points_inside_coil(coil.x_boundary, coil.z_boundary))
 
 
-@pytest.mark.longrun
 class TestSemiAnalytic:
     """
     Compare all three control response methods, and that the combination of the
@@ -192,49 +190,78 @@ class TestSemiAnalytic:
 
     @classmethod
     def setup_class(cls):
-        cls.coil = CoilGroup(Coil(x=4, z=4, current=10e6, dx=1, dz=2))
+        cls.coil = Coil(x=4, z=4, ctype="PF", current=10e6, dx=1, dz=2)
+        cls.cg1 = CoilGroup(cls.coil)
+        cls.cg2 = CoilGroup(
+            cls.coil, Coil(x=8, z=8, ctype="PF", current=10e6, dx=1, dz=2)
+        )
         cls.coil.discretisation = 0.2
+        cls.cg1.discretisation = 0.2
+        cls.cg2.discretisation = 0.2
         cls.grid = Grid(0.1, 8, 0, 8, 100, 100)
-        cls.x_boundary = np.append(cls.coil.x_boundary, cls.coil.x_boundary[0])
-        cls.z_boundary = np.append(cls.coil.z_boundary, cls.coil.z_boundary[0])
+        cls.grid2 = Grid(0.1, 12, 0, 12, 100, 100)
+        cls.x_boundary = [np.append(cls.coil.x_boundary, cls.coil.x_boundary[0])]
+        cls.z_boundary = [np.append(cls.coil.z_boundary, cls.coil.z_boundary[0])]
+        cls.x_boundary.append(
+            np.append(cls.cg2.x_boundary, cls.cg2.x_boundary[:, 0][:, None], axis=-1)
+        )
+        cls.z_boundary.append(
+            np.append(cls.cg2.z_boundary, cls.cg2.z_boundary[:, 0][:, None], axis=-1)
+        )
 
     def teardown_method(self):
         plt.show()
         plt.close("all")
 
-    def test_bx(self):
-        gp = self.coil.unit_Bx(self.grid.x, self.grid.z)
-        gp_greens = self.coil._unit_Bx_greens(self.grid.x, self.grid.z)
-        gp_analytic = self.coil._unit_Bx_analytical(self.grid.x, self.grid.z)
+    def _plotter(self, gp, gp_greens, gp_analytic):
+        _, ax = plt.subplots(3, 3)
 
-        _, ax = plt.subplots(1, 3)
-        levels = np.linspace(np.amin(gp), np.amax(gp), 20)
-        ax[0].contourf(self.grid.x, self.grid.z, gp_greens)
-        ax[1].contourf(self.grid.x, self.grid.z, gp, levels=levels)
-        ax[2].contourf(self.grid.x, self.grid.z, gp_analytic, levels=levels)
-        for axis in ax:
-            axis.plot(self.x_corner, self.z_corner, color="r")
+        for axis in ax[:2].flat:
+            axis.plot(self.x_boundary[0], self.z_boundary[0], color="r")
             axis.set_aspect("equal")
-        ax[0].set_title("Green's functions")
-        ax[1].set_title("Combined Green's and semi-analytic")
-        ax[2].set_title("Semi-analytic method")
 
-    def test_bz(self):
-        gp = self.coil.unit_Bz(self.grid.x, self.grid.z)
-        gp_greens = self.coil._unit_Bz_greens(self.grid.x, self.grid.z)
-        gp_analytic = self.coil._unit_Bz_analytical(self.grid.x, self.grid.z)
-
-        _, ax = plt.subplots(1, 3)
-        levels = np.linspace(np.amin(gp), np.amax(gp), 20)
-        ax[0].contourf(self.grid.x, self.grid.z, gp_greens)
-        ax[1].contourf(self.grid.x, self.grid.z, gp, levels=levels)
-        ax[2].contourf(self.grid.x, self.grid.z, gp_analytic, levels=levels)
-        for axis in ax:
-            axis.plot(self.x_corner, self.z_corner, color="r")
+        for axis in ax[2].flat:
+            axis.plot(self.x_boundary[1][0], self.z_boundary[1][0], color="r")
+            axis.plot(self.x_boundary[1][1], self.z_boundary[1][1], color="r")
             axis.set_aspect("equal")
-        ax[0].set_title("Green's functions")
-        ax[1].set_title("Combined Green's and semi-analytic")
-        ax[2].set_title("Semi-analytic method")
+
+        levels = np.linspace(np.amin(gp[0]), np.amax(gp[0]), 20)
+
+        ax[0, 0].set_title("Green's functions")
+        ax[0, 1].set_title("Combined Green's and semi-analytic")
+        ax[0, 2].set_title("Semi-analytic method")
+
+        for i in range(2):
+            ax[i, 0].contourf(self.grid.x, self.grid.z, gp_greens[i], levels=levels)
+            ax[i, 1].contourf(self.grid.x, self.grid.z, gp[i], levels=levels)
+            ax[i, 2].contourf(self.grid.x, self.grid.z, gp_analytic[i], levels=levels)
+
+        levels = np.linspace(np.amin(gp[2]), np.amax(gp[2]), 20)
+        ax[2, 0].contourf(
+            self.grid2.x, self.grid2.z, np.sum(gp_greens[2], axis=-1), levels=levels
+        )
+        ax[2, 1].contourf(
+            self.grid2.x, self.grid2.z, np.sum(gp[2], axis=-1), levels=levels
+        )
+        ax[2, 2].contourf(
+            self.grid2.x, self.grid2.z, np.sum(gp_analytic[2], axis=-1), levels=levels
+        )
+
+    @pytest.mark.parametrize("fd", ["Bx", "Bz"])
+    def test_bfield(self, fd):
+        gp_greens = []
+        gp_analytic = []
+        gp = []
+        for cl, grid in [
+            [self.coil, self.grid],
+            [self.cg1, self.grid],
+            [self.cg2, self.grid2],
+        ]:
+            gp_greens.append(getattr(cl, f"_unit_{fd}_greens")(grid.x, grid.z))
+            gp_analytic.append(getattr(cl, f"_unit_{fd}_analytical")(grid.x, grid.z))
+            gp.append(getattr(cl, f"unit_{fd}")(grid.x, grid.z))
+
+        self._plotter(gp, gp_greens, gp_analytic)
 
 
 class TestCoilGroup:
@@ -279,6 +306,17 @@ class TestCoilGroup:
 
         # TODO test nested removal
 
+    def test_psi(self):
+        callable_tester(self.group.psi, self.group.n_coils)
+
+    def test_bx(self):
+        callable_tester(self.group.Bx, self.group.n_coils)
+
+    def test_bz(self):
+        callable_tester(self.group.Bz, self.group.n_coils)
+
+    def test_bp(self):
+        callable_tester(self.group.Bp, self.group.n_coils)
 
 class TestPositionalSymmetricCircuit:
     @classmethod