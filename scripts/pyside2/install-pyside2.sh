ace2a3ecc4edcbe2e6cd1405bf53381d98e5113f
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-12-07 14:39:06 +0000

    Remove SKIPMODS arg from pyside build script
    The pyside build failed complaining of missing Qt libraries. We may wish
    to revisit this to avoid building more than we need to, but this makes
    the build work.

--- scripts/pyside2/install-pyside2.sh
+++ scripts/pyside2/install-pyside2.sh
@@ -15,10 +15,5 @@ if [[ $(basename $PWD) == *"bluemira"* ]]; then
 fi
 
 cd pyside-setup
-SKIP_MODS=Multimedia,MultimediaWidgets,Positioning,Location,Qml,Quick,\
-QuickWidgets,RemoteObjects,Scxml,Script,ScriptTools,Sensors,SerialPort,TextToSpeech,\
-Charts,DataVisualization,WebChannel,WebEngineCore,WebEngine,WebEngineWidgets,WebSockets,\
-3DCore,3DRender,3DInput,3DLogic,3DAnimation,3DExtras
-# https://bugreports.qt.io/browse/PYSIDE-1873 (limited api switch)
-python3 setup.py build --qmake=/usr/local/Qt-5.15.5/bin/qmake --parallel=$NJOBS --limited-api=yes --skip-modules=$SKIP_MODS
+python3 setup.py build --qmake=/usr/local/Qt-5.15.5/bin/qmake --parallel=$NJOBS --limited-api=yes
 python3 setup.py install --qmake=/usr/local/Qt-5.15.5/bin/qmake --parallel=$NJOBS --limited-api=yes
