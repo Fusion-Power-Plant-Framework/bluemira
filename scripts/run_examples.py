d4a5d53ac1240e6a932cf194222e7b01f1d952bb
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-12-16 11:25:37 +0000

    Run examples in CI (#1748)
    * Add script to run examples
    
    * Add GitHub action to run examples
    
    * Add 'push' to examples trigger
    
    This is just to get the workflow triggered once, so we can trigger it
    manually from then on
    
    * Revert "Add 'push' to examples trigger"
    
    This reverts commit a00b826a50b6279db462a0eaa2e9e36f90359e71.
    
    * Use bash login shell when running examples in CI
    
    * Pip install bluemira in examples CI
    
    * Close plots after each example in examples script
    
    * Print summary of results in 'run_examples'
    
    * Disable process/plasmod dependent examples in CI
    
    * Flesh out examples CI script docstring
    
    * Fix 'run_examples' script CLI argument help
    
    * Use in-place bluemira install in examples CI
    
    My theory is that doing a normal pip-install installs bluemira into
    site-packages, but does not copy the examples with it. Hence any
    examples that use `get_bluemira_path("", "examples")` are failing
    because it's looking in the wrong place for the examples.
    
    Using an in-place install means that the bluemira module is not moved
    relative to the examples, so (hopefully) the 'get_.._path' call will
    work.
    
    * Flush stdout after each example in examples script
    
    In the CI logs, some error messages were being printed under the wrong
    example's banner. I'm hoping flushing after each example is run will fix
    this.
    
    * Add comment on disabled examples in examples CI
    
    * Add 'py' extension to examples script
    
    This keeps consistency with the other Python scripts
    
    * Remove redundant 'except' case
    
    * Add typing to examples script function arg
    
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>
    
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>

---
+++ scripts/run_examples.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+"""
+Run Bluemira example files in sequence. Exit with code 1 if any errors.
+
+This will recursively search for python files within a given directory,
+and run them in sequence. It will report which ran without error, and
+which failed.
+
+By default, this script will disable plot/CAD display windows. The idea
+being that this script will mostly be automatically run in a
+non-interactive shell.
+"""
+
+import os
+import re
+import sys
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Tuple
+from unittest import mock
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from bluemira.base.file import get_bluemira_path
+
+BANNER = "\n" + "-" * 72 + "\n| {}\n" + "-" * 72 + "\n"
+EXAMPLES_ROOT = os.path.normpath(get_bluemira_path("examples", subfolder=""))
+EXCLUDE_PATTERNS = ["convert_py_to_ipynb"]
+
+
+@dataclass
+class Args:
+    """Command line arguments."""
+
+    examples_dir: str
+    exclude_pattern: List[str]
+    plotting_on: bool
+
+
+def parse_args(sys_args: List[str]) -> Args:
+    """Parse command line arguments"""
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--examples-dir",
+        default=EXAMPLES_ROOT,
+        help=f"the directory in which to look for examples (default: {EXAMPLES_ROOT})",
+    )
+    parser.add_argument(
+        "-e",
+        "--exclude-pattern",
+        action="append",
+        default=EXCLUDE_PATTERNS,
+        help=(
+            "do not run example files that contain this regex pattern. "
+            "This argument may be used more than once"
+        ),
+    )
+    parser.add_argument(
+        "--plotting-on",
+        action="store_true",
+        default=False,
+        help="enable interactive plotting windows",
+    )
+    args = parser.parse_args(sys_args)
+    return Args(**vars(args))
+
+
+def find_python_files(examples_dir: str, exclude_patterns: List[str]) -> List[str]:
+    """Glob for Python files in the given directory."""
+    files = []
+    for path in Path(examples_dir).rglob("*.py"):
+        if not any(re.search(p, str(path)) for p in exclude_patterns):
+            files.append(path)
+    return sorted(files)
+
+
+def run_example(file_path: str) -> bool:
+    """Run the given Python file; return True if no errors, else False."""
+    source = Path(file_path).read_text()
+    try:
+        exec(compile(source, file_path, "exec"), globals())  # noqa: S102
+    except Exception as e:
+        print(e, file=sys.stderr)
+        return False
+    finally:
+        plt.close("all")
+    return True
+
+
+def run_examples(
+    example_files: List[str], plotting_on: bool = False
+) -> List[Tuple[str, bool]]:
+    """
+    Run the given example files.
+
+    Returns the list of examples run, along with a boolean indicating
+    whether there were any errors when running the example (True
+    indicating no errors, and False the opposite).
+    """
+    if not plotting_on:
+        mpl.use("Agg")
+        # Disable CAD viewer by mocking out FreeCAD API's displayer.
+        # Note that if we use a new CAD backend, this must be changed.
+        mock.patch("bluemira.codes._freecadapi.show_cad").start()
+
+    failed = []
+    for example in example_files:
+        display_path = Path(example).relative_to(args.examples_dir)
+        print(BANNER.format(display_path))
+        result = run_example(example)
+        # Flush stdout so we don't get un-flushed output from previous
+        # examples under this example's banner
+        sys.stdout.flush()
+        failed.append((display_path, result))
+    return failed
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    example_py_files = find_python_files(args.examples_dir, args.exclude_pattern)
+    if not example_py_files:
+        print(f"found no python files in '{args.examples_dir}'.")
+        sys.exit(1)
+
+    results = run_examples(example_py_files, args.plotting_on)
+    print("\nExamples run:")
+    print("-------------")
+    for example, result in results:
+        print(f"{'✔️' if result else '❌'} {example}")
+    sys.exit(not all(r[1] for r in results))
