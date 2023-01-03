65d8b1858cba41bb4889fb6b77cc775e76c1cf41
James <james.cook1@ukaea.uk>
2023-01-03 10:20:32 +0000

    🚨 copyright update

--- tests/mesh/test_meshing.py
+++ tests/mesh/test_meshing.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh,
+# Copyright (C) 2021-2023 M. Coleman, J. Cook, F. Franza, I.A. Maione, S. McIntosh,
 #                         J. Morris, D. Short
 #
 # bluemira is free software; you can redistribute it and/or
