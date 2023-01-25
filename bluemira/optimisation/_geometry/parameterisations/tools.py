774fd869006fbe8fe4ef901eae250c08713d5c8b
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2023-01-25 11:20:27 +0000

    Make optimisation submodule a directory

--- bluemira/optimisation/_geometry/parameterisations/tools.py
+++ bluemira/optimisation/_geometry/parameterisations/tools.py
@@ -18,17 +18,17 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
-import numpy as np
-
-from bluemira.geometry.parameterisations import GeometryParameterisation, PrincetonD
+from bluemira.utilities.opt_variables import OptVariables
 
 
-def _get_x_norm_index(variables, name: str):
+def get_x_norm_index(variables: OptVariables, name: str):
     """
     Get the index of a variable name in the modified-length x_norm vector
 
     Parameters
     ----------
+    variables: OptVariables
+        Bounded optimisation variables
     name: str
         Variable name for which to get the index
 
@@ -50,13 +50,15 @@ def _get_x_norm_index(variables, name: str):
     return idx_actual - count
 
 
-def _process_x_norm_fixed(variables, x_norm):
+def process_x_norm_fixed(variables: OptVariables, x_norm):
     """
     Utility for processing a set of free, normalised variables, and folding the fixed
     un-normalised variables back into a single list of all actual values.
 
     Parameters
     ----------
+    variables: OptVariables
+        Bounded optimisation variables
     x_norm: np.ndarray
         Normalised vector of variable values
 
@@ -75,33 +77,3 @@ def _process_x_norm_fixed(variables, x_norm):
         for i in fixed_idx:
             x_actual.insert(i, x_fixed[i])
     return x_actual
-
-
-def f_ineq_constraint_princeton_d(geom: GeometryParameterisation) -> np.ndarray:
-    """Inequality constraint for PrincetonD."""
-    free_vars = geom.variables.get_normalised_values()
-    x1, x2, _ = _process_x_norm_fixed(geom.variables, free_vars)
-    return np.array([x1 - x2])
-
-
-def df_ineq_constraint_princeton_d(geom: GeometryParameterisation) -> np.ndarray:
-    """Inequality constraint gradient for PrincetonD."""
-    opt_vars = geom.variables
-    free_vars = opt_vars.get_normalised_values()
-    grad = np.zeros((1, len(free_vars)))
-    if not geom.variables["x1"].fixed:
-        grad[0, _get_x_norm_index(opt_vars, "x1")] = 1
-    if not geom.variables["x2"].fixed:
-        grad[0, _get_x_norm_index(opt_vars, "x2")] = -1
-    return grad
-
-
-INEQ_CONSTRAINT_REGISTRY = {
-    PrincetonD: [
-        {
-            "f_constraint": f_ineq_constraint_princeton_d,
-            "df_constraint": df_ineq_constraint_princeton_d,
-            "tolerance": np.array([np.finfo(float).eps]),
-        }
-    ]
-}
