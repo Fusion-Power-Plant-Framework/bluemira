Fix shape of inequality constraint gradients
This is now consistent with the shape of the inequality constraint
gradients in the old optimiser.

bluemira/optimisation/_geometry/parameterisations/princeton_d.py
@@ -37,9 +37,9 @@ def df_ineq_constraint(geom: PrincetonD) -> np.ndarray:
     free_vars = opt_vars.get_normalised_values()
     grad = np.zeros((1, len(free_vars)))
     if not geom.variables["x1"].fixed:
-        grad[0, tools.get_x_norm_index(opt_vars, "x1")] = 1
+        grad[0][tools.get_x_norm_index(opt_vars, "x1")] = 1
     if not geom.variables["x2"].fixed:
-        grad[0, tools.get_x_norm_index(opt_vars, "x2")] = -1
+        grad[0][tools.get_x_norm_index(opt_vars, "x2")] = -1
     return grad
 
 

bluemira/optimisation/_geometry/parameterisations/sextuple_arc.py
@@ -40,11 +40,11 @@ def f_ineq_constraint(geom: SextupleArc) -> np.ndarray:
 def df_ineq_constraint(geom: SextupleArc) -> np.ndarray:
     """Inequality constraint gradient for TripleArc."""
     x_norm = geom.variables.get_normalised_values()
-    gradient = np.zeros(len(x_norm))
+    gradient = np.zeros((1, len(x_norm)))
     for var in ["a1", "a2", "a3", "a4", "a5"]:
         if not geom.variables[var].fixed:
             var_idx = tools.get_x_norm_index(geom.variables, var)
-            gradient[var_idx] = 1
+            gradient[0][var_idx] = 1
     return gradient
 
 

bluemira/optimisation/_geometry/parameterisations/triple_arc.py
@@ -39,14 +39,14 @@ def f_ineq_constraint(geom: TripleArc) -> np.ndarray:
 def df_ineq_constraint(geom: TripleArc) -> np.ndarray:
     """Inequality constraint gradient for TripleArc."""
     free_vars = geom.variables.get_normalised_values()
-    g = np.zeros((len(free_vars), 1))
+    g = np.zeros((1, len(free_vars)))
     if not geom.variables["a1"].fixed:
         idx_a1 = tools.get_x_norm_index(geom.variables, "a1")
-        g[idx_a1] = 1
+        g[0][idx_a1] = 1
     if not geom.variables["a2"].fixed:
         idx_a2 = tools.get_x_norm_index(geom.variables, "a2")
-        g[idx_a2] = 1
-    return g[0, :]
+        g[0][idx_a2] = 1
+    return g
 
 
 def tol() -> np.ndarray: