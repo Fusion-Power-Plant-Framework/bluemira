Fix bug in optimisation approx derivatives
We were caching the previous f(x) result in the optimiser, and passing
this through to the derivative approximator, so it didn't need to be
recalculated. However, (at least with SLSQP) this was causing some
issue. I'm still not entirely clear why, it needs investigating more.

bluemira/optimisation/_nlopt/constraint.py
@@ -52,7 +52,6 @@ class Constraint:
         self.f = f
         self.tolerance = tolerance
         self.df = df if df is not None else self._approx_derivative
-        self.f0: Optional[np.ndarray] = None
         self.bounds = bounds
 
     def nlopt_call(self, result: np.ndarray, x: np.ndarray, grad: np.ndarray) -> None:
@@ -64,10 +63,9 @@ class Constraint:
         if grad.size > 0:
             grad[:] = self.df(x)
         result[:] = self.f(x)
-        self.f0 = result
 
     def _approx_derivative(self, x: np.ndarray) -> np.ndarray:
-        return approx_derivative(self.f, x, f0=self.f0, bounds=self.bounds)
+        return approx_derivative(self.f, x, bounds=self.bounds)
 
     def set_approx_derivative_lower_bound(self, lower_bound: np.ndarray) -> None:
         """Set the lower bounds for use in derivative approximation."""

bluemira/optimisation/_nlopt/objective.py
@@ -49,7 +49,6 @@ class NloptObjectiveFunction:
         ),
     ):
         self.f = f
-        self.f0: Optional[float] = None
         self.df = df if df is not None else self._approx_derivative
         self.bounds = bounds
         self.history: List[np.ndarray] = []
@@ -58,19 +57,17 @@ class NloptObjectiveFunction:
         """Execute the NLOpt objective function."""
         if grad.size > 0:
             grad[:] = self.df(x)
-        self.f0 = self.f(x)
-        return self.f0
+        return self.f(x)
 
     def call_with_history(self, x: np.ndarray, grad: np.ndarray) -> float:
         """Execute the NLOpt objective function, recording the iteration history."""
         self.history.append(np.copy(x))
         if grad.size > 0:
             grad[:] = self.df(x)
-        self.f0 = self.f(x)
-        return self.f0
+        return self.f(x)
 
     def _approx_derivative(self, x: np.ndarray) -> np.ndarray:
-        return approx_derivative(self.f, x, f0=self.f0, bounds=self.bounds)
+        return approx_derivative(self.f, x, bounds=self.bounds)
 
     def set_approx_derivative_lower_bound(self, lower_bound: np.ndarray) -> None:
         """Set the lower bounds for use in derivative approximation."""

tests/optimisation/test_geometry.py
@@ -117,7 +117,12 @@ class TestGeometry:
         # height of the TripleArc to match the height of the keep-in
         # zone, and the width of the TripleArc should be less than the
         # width of the keep-in zone.
-        arc = TripleArc()
+        arc = TripleArc(
+            {
+                "x1": {"value": 3.5, "lower_bound": 2.75},
+                "dz": {"value": 0},
+            }
+        )
         kiz = make_polygon(
             np.array([[3, 13, 13, 3], [0, 0, 0, 0], [-5, -5, 6, 6]]), closed=True
         )
@@ -126,11 +131,8 @@ class TestGeometry:
             arc,
             f_objective=lambda geom: -geom.create_shape().length,
             keep_in_zones=(kiz,),
+            algorithm="SLSQP",
             opt_conditions={"max_eval": 5000, "ftol_rel": 1e-6},
-            # TODO(hsaunders1904): more investigation why we get error:
-            #  'RuntimeError: bug: more than iter SQP iterations'
-            #  for SLSQP
-            algorithm="COBYLA",
         )
 
         opt_shape = result.geom.create_shape()
@@ -177,24 +179,16 @@ class TestGeometry:
         # Run an optimisation to maximise the size of the angles in a
         # SextupleArc. The shape constraint should ensure the angles do
         # not sum to more than 360 degrees.
+        arc = SextupleArc()
         angle_vars = ["a1", "a2", "a3", "a4", "a5"]
 
         def sum_angles(geom: SextupleArc) -> float:
             angles = [geom.variables[a].normalised_value for a in angle_vars]
             return np.sum(angles)
 
-        def d_sum_angles(geom: SextupleArc) -> np.ndarray:
-            grad = np.zeros(len(geom.variables.get_normalised_values()))
-            for angle in angle_vars:
-                grad[geom._get_x_norm_index(angle)] = 1
-            return grad
-
-        arc = SextupleArc()
-
         result = optimise_geometry(
             arc,
             f_objective=lambda geom: -sum_angles(geom),
-            df_objective=lambda geom: -d_sum_angles(geom),
             opt_conditions={"max_eval": 5000, "ftol_rel": 1e-6},
             algorithm="SLSQP",
         )