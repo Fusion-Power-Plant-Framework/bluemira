52c3d1fa2af4afeaf43c886c9a3769141be4c5bd
James <james.cook1@ukaea.uk>
2023-02-03 12:05:29 +0000

    🐛 change to mambaforge

--- scripts/install-process.sh
+++ scripts/install-process.sh
@@ -7,7 +7,7 @@
 set -e
 
 # Ensure we are working in the (bluemira) conda environment
-source ~/.miniforge-init.sh && conda activate bluemira
+source ~/.mambaforge-init.sh && conda activate bluemira
 
 if [[ $(basename $PWD) == *"bluemira"* ]]; then
   cd ..
