fc2a729b893881bc28e005e7c9a60eeb1b36f9e1
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2023-03-01 15:57:14 +0100

    complete missing tests and revise docstrings

--- bluemira/power_cycle/net_manager.py
+++ bluemira/power_cycle/net_manager.py
@@ -3,3 +3,14 @@
 """
 Classes for the calculation of net power in the power cycle model.
 """
+
+
+class PlantSystem:
+    # Build PowerLoad for that PlantSystem
+    pass
+
+
+class PowerLoadManager:
+    # Read inputs for each PlantSystem
+    # Build PhaseLoads and Pulse
+    pass
