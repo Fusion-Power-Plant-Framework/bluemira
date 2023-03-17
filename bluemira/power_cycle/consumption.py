5e99982d599a50fb6fddaa38e00f91d4695afa3d
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2023-03-17 17:41:48 +0100

    clean branch of future-development files

--- bluemira/power_cycle/consumption.py
+++
@@ -1,9 +0,0 @@
-"""
-Base classes for the calculation of power consumption in the power plant
-with the Recirculation-Electric-Loads (REL) sub-module routines.
-"""
-
-# class CoilPowerLoad():
-# class NBIPowerLoad():
-# class ECHPowerLoad():
-# class ICHPowerLoad():
