Fix incorrect panel length calculation

eudemo/eudemo/ivc/panelling/_designer.py
@@ -76,7 +76,7 @@ class PanellingDesigner(Designer[np.ndarray]):
     params: PanellingDesignerParams
     _defaults = {
         "algorithm": "SLSQP",
-        "opt_conditions": {"max_eval": 400, "ftol_rel": 1e-4},
+        "opt_conditions": {"max_eval": 400, "ftol_rel": 1e-6},
     }
 
     def __init__(

eudemo/eudemo/ivc/panelling/_opt_problem.py
@@ -59,10 +59,38 @@ class PanellingOptProblem(OptimisationProblem):
         constraint = OptimisationConstraint(
             self.constrain_min_length_and_angles,
             f_constraint_args={},
-            tolerance=np.full(self.n_constraints, 1e-5),
+            tolerance=np.full(self.n_constraints, 1e-3),
         )
         super().__init__(self.paneller.x0, optimiser, objective, [constraint])
         self.set_up_optimiser(self.n_opts, bounds=self.bounds)
+
+    @property
+    def n_opts(self) -> int:
+        """
+        The number of optimisation parameters.
+
+        The optimisation parameters are how far along the boundary's
+        length each panel tangents the boundary. We exclude the start
+        and end points which are fixed.
+        """
+        # exclude start and end points; hence 'N - 2'
+        return self.paneller.n_panels - 2
+
+    @property
+    def n_constraints(self) -> int:
+        """
+        The number of optimisation constraints.
+
+        We constrain:
+
+            - the minimum length of each panel
+            - the angle between each panel
+              (no. of angles = no. of panels - 1)
+
+        Note that we exclude the start and end touch points which are
+        fixed.
+        """
+        return 2 * self.paneller.n_panels - 1
 
     def optimise(self):
         """Perform the optimisation."""
@@ -82,56 +110,35 @@ class PanellingOptProblem(OptimisationProblem):
         self, constraint: np.ndarray, x: np.ndarray, grad: np.ndarray
     ) -> np.ndarray:
         """Constraint function to pass to ``nlopt``."""
-        # Constrain minimum length
+        n_panels = self.paneller.n_panels
+        self._constrain_min_length(constraint[:n_panels], x, grad[:n_panels])
+        self._constrain_max_angle(constraint[n_panels:], x, grad[n_panels:])
+        return constraint
+
+    def _constrain_min_length(
+        self, constraint: np.ndarray, x: np.ndarray, grad: np.ndarray
+    ) -> None:
         lengths = self.paneller.panel_lengths(x)
-        constraint[: len(lengths)] = self.paneller.dx_min - lengths
+        constraint[:] = self.paneller.dx_min - lengths
         if grad.size > 0:
             # TODO(hsaunders1904): work out what BLUEPRINT was doing to
             #  get this gradient
-            grad[: len(lengths)] = approx_derivative(
+            grad[:] = approx_derivative(
                 lambda x_opt: -self.paneller.panel_lengths(x_opt),
                 x0=x,
-                f0=constraint[: len(lengths)],
+                f0=constraint,
                 bounds=self.bounds,
             )
 
-        # Constrain joint angles
-        constraint[len(lengths) :] = self.paneller.angles(x) - self.paneller.max_angle
+    def _constrain_max_angle(
+        self, constraint: np.ndarray, x: np.ndarray, grad: np.ndarray
+    ) -> None:
+        constraint[:] = self.paneller.angles(x) - self.paneller.max_angle
         if grad.size > 0:
             # TODO(hsaunders): I'm sure we can be smarter about this gradient
-            grad[len(lengths) :, :] = approx_derivative(
-                lambda x_opt: self.paneller.angles(x_opt),
+            grad[:] = approx_derivative(
+                self.paneller.angles,
                 x0=x,
-                f0=constraint[len(lengths) :],
+                f0=constraint,
                 bounds=self.bounds,
             )
-        return constraint
-
-    @property
-    def n_opts(self) -> int:
-        """
-        The number of optimisation parameters.
-
-        The optimisation parameters are how far along the boundary's
-        length each panel tangents the boundary. We exclude the start
-        and end points which are fixed.
-        """
-        # exclude start and end points; hence 'N - 2'
-        return self.paneller.n_points - 2
-
-    @property
-    def n_constraints(self) -> int:
-        """
-        The number of optimisation constraints.
-
-        We constrain:
-
-            - the minimum length of each panel
-              (no. of panels = no. of touch points + 2)
-            - the angle between each panel
-              (no. of angles = no. of touch points + 1)
-
-        Note that we exclude the start and end touch points which are
-        fixed.
-        """
-        return 2 * self.paneller.n_points

eudemo/eudemo/ivc/panelling/_paneller.py
@@ -49,8 +49,12 @@ class Paneller:
             max_angle=max_angle,
             dx_min=dx_min,
         )
-        self.n_points = len(idx)
         self.x0: np.ndarray = self.boundary.length_norm[idx][1:-1]
+
+    @property
+    def n_panels(self) -> int:
+        """The number of panels defined by this paneller."""
+        return len(self.x0) + 2
 
     def joints(self, dists: np.ndarray) -> np.ndarray:
         """
@@ -104,8 +108,8 @@ class Paneller:
 
     def panel_lengths(self, dists: np.ndarray) -> np.ndarray:
         """Return the lengths of each panel."""
-        joints = self.joints(dists)
-        return np.hypot(joints[0], joints[1])
+        panel_vecs = np.diff(self.joints(dists))
+        return np.hypot(panel_vecs[0], panel_vecs[1])
 
 
 class LengthNormBoundary:

eudemo/eudemo_tests/ivc/panelling/test_designer.py
@@ -123,10 +123,8 @@ class TestPanellingDesigner:
         angles = []
         for i in range(len(panel_vecs) - 1):
             angles.append(find_clockwise_angle_2d(panel_vecs[i], panel_vecs[i + 1]))
-        abs_tol = 1e-3
         assert (
-            np.less_equal(angles, max_angle - abs_tol)
-            | np.isclose(angles, max_angle, rtol=1e-3)
+            np.less_equal(angles, max_angle) | np.isclose(angles, max_angle, rtol=1e-3)
         ).all()
 
     @pytest.mark.parametrize("dl_min", [0, 0.1, 0.9])