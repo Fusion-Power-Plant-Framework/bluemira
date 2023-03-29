42d35850a98ba087f450ab29fa0bb78a635d1b4f
Harry Saunders <harry.saunders@ukaea.uk>
2023-03-29 14:04:26 +0100

    Avoid division by zero in Paneller

--- eudemo/eudemo/ivc/panelling/_paneller.py
+++ eudemo/eudemo/ivc/panelling/_paneller.py
@@ -111,6 +111,8 @@ class Paneller:
         joints = self.joints(dists)
         line_vectors = np.diff(joints, axis=1)
         magnitudes = np.linalg.norm(line_vectors, axis=0)
+        # avoid division by zero later
+        magnitudes[np.isclose(magnitudes, 0)] += 1e-8
         dots = (line_vectors[:, :-1] * line_vectors[:, 1:]).sum(axis=0)
         dots /= magnitudes[:-1] * magnitudes[1:]
         return np.degrees(np.arccos(np.clip(dots, -1.0, 1.0)), out=dots)
