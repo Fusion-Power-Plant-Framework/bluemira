65967062f130e3b0c759be96f33529a04586c43f
james <james.cook1@ukaea.uk>
2023-04-11 12:16:35 +0100

    ✅ fix test

--- eudemo/eudemo/tf_coils/tf_coils.py
+++ eudemo/eudemo/tf_coils/tf_coils.py
@@ -337,6 +337,7 @@ class TFCoilDesigner(Designer[GeometryParameterisation]):
             self.problem_settings["ripple_selector"] = ripple_selector(
                 **rs_config.get("args", {})
             )
+        
         design_problem = self.problem_class(
             parameterisation,
             self.algorithm_name,
