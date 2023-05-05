8f49fcd1dacd185110715f57d2282ae68187a469
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2023-05-05 11:59:00 +0100

    Remove typing from optimisation module's docstrings (#2191)
    * Remove typing from optimisation module docstrings
    
    The typing is already given via type annotations. No need to duplicate.
    
    * Add colons to optimisation parameter docstrings
    
    * Add missing parameter to `optimise` docstring

--- bluemira/optimisation/_geometry/parameterisations/tools.py
+++ bluemira/optimisation/_geometry/parameterisations/tools.py
@@ -18,6 +18,8 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
+import numpy as np
+
 from bluemira.utilities.opt_variables import OptVariables
 
 
@@ -27,15 +29,14 @@ def get_x_norm_index(variables: OptVariables, name: str):
 
     Parameters
     ----------
-    variables: OptVariables
+    variables:
         Bounded optimisation variables
-    name: str
+    name:
         Variable name for which to get the index
 
     Returns
     -------
-    idx_x_norm: int
-        Index of the variable name in the modified-length x_norm vector
+    Index of the variable name in the modified-length x_norm vector
     """
     fixed_idx = variables._fixed_variable_indices
     idx_actual = variables.names.index(name)
@@ -50,22 +51,21 @@ def get_x_norm_index(variables: OptVariables, name: str):
     return idx_actual - count
 
 
-def process_x_norm_fixed(variables: OptVariables, x_norm):
+def process_x_norm_fixed(variables: OptVariables, x_norm: np.ndarray):
     """
     Utility for processing a set of free, normalised variables, and folding the fixed
     un-normalised variables back into a single list of all actual values.
 
     Parameters
     ----------
-    variables: OptVariables
+    variables:
         Bounded optimisation variables
-    x_norm: np.ndarray
+    x_norm:
         Normalised vector of variable values
 
     Returns
     -------
-    x_actual: list
-        List of ordered actual (un-normalised) values
+    List of ordered actual (un-normalised) values
     """
     fixed_idx = variables._fixed_variable_indices
 
