6ac9095115a7068b63632704301fbbf6fbeed9cc
CoronelBuendia <matti.coleman@gmail.com>
2023-05-09 13:34:18 +0200

    add tests for tesselation not changing properties

--- tests/geometry/test_bound_box.py
+++ tests/geometry/test_bound_box.py
@@ -25,7 +25,7 @@ import pytest
 from bluemira.geometry.bound_box import BoundingBox
 from bluemira.geometry.face import BluemiraFace
 from bluemira.geometry.parameterisations import PolySpline
-from bluemira.geometry.tools import boolean_cut, make_polygon
+from bluemira.geometry.tools import boolean_cut, make_polygon, revolve_shape
 
 
 class TestBoundingBox:
@@ -69,6 +69,9 @@ class TestHardBoundingBox:
         make_polygon({"x": [0, 15, 15, 0], "z": [-7, -7, -5, -5], "y": 0}, closed=True)
     )
     wire = boolean_cut(ps.create_shape(), cut_box)[0]
+    temp = wire.deepcopy()
+    temp.close()
+    solid = revolve_shape(BluemiraFace(temp), degree=159)
 
     @pytest.mark.xfail
     def test_bad_bounding_box(self):
@@ -78,6 +81,20 @@ class TestHardBoundingBox:
     def test_opt_bounding_box(self, tol):
         bb = self.wire.get_optimal_bounding_box(tolerance=tol)
         assert np.isclose(bb.z_min, -5.0)
+
+    @pytest.mark.parametrize("tol", [10.0, 1.0, 0.1, 0.01])
+    def test_opt_bounding_box(self, tol):
+        solid = self.solid.deepcopy()
+        vertices, indices = solid._tessellate(1.0)
+        bb = self.solid.get_optimal_bounding_box(tolerance=tol)
+        vertices2, indices2 = solid._tessellate(1.0)
+        assert np.isclose(bb.z_min, -5.0)
+        # Test that bounding box via tesselation did not modify properties
+        np.testing.assert_allclose(vertices, vertices2)
+        np.testing.assert_allclose(indices, indices2)
+        vertices3, indices3 = solid._tessellate(0.01)
+        assert vertices3.shape != vertices2.shape
+        assert indices.shape != indices2.shape
 
     @pytest.mark.parametrize("tol", [0.0, -1e-9])
     def test_bad_tolerace(self, tol):
