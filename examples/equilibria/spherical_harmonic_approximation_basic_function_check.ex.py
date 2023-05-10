177878613071982a86b7afd3a704996664d89a35
geograham <g.a.graham09@gmail.com>
2023-05-10 09:39:47 +0100

    double space indent

--- examples/equilibria/spherical_harmonic_approximation_basic_function_check.ex.py
+++ examples/equilibria/spherical_harmonic_approximation_basic_function_check.ex.py
@@ -4,9 +4,9 @@
 #    cell_metadata_filter: tags,-all
 #    notebook_metadata_filter: -jupytext.text_representation.jupytext_version
 #       text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
+#         extension: .py
+#         format_name: percent
+#         format_version: '1.3'
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
