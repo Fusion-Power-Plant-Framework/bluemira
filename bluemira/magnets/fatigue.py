3dd40850fb9ef5b5f7e370352c87fc77e44bc69c
CoronelBuendia <matti.coleman@gmail.com>
2023-05-30 16:31:12 +0200

    urgh

--- bluemira/magnets/fatigue.py
+++ bluemira/magnets/fatigue.py
@@ -115,7 +115,14 @@ def calculate_n_pulses(
     delta = 1e-4  # Crack size increment
 
     while a < max_crack_depth and c < max_crack_width and K_max < max_stress_intensity:
-        Ka = 0.0
+        Ka = _calc_stress_intensity_factor(
+            conductor.max_hoop_stress,
+            conductor.tk_radial,
+            conductor.width,
+            a,
+            c,
+            initial_crack.angle,
+        )
         Km = 0.0
         K_max = max(Ka, Km)
 
@@ -126,3 +133,30 @@ def calculate_n_pulses(
     n_cycles /= safety.sf_n_cycle
 
     return n_cycles // 2
+
+
+def _calc_stress_intensity_factor(hoop_stress, t, w, a, c, phi):
+    bend_stress = 0.0
+
+    if a <= c:  # a/c <= 1
+        ratio = a / c
+        m1 = 1.13 - 0.09 * ratio
+        m2 = -0.54 + 0.89 / (0.2 + ratio)
+        m3 = 0.5 - 1.0 / (0.65 + ratio) + 14 * (1 - ratio) ** 24
+        g = 1.0 + (0.1 + 0.35 * ratio**2) * (1 - np.sin(phi)) ** 2
+        f_phi = (ratio**2 * np.cos(phi) ** 2 + np.sin(phi) ** 2) ** 4
+        f_w = np.sqrt(1.0 / np.cos(np.sqrt(a / t) * np.pi * c / (2 * w)))
+        g21 = -1.22 - 0.12 * ratio
+        g22 = 0.55 - 1.05 * ratio**0.75 + 0.47 * ratio**1.5
+        h1 = 1.0 - 0.34 * a / t - 0.11 * ratio * (a / t)
+        h2 = 1 + g21
+
+    else:  # a/c > 1
+        ratio = c / a
+
+    p = 0.2 + ratio + 0.6 * a / t
+    H_s = h1 + (h2 - h1) * np.sin(phi) ** p
+    Q = 1.0 + 1.464 * ratio**1.65
+    F_s = (m1 + m2 * (a / t) ** 2 + m3 * (a / t) ** 4) * g * f_phi * f_w
+
+    return hoop_stress + H_s * bend_stress * np.sqrt(np.pi * a * F_s / Q)
