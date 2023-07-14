3aa41dcfa9e343ae3a0b15ddfc4eae03f1bc96ca
CoronelBuendia <matti.coleman@gmail.com>
2023-07-14 11:07:37 +0200

    fix PF coil sizes and allow relaxation

--- bluemira/equilibria/run.py
+++ bluemira/equilibria/run.py
@@ -711,18 +711,23 @@ class OptimisedPulsedCoilsetDesign(PulsedCoilsetDesign):
         mesh.
         """
         max_cs_currents = coilset.get_coiltype("CS").get_max_current(0.0)
+        pf_currents = []
+        for problem in sub_opt_problems:
+            pf_coils = problem.eq.coilset.get_coiltype("PF").get_control_coils()
+            pf_currents.append(np.abs(pf_coils.current))
+
+        max_pf_currents = np.max(pf_currents, axis=0)
+        # Relax the max currents a bit to avoid oscillation
+        max_pf_current = self._eq_settings.peak_PF_current_factor * self.params.I_p.value
+        max_pf_currents = np.clip(1.05 * max_pf_currents, 0, max_pf_current)
 
         for problem in sub_opt_problems:
             pf_coils = problem.eq.coilset.get_coiltype("PF").get_control_coils()
-            pf_current = pf_coils.current
-            max_pf_current = np.max(np.abs(pf_current))
-            pf_coils.resize(max_pf_current)
+            pf_coils.resize(max_pf_currents)
             pf_coils.fix_sizes()
             pf_coils.discretisation = self._eq_settings.coil_mesh_size
             problem.set_current_bounds(
-                np.concatenate(
-                    [np.full(pf_current.size, max_pf_current), max_cs_currents]
-                )
+                np.concatenate([max_pf_currents, max_cs_currents])
             )
 
         consolidated_coilset = deepcopy(problem.eq.coilset)
