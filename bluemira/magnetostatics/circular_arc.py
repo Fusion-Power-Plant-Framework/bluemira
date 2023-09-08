`CircularArcCurrentSource` API fixed to degrees, not radians (#2595)
* move to degree

* missed a test

bluemira/magnetostatics/circular_arc.py
@@ -623,7 +623,7 @@ class CircularArcCurrentSource(RectangularCrossSectionCurrentSource):
     radius:
         The radius of the circular arc from the origin [m]
     dtheta:
-        The azimuthal width of the arc [rad]
+        The azimuthal width of the arc [°]
     current:
         The current flowing through the source [A]
 
@@ -654,7 +654,7 @@ class CircularArcCurrentSource(RectangularCrossSectionCurrentSource):
         self._radius = radius
         self._update_r1r2()
 
-        self.dtheta = dtheta
+        self.dtheta = np.deg2rad(dtheta)
         self.rho = current / (4 * breadth * depth)
         self.dcm = np.array([ds, normal, t_vec])
         self.points = self._calculate_points()

documentation/source/magnetostatics/doc_circular.py
@@ -11,7 +11,7 @@ source = CircularArcCurrentSource(
     breadth=0.25,
     depth=1,
     radius=2,
-    dtheta=3 * np.pi / 2,
+    dtheta=270,
     current=1e6,
 )
 

tests/magnetostatics/test_circuits.py
@@ -96,16 +96,16 @@ def test_mixedsourcesolver():
     )
 
     arc_1 = CircularArcCurrentSource(
-        [-1, 0, 1], [0, 0, 1], [-1, 0, 0], [0, 1, 0], dz, dx, 1, np.pi / 2, current
+        [-1, 0, 1], [0, 0, 1], [-1, 0, 0], [0, 1, 0], dz, dx, 1, 90, current
     )
     arc_2 = CircularArcCurrentSource(
-        [-1, 0, -1], [-1, 0, 0], [0, 0, -1], [0, 1, 0], dz, dx, 1, np.pi / 2, current
+        [-1, 0, -1], [-1, 0, 0], [0, 0, -1], [0, 1, 0], dz, dx, 1, 90, current
     )
     arc_3 = CircularArcCurrentSource(
-        [1, 0, -1], [0, 0, -1], [1, 0, 0], [0, 1, 0], dz, dx, 1, np.pi / 2, current
+        [1, 0, -1], [0, 0, -1], [1, 0, 0], [0, 1, 0], dz, dx, 1, 90, current
     )
     arc_4 = CircularArcCurrentSource(
-        [1, 0, 1], [1, 0, 0], [0, 0, 1], [0, 1, 0], dz, dx, 1, np.pi / 2, current
+        [1, 0, 1], [1, 0, 0], [0, 0, 1], [0, 1, 0], dz, dx, 1, 90, current
     )
 
     solver = SourceGroup([bar_1, bar_2, bar_3, bar_4, arc_1, arc_2, arc_3, arc_4])

tests/magnetostatics/test_circular_arc.py
@@ -42,7 +42,7 @@ class TestCircularArcCurrentSource:
             cls.dx,
             cls.dz,
             cls.xc,
-            2 * np.pi,
+            360,
             cls.current,
         )
 