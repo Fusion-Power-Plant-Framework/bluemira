b9d9d854e7916feb35cc7d28cc567ef37375c7b6
james <james.cook1@ukaea.uk>
2023-09-18 07:35:19 +0100

    🚨 Cleanup and linting

--- scripts/openmc_data_download/multithreaded_download.py
+++ scripts/openmc_data_download/multithreaded_download.py
@@ -4,70 +4,63 @@ import functools
 import hashlib
 import os
 import signal
-import socket
 from pathlib import Path
-from typing import Optional
-from urllib.request import Request, urlopen
+from typing import Callable, Optional
 
 import requests
 from rich.progress import Progress
 
 
-async def get_size(url):
-    response = requests.head(url)
-    if int(response.headers["Content-Length"]) == 0:
-        with urlopen(url) as response:
-            return int(response.length)
+async def get_size(url: str, timeout: int = 10) -> int:
+    """Get size of file"""
+    response = requests.head(  # noqa: ASYNC100
+        url, allow_redirects=True, timeout=timeout
+    )
     return int(response.headers["Content-Length"])
 
 
 def download_range(
-    url,
-    start,
-    end,
-    output,
-    progress,
-    task_prog,
-    timeout=socket._GLOBAL_DEFAULT_TIMEOUT,
-    cafile=None,
-    capath=None,
-    cadefault=False,
-    context=None,
+    url: str,
+    start: int,
+    end: int,
+    output: Path,
+    progress: Callable[[], None],
+    timeout: int = 10,
 ):
-    page = Request(
-        url, headers={"User-Agent": "Mozilla/5.0", "Range": f"bytes={start}-{end}"}
-    )
-
-    if output.is_file() and output.stat().st_size == end + 1 - start:
-        progress.update(task_prog, advance=1)
-        return
-
-    with urlopen(
-        page,
-        timeout=timeout,
-        cafile=cafile,
-        capath=capath,
-        cadefault=cadefault,
-        context=context,
-    ) as response, open(output, "wb") as f:
-        while chunk := response.read(10000):
-            f.write(chunk)
-    progress.update(task_prog, advance=1)
+    """Download worker"""
+    if not (output.is_file() and output.stat().st_size == end + 1 - start):
+        headers = {"User-Agent": "Mozilla/5.0", "Range": f"bytes={start}-{end}"}
+        response = requests.get(url, headers=headers, timeout=timeout)
+        with open(output, "wb") as f:
+            for part in response.iter_content(1024):
+                f.write(part)
+    progress()
 
 
-async def download(run, url, output, chunk_size=10000000, **kwargs):
+async def _download(
+    run: Callable,
+    url: str,
+    output: Path,
+    *,
+    chunk_size: int = 10000000,
+    timeout: int = 10,
+):
+    """Downloader event loop"""
+    if not url.startswith(("http:", "https:")):
+        raise ValueError("Must be an http or https url")
+
     file_size = await get_size(url)
 
     if output.is_file() and output.stat().st_size == file_size:
         return
 
     chunks = range(0, file_size, chunk_size)
-
     with Progress() as progress:
-        task_prog = progress.add_task(
-            f"Downloading {output.parts[-1]}", total=len(chunks)
+        updater = functools.partial(
+            progress.update,
+            progress.add_task(f"Downloading {output.parts[-1]}", total=len(chunks)),
+            advance=1,
         )
-
         tasks = [
             run(
                 download_range,
@@ -75,20 +68,21 @@ async def download(run, url, output, chunk_size=10000000, **kwargs):
                 start,
                 start + chunk_size - 1,
                 Path(f"{output}.part{i}"),
-                progress,
-                task_prog,
-                *kwargs.values(),
+                updater,
+                timeout,
             )
             for i, start in enumerate(chunks)
         ]
 
         await asyncio.wait(tasks)
 
-    with open(output, "wb") as o:
+    with open(output, "wb") as o:  # noqa: ASYNC101
         for i in range(len(chunks)):
             chunk_path = Path(f"{output}.part{i}")
-            with open(chunk_path, "rb") as s:
+            with open(chunk_path, "rb") as s:  # noqa: ASYNC101
                 o.write(s.read())
+
+    # Write all before deleting
     for i in range(len(chunks)):
         chunk_path = Path(f"{output}.part{i}").unlink()
 
@@ -96,24 +90,14 @@ async def download(run, url, output, chunk_size=10000000, **kwargs):
 def downloader(
     url: str,
     checksum: Optional[int] = None,
-    as_browser: bool = False,
+    as_browser: bool = False,  # noqa: ARG001
     output_path: Optional[os.PathLike] = None,
     output_filename: Optional[os.PathLike] = None,
     *,
     max_workers: int = 5,
-    timeout=socket._GLOBAL_DEFAULT_TIMEOUT,
-    cafile=None,
-    capath=None,
-    cadefault=False,
-    context=None,
+    timeout: int = 10,
 ):
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
-    loop = asyncio.new_event_loop()
-    run = functools.partial(loop.run_in_executor, executor)
-
-    asyncio.set_event_loop(loop)
-
+    """Asynchronous multithreaded downloader"""
     if output_filename is None:
         output_filename = Path(url.rsplit("/", 1)[-1])
 
@@ -121,27 +105,25 @@ def downloader(
         local_path = Path(output_filename)
     else:
         Path(output_path).mkdir(parents=True, exist_ok=True)
-        local_path = Path(output_path) / output_filename
+        local_path = Path(output_path, output_filename)
+
+    # Reenable Keyboard Interrupt
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
+    loop = asyncio.new_event_loop()
+    run = functools.partial(loop.run_in_executor, executor)
+    asyncio.set_event_loop(loop)
 
     try:
-        loop.run_until_complete(
-            download(
-                run,
-                url,
-                local_path,
-                timeout=timeout,
-                cafile=cafile,
-                capath=capath,
-                cadefault=cadefault,
-                context=context,
-            )
-        )
+        loop.run_until_complete(_download(run, url, local_path, timeout=timeout))
     finally:
         loop.close()
 
     if (
         checksum is not None
-        and hashlib.md5(open(local_path, "rb").read()).hexdigest() != checksum
+        and hashlib.md5(open(local_path, "rb").read()).hexdigest()  # noqa: S324, SIM115
+        != checksum
     ):
         raise OSError(
             f"MD5 checksum for {local_path} does not match."
