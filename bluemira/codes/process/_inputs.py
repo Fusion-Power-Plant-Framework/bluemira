0e3f2309cc94d677fba9442f039d097b9a5d08f2
CoronelBuendia <matti.coleman@gmail.com>
2023-10-06 11:09:18 +0200

    adding endless names

--- bluemira/codes/process/_inputs.py
+++ bluemira/codes/process/_inputs.py
@@ -94,27 +94,28 @@ class ProcessInputs:
     cptdin: List[float] = field(
         default_factory=lambda: [*([42200.0] * 4), *([43000.0] * 4)]
     )
-    cpttf: float = 65000.0
+    cpttf: float = 70000.0
+    cpttf_max: float = 90000.0
     d_vv_bot: float = 0.6
     d_vv_in: float = 0.6
     d_vv_out: float = 1.1
     d_vv_top: float = 0.6
     ddwex: float = 0.15
     dene: float = 7.4321e19
-    dhecoil: float = 0.01
+    dhecoil: float = 0.005
     dintrt: float = 0.0
     discount_rate: float = 0.06
     divdum: int = 1
     divfix: float = 0.621
     dnbeta: float = 3.0
-    thkcas: float = 0.52465
+    thkcas: float = 0.3
     casthi: float = 0.06
-    emult: float = 1.35
+    emult: float = 1.269
     enbeam: float = 1e3
     epsvmc: float = 1e-08
     etaech: float = 0.4
-    etahtp: float = 0.87
-    etaiso: float = 0.9
+    etahtp: float = 0.95
+    etaiso: float = 0.85
     etanbi: float = 0.3
     etath: float = 0.375
     fbetatry: float = 0.48251
@@ -124,7 +125,7 @@ class ProcessInputs:
     fcontng: float = 0.15
     fcr0: float = 0.065
     fcuohsu: float = 0.7
-    fcutfsu: float = 0.80884
+    fcutfsu: float = 0.69
     fdene: float = 1.2
     ffuspow: float = 1.0
     fgwped: float = 0.85
@@ -163,7 +164,7 @@ class ProcessInputs:
     hfact: float = 1.1
     hldivlim: float = 10.0
     i_single_null: int = 1
-    i_tf_sc_mat: int = 5
+    i_tf_sc_mat: int = 1
     i_tf_turns_integer: int = 1
     ilhthresh: int = 6
     snull: int = 1
@@ -202,6 +203,7 @@ class ProcessInputs:
     minmax: int = 1
     n_layer: int = 10
     n_pancake: int = 20
+    n_rad_per_layer: int = 100
     n_tf: int = 16
     ncls: List[int] = field(default_factory=lambda: [1, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     neped: float = 6.78e19
@@ -216,7 +218,7 @@ class ProcessInputs:
     pinjalw: float = 51.0
     plasma_res_factor: float = 0.66
     pnetelin: float = 500.0
-    primary_pumping: int = 3
+    primary_pumping: int = 2
     prn1: float = 0.4
     psepbqarmax: float = 9.2
     pulsetimings: float = 0.0
@@ -226,7 +228,7 @@ class ProcessInputs:
     ralpne: float = 0.06894
     rhopedn: float = 0.94
     rhopedt: float = 0.94
-    ripmax: float = 0.6
+    ripmax: float = 1.0
     rjconpf: List[float] = field(default_factory=lambda: [30000000] * 22)
     vf: List[float] = field(default_factory=lambda: [0.3] * 22)
     rmajor: float = 8.8901
@@ -238,36 +240,39 @@ class ProcessInputs:
     shldlth: float = 1e-06
     shldoth: float = 1e-06
     shldtth: float = 1e-06
-    sig_tf_case_max: float = 580000000.0
-    sig_tf_wp_max: float = 580000000.0
+    sig_tf_case_max: float = 600000000.0
+    sig_tf_wp_max: float = 600000000.0
     ssync: float = 0.6
     tbeta: float = 2.0
     tbrnmn: float = 7200.0
     tburn: float = 1000.0
-    tdmptf: float = 25.829
-    tdwell: float = 0.0
+    tdmptf: float = 10.0
+    tdwell: float = 1800.0
+    theat: float = 10.0
+    tohs: float = 30.0
+    tqnch: float = 15.0
+    tramp: float = 15.0
     te: float = 12.33
     teped: float = 5.5
     tesep: float = 0.1
     tfcth: float = 1.208
     tftmp: float = 4.75
     tftsgap: float = 0.05
-    thicndut: float = 0.002
+    thicndut: float = 0.0008
     thshield_ib: float = 0
     thshield_ob: float = 0
     thshield_vb: float = 0
     thwcndut: float = 0.008
-    tinstf: float = 0.008
+    tinstf: float = 0.018
     tlife: float = 40.0
     tmargmin: float = 1.5
-    tramp: float = 500.0
     triang: float = 0.5
     ucblvd: float = 280.0
     ucdiv: float = 500000.0
     ucme: float = 300000000.0
-    vdalw: float = 10.0
-    vfshld: float = 0.6
-    vftf: float = 0.3
+    vdalw: float = 20.0
+    vfshld: float = 0.25
+    vftf: float = 0.4
     vgap2: float = 0.05
     vvblgap: float = 0.02
     walalw: float = 8.0
@@ -306,6 +311,158 @@ class ProcessInputs:
     # Undocumented danger stuff
     blk_life_csf: int = 1
     iblanket: int = 1
+    iblnkith: int = 1
+
+    # Top down of PROCESS variables list
+    denstl: float = 7800.0
+    denw: float = 19250.0
+
+    fblss: float = 0.09705
+    fdiv: float = 0.115
+    fwbsshape: int = 2
+    fw_armour_thickness: float = 0.005
+    li6enrich: float = 30.0
+    breeder_f: float = 0.5
+    breeder_multiplier: float = 0.75
+    vfcblkt: float = 0.05295
+    vfpblkt: float = 0.1
+    # f_neut_shield: float = # -1.0 the documentation defaults cannot be right...
+    breedmat: int = 1
+    fblbe: float = 0.6
+    fblbreed: float = 0.154
+    fblhebmi: float = 0.4
+    fblhebmo: float = 0.4
+    fblhebpi: float = 0.6595
+    fblhebpo: float = 0.6713
+    hcdportsize: int = 1
+    npdiv: int = 2
+    nphcdin: int = 2
+    nphcdout: int = 2
+    wallpf: float = 1.21
+    iblanket_thickness: int = 2
+    secondary_cycle_liq: int = 4
+
+    afwi: float = 0.008
+    afwo: float = 0.008
+    fw_wall: float = 0.003
+    afw: float = 0.006
+    pitch: float = 0.02
+    fwinlet: float = 573.0
+    fwoutlet: float = 823.0
+    fwpressure: float = 15500000.0
+    roughness: float = 1.0e-6
+    fw_channel_length: float = 4.0
+    peaking_factor: float = 1.0
+    blpressure: float = 15500000.0
+    inlet_temp: float = 573.0
+    outlet_temp: float = 823.0
+    coolp: float = 15500000.0
+    nblktmodpo: int = 8
+    nblktmodpi: int = 7
+    nblktmodto: int = 48
+    nblktmodti: int = 32
+    tfwmatmax: float = 823.0
+    fw_th_conductivity: float = 28.34
+    fvoldw: float = 1.74
+    fvolsi: float = 1.0
+    fvolso: float = 0.64
+    fwclfr: float = 0.15
+    rpf2dewar: float = 0.5
+    irefprop: int = 1
+    fblli2o: float = 0.08
+    fbllipb: float = 0.68
+    vfblkt: float = 0.25
+    declblkt: float = 0.075
+    declfw: float = 0.075
+    declshld: float = 0.075
+    blkttype: int = 3
+    n_liq_recirc: int = 10
+
+    bz_channel_conduct_liq: float = 833000.0
+
+    blpressure_liq: float = 1700000.0
+    inlet_temp_liq: float = 570.0
+    outlet_temp_liq: float = 720.0
+    f_nuc_pow_bz_struct: float = 0.34
+    pnuc_fw_ratio_dcll: float = 0.14
+
+    bcritsc: float = 24.0
+    casthi_fraction: float = 0.05
+    casths_fraction: float = 0.06
+    f_t_turn_tf: float = 1.0
+    t_turn_tf_max: float = 0.05000000074505806
+    dcase: float = 8000.0
+    dcond: List[float] = field(
+        default_factory=lambda: [
+            6080.0,
+            6080.0,
+            6070.0,
+            6080.0,
+            6080.0,
+            8500.0,
+            6070.0,
+            8500.0,
+            8500.0,
+        ]
+    )
+    dcondins: float = 1800.0
+    farc4tf: float = 0.7
+    b_crit_upper_nbti: float = 14.86
+    t_crit_nbti: float = 9.04
+    fhts: float = 0.5
+    i_tf_stress_model: int = 1
+    i_tf_cond_eyoung_trans: int = 1
+    i_tf_bucking = -1
+    n_tf_graded_layers: int = 1
+    n_tf_wp_layers: int = 5
+    jbus: float = 1250000.0
+    eyoung_ins: float = 100000000.0
+    eyoung_steel: float = 205000000000.0
+    eyong_cond_axial: float = 660000000.0
+    eyoung_res_tf_buck: float = 150000000000.0
+    eyoung_copper: float = 117000000000.0
+    eyoung_al: float = 69000000000.0
+    poisson_steel: float = 0.3
+    poisson_copper: float = 0.35
+    poisson_al: float = 0.35
+    poisson_ins: float = 0.34
+    poisson_cond_axial: float = 0.30000001192092896
+    poisson_cond_trans: float = 0.30000001192092896
+    toroidalgap: float = 1.0
+    n_radial_array: int = 50
+    str_cs_con_res: float = -0.005
+    str_pf_con_res: float = -0.005
+    str_tf_con_res: float = -0.005
+    str_wp_max: float = 0.007
+    i_str_wp: int = 1
+    quench_model: str = b"exponential"
+    tcritsc: float = 16.0
+    rhotfbus: float = -1.0
+    frhocp: float = 1.0
+    frholeg: float = 1.0
+    i_cp_joints: int = -1
+    rho_tf_joints: float = 2.5e-10
+    n_tf_joints_contact: int = 6
+    n_tf_joints: int = 4
+    th_joint_contact: float = 0.03
+    eff_tf_cryo: float = -1.0
+    tftort: float = 1.0
+    tmaxpro: float = 150.0
+    tmax_croco: float = 200.0
+    tmpcry: float = 4.5
+    v_force_inboard: float = 0.5
+    etapump: float = 0.8
+    fcoolcp: float = 0.3
+    fcoolleg: float = 0.2
+    ptempalw: float = 473.15
+    rcool: float = 0.005
+    tcoolin: float = 313.15
+    tcpav: float = 373.15
+    tlegav: float = -1.0
+    vcool: float = 20.0
+    theta1_coil: float = 45.0
+    theta1_vv: float = 1.0
+
     # Inputs with defaults for JIC sanity
     i_pf_current: int = 1
     nfxfh: int = 7
