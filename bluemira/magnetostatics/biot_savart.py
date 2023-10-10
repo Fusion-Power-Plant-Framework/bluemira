Improve self-inductance calculation and fix bug in `circular_coil_inductance_elliptic` (#2411)
* improve and remove longrun flag

* flake8

* JIT elliptic inductance

* add figure labels

* minor

* rename bar to hat

* pause

* add error tail and rectangular test

* minor cleanup

* minor error shift

* remove print statement in test

bluemira/magnetostatics/biot_savart.py
@@ -218,17 +218,21 @@ class BiotSavartFilament(CurrentSource):
         for _i, (x1, dx1) in enumerate(zip(self.ref_mid_points, self.ref_d_l)):
             # We create a mask to drop the point where x1 == x2
             r = x1 - self.mid_points
-            mask = np.sum(r**2, axis=1) > self.radius
+            mask = np.sum(r**2, axis=1) > 0.5 * self.length_scale
             inductance += np.sum(
                 np.dot(dx1, self.d_l[mask].T) / np.linalg.norm(r[mask], axis=1)
             )
 
         # Self-inductance correction (Y = 0.5 for homogenous current distribution)
-        inductance += (
-            2 * self.length * (np.log(2 * self.length_scale / self.radius) + 0.25)
-        )
+        # Equation 6 of https://arxiv.org/pdf/1204.1486.pdf
+        error_tail = 0
+        a, b = self.radius, 0.5 * self.length_scale
+        if b > 10 * a:
+            # Equation A.4 of https://arxiv.org/pdf/1204.1486.pdf
+            error_tail = a**2 / b**2 - 3 / (8 * b**4) * (a**4 - 2 * a**2)
+        l_hat_0 = self.length * (2 * np.log(2 * b / a) + 0.5) + error_tail
 
-        return MU_0_4PI * inductance
+        return MU_0_4PI * (inductance + l_hat_0)
 
     def rotate(self, angle: float, axis: Union[str, np.ndarray]):
         """

bluemira/magnetostatics/greens.py
@@ -102,6 +102,7 @@ def ellipk_nb(k: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
 ellipk_nb.__doc__ += ellipk.__doc__
 
 
+@nb.jit(nopython=True)
 def circular_coil_inductance_elliptic(radius: float, rc: float) -> float:
     """
     Calculate the inductance of a circular coil by elliptic integrals.
@@ -118,11 +119,8 @@ def circular_coil_inductance_elliptic(radius: float, rc: float) -> float:
     The self-inductance of the circular coil [H]
     """
     k = 4 * radius * (radius - rc) / (2 * radius - rc) ** 2
-    args = (
-        np.array(arg, dtype=np.float64) for arg in (k, GREENS_ZERO, 1.0 - GREENS_ZERO)
-    )
-    k = clip_nb(*args)
-    return MU_0 * (2 * radius - rc) * ((1 - k**2 / 2) * ellipk(k) - ellipe(k))
+    k = clip_nb(k, GREENS_ZERO, 1.0 - GREENS_ZERO)
+    return MU_0 * (2 * radius - rc) * ((1 - k**2 / 2) * ellipk_nb(k) - ellipe_nb(k))
 
 
 def circular_coil_inductance_kirchhoff(radius: float, rc: float) -> float:

tests/magnetostatics/test_biot_savart.py
@@ -25,7 +25,8 @@ import pytest
 
 from bluemira.base.constants import MU_0
 from bluemira.display.auto_config import plot_defaults
-from bluemira.geometry.tools import make_circle
+from bluemira.geometry.coordinates import Coordinates
+from bluemira.geometry.tools import make_circle, make_polygon
 from bluemira.magnetostatics.biot_savart import BiotSavartFilament
 from bluemira.magnetostatics.greens import (
     circular_coil_inductance_elliptic,
@@ -156,30 +157,92 @@ def plot_errors(x, z, Bx, Bz, Bp, Bx2, Bz2, Bp2):
     plt.close(f)
 
 
-@pytest.mark.longrun
-def test_inductance():
-    radii = np.linspace(1, 100, 100)
-    rci = np.linspace(0.0001, 1, 100)
-    ind, ind2 = np.zeros((100, 100)), np.zeros((100, 100))
+class TestSelfInductance:
+    def test_circular_inductance(self):
+        n = 25
+        radii = np.linspace(1, 100, n)
+        rci = np.linspace(0.0001, 1, n)
+        ind, ind2 = np.zeros((n, n)), np.zeros((n, n))
+        ind3 = np.zeros((n, n))
 
-    ind3 = np.zeros((100, 100))
-    for i, r in enumerate(radii):
-        circle = make_circle(r, center=(0, 0, 0), axis=(0, 1, 0))
-        filament = circle.discretize(ndiscr=200)
-        for j, rc in enumerate(rci):
-            bsf = BiotSavartFilament(filament, rc)
-            ind[i, j] = circular_coil_inductance_elliptic(r, rc)
-            ind2[i, j] = circular_coil_inductance_kirchhoff(r, rc)
-            ind3[i, j] = bsf.inductance()
+        for i, r in enumerate(radii):
+            circle = make_circle(r, center=(0, 0, 0), axis=(0, 1, 0))
+            filament = circle.discretize(dl=circle.length / 50)
+            for j, rc in enumerate(rci):
+                bsf = BiotSavartFilament(filament, rc)
+                ind[i, j] = circular_coil_inductance_elliptic(r, rc)
+                ind2[i, j] = circular_coil_inductance_kirchhoff(r, rc)
+                ind3[i, j] = bsf.inductance()
 
-    levels = np.linspace(np.amin(ind), np.amax(ind), 20)
-    f, ax = plt.subplots(1, 4)
-    xx, yy = np.meshgrid(radii, rci)
-    ax[0].contourf(xx, yy, ind, levels=levels)
-    ax[1].contourf(xx, yy, ind2, levels=levels)
-    ax[2].contourf(xx, yy, ind3, levels=levels)
-    diff = 100 * (ind - ind3) / ind
-    cm = ax[3].contourf(xx, yy, diff, levels=np.linspace(-10, 10, 100))
-    f.colorbar(cm)
-    plt.show()
-    plt.close(f)
+        levels = np.linspace(np.amin(ind), np.amax(ind), 20)
+        f, ax = plt.subplots(1, 4)
+        xx, yy = np.meshgrid(radii, rci)
+        ax[0].contourf(xx, yy, ind, levels=levels)
+        ax[0].set_title("Elliptic")
+        ax[1].contourf(xx, yy, ind2, levels=levels)
+        ax[1].set_title("Kirchoff")
+        ax[2].contourf(xx, yy, ind3, levels=levels)
+        ax[2].set_title("Biot-Savart")
+
+        for a in ax:
+            a.set_xlabel("radius [m]")
+            a.set_ylabel("filament radius [m]")
+
+        diff = 100 * (ind2 - ind3) / ind2
+        cm = ax[3].contourf(xx, yy, diff, levels=np.linspace(-5, 5, 100))
+        ax[3].set_title("K - (BS / K)")
+        cb = f.colorbar(cm)
+        cb.set_label("%")
+        plt.subplots_adjust(wspace=0.5)
+        plt.show()
+        plt.close(f)
+        res = np.sum((ind2 - ind3) ** 2)
+        tot = np.sum((ind2 - np.average(ind2)) ** 2)
+        r2 = 1 - res / tot
+        assert r2 > 0.998
+
+    @pytest.mark.parametrize("discr", [False, True])
+    @pytest.mark.parametrize(
+        ("a", "c", "d", "f_error"),
+        [
+            (1, 10, 10, 0.13),
+            (1, 20, 20, 0.1),
+            (1, 40, 40, 0.8),
+            (1, 80, 80, 0.7),
+            (1, 160, 160, 0.6),
+        ],
+    )
+    def test_rectangular_inductance(
+        self, discr: bool, a: float, c: float, d: float, f_error: float
+    ):
+        """
+        Attempt at replicating results from https://arxiv.org/pdf/1204.1486.pdf
+        for a rectangle. Differences include discretisation and lack of corners,
+        and the lack of knowledge of `d`, assumed here as `d`=`c`.
+        """
+        poly = Coordinates(
+            {
+                "x": [-c / 2, c / 2, c / 2, -c / 2],
+                "y": 0,
+                "z": [-d / 2, -d / 2, d / 2, d / 2],
+            }
+        )
+        poly.close()
+        if discr:
+            poly = make_polygon(poly, closed=True).discretize(dl=12 * a, byedges=True)
+        filament = BiotSavartFilament(poly, radius=a)
+        inductance = filament.inductance()
+        exact = (
+            MU_0
+            / np.pi
+            * (
+                c * np.log(2 * c / a)
+                + d * np.log(2 * d / a)
+                - (c + d) * (2 - 0.5 / 2)
+                + 2 * np.hypot(c, d)
+                - c * np.arcsinh(c / d)
+                - d * np.arcsinh(d / c)
+                + a
+            )
+        )
+        assert abs(1 - inductance / exact) < f_error