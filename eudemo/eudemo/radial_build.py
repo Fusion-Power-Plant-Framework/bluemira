798cae3605b460f41b2c394412d5080f6af89eab
james <james.cook1@ukaea.uk>
2023-10-13 12:49:51 +0100

    🎨 Add runtitle

--- eudemo/eudemo/radial_build.py
+++ eudemo/eudemo/radial_build.py
@@ -317,8 +317,6 @@ template_builder.add_input_values(
     }
 )
 
-template = template_builder.make_inputs()
-
 
 def radial_build(params: _PfT, build_config: Dict) -> _PfT:
     """
@@ -338,7 +336,10 @@ def radial_build(params: _PfT, build_config: Dict) -> _PfT:
     run_mode = build_config.pop("run_mode", "mock")
     plot = build_config.pop("plot", False)
     if run_mode == "run":
-        build_config["template_in_dat"] = template
+        template_builder.set_run_title(
+            build_config.pop("PROCESS_runtitle", "Bluemira EUDEMO")
+        )
+        build_config["template_in_dat"] = template_builder.make_inputs()
     solver = systems_code_solver(params, build_config)
     new_params = solver.execute(run_mode)
 
