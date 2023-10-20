44593f035780c60496ce647dbc102ea9fcb76884
CoronelBuendia <matti.coleman@gmail.com>
2023-10-20 16:39:46 +0200

    Add an alternative polyhedral VIM formulation (#2731)
    * working bottura
    
    * get rid of some minuses
    
    * kernel start
    
    * docstrings
    
    * ruff
    
    * make tests better written
    
    * more maths
    
    * fix API documentation

--- tests/magnetostatics/test_polyhedral_prism.py
+++ tests/magnetostatics/test_polyhedral_prism.py
@@ -19,13 +19,19 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with bluemira; if not, see <https://www.gnu.org/licenses/>.
 
+from copy import deepcopy
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
 from bluemira.base.constants import EPS, raw_uc
 from bluemira.geometry.tools import Coordinates
-from bluemira.magnetostatics.polyhedral_prism import PolyhedralPrismCurrentSource
+from bluemira.magnetostatics.polyhedral_prism import (
+    Bottura,
+    Fabbri,
+    PolyhedralPrismCurrentSource,
+)
 from bluemira.magnetostatics.trapezoidal_prism import TrapezoidalPrismCurrentSource
 
 
@@ -91,10 +97,15 @@ class TestPolyhedralMaths:
     )
     test_cases = (same_angle, diff_angle)
 
+    @pytest.mark.parametrize("kernel", ["Fabbri", "Bottura"])
     @pytest.mark.parametrize(("trap", "poly"), test_cases)
     def test_geometry(
-        self, trap: TrapezoidalPrismCurrentSource, poly: PolyhedralPrismCurrentSource
+        self,
+        kernel: str,
+        trap: TrapezoidalPrismCurrentSource,
+        poly: PolyhedralPrismCurrentSource,
     ):
+        poly._kernel = Fabbri() if kernel == "Fabbri" else Bottura()
         poly.plot()
         ax = plt.gca()
         trap.plot(ax)
@@ -112,10 +123,15 @@ class TestPolyhedralMaths:
         for i in range(len(trap._points)):
             np.testing.assert_allclose(trap._points[i], poly._points[i])
 
+    @pytest.mark.parametrize("kernel", ["Fabbri", "Bottura"])
     @pytest.mark.parametrize(("trap", "poly"), test_cases)
     def test_xz_field(
-        self, trap: TrapezoidalPrismCurrentSource, poly: PolyhedralPrismCurrentSource
+        self,
+        kernel: str,
+        trap: TrapezoidalPrismCurrentSource,
+        poly: PolyhedralPrismCurrentSource,
     ):
+        poly._kernel = Fabbri() if kernel == "Fabbri" else Bottura()
         f = plt.figure()
         ax = f.add_subplot(1, 2, 1, projection="3d")
         ax.set_title("TrapezoidalPrism")
@@ -132,20 +148,25 @@ class TestPolyhedralMaths:
         f.colorbar(cm)
 
         ax = f.add_subplot(1, 2, 2, projection="3d")
-        ax.set_title("PolyhedralPrism")
+        ax.set_title(f"PolyhedralPrism {kernel}")
         poly.plot(ax)
-
         Bx, By, Bz = poly.field(xx, yy, zz)
         B_new = np.sqrt(Bx**2 + By**2 + Bz**2)
         cm = ax.contourf(xx, B_new, zz, zdir="y", offset=0)
         f.colorbar(cm)
         plt.show()
+
         np.testing.assert_allclose(B_new, B)
 
+    @pytest.mark.parametrize("kernel", ["Fabbri", "Bottura"])
     @pytest.mark.parametrize(("trap", "poly"), test_cases)
     def test_xy_field(
-        self, trap: TrapezoidalPrismCurrentSource, poly: PolyhedralPrismCurrentSource
+        self,
+        kernel: str,
+        trap: TrapezoidalPrismCurrentSource,
+        poly: PolyhedralPrismCurrentSource,
     ):
+        poly._kernel = Fabbri() if kernel == "Fabbri" else Bottura()
         n = 50
         x = np.linspace(8, 12, n)
         y = np.linspace(-2, 2, n)
@@ -163,19 +184,25 @@ class TestPolyhedralMaths:
         f.colorbar(cm)
 
         ax = f.add_subplot(1, 2, 2, projection="3d")
-        ax.set_title("PolyhedralPrism")
+        ax.set_title(f"PolyhedralPrism {kernel}")
         poly.plot(ax)
         Bx, By, Bz = poly.field(xx, yy, zz)
         B_new = np.sqrt(Bx**2 + By**2 + Bz**2)
         cm = ax.contourf(xx, yy, B_new, zdir="z", offset=0)
         f.colorbar(cm)
         plt.show()
+
         np.testing.assert_allclose(B_new, B)
 
+    @pytest.mark.parametrize("kernel", ["Fabbri", "Bottura"])
     @pytest.mark.parametrize(("trap", "poly"), test_cases)
     def test_yz_field(
-        self, trap: TrapezoidalPrismCurrentSource, poly: PolyhedralPrismCurrentSource
+        self,
+        kernel: str,
+        trap: TrapezoidalPrismCurrentSource,
+        poly: PolyhedralPrismCurrentSource,
     ):
+        poly._kernel = Fabbri() if kernel == "Fabbri" else Bottura()
         n = 50
         y = np.linspace(-2, 2, n)
         z = np.linspace(-2, 2, n)
@@ -192,13 +219,14 @@ class TestPolyhedralMaths:
         f.colorbar(cm)
 
         ax = f.add_subplot(1, 2, 2, projection="3d")
-        ax.set_title("PolyhedralPrism")
+        ax.set_title(f"PolyhedralPrism {kernel}")
         poly.plot(ax)
         Bx, By, Bz = poly.field(xx, yy, zz)
         B_new = np.sqrt(Bx**2 + By**2 + Bz**2)
         cm = ax.contourf(B_new, yy, zz, zdir="x", offset=10)
         f.colorbar(cm)
         plt.show()
+
         np.testing.assert_allclose(B_new, B)
 
     def teardown_method(self):
@@ -206,40 +234,42 @@ class TestPolyhedralMaths:
 
 
 class TestPolyhedralPrismBabicAykel:
-    @classmethod
-    def setup_class(cls):
-        """
-        Verification test.
+    """
+    Verification test.
 
-        Babic and Aykel example
+    Babic and Aykel example
 
-        https://onlinelibrary.wiley.com/doi/epdf/10.1002/jnm.594
-        """
-        # Babic and Aykel example (single trapezoidal prism)
-        cls.trap = TrapezoidalPrismCurrentSource(
-            np.array([0, 0, 0]),
-            np.array([2 * 2.154700538379251, 0, 0]),  # This gives b=1
-            np.array([0, 1, 0]),
-            np.array([0, 0, 1]),
-            1,
-            1,
-            60.0,
-            30.0,
-            4e5,
-        )
-        cls.poly = PolyhedralPrismCurrentSource(
-            np.array([0, 0, 0]),
-            np.array([2 * 2.154700538379251, 0, 0]),  # This gives b=1
-            np.array([0, 1, 0]),
-            np.array([0, 0, 1]),
-            make_xs_from_bd(1, 1),
-            60.0,
-            30.0,
-            4e5,
-        )
+    https://onlinelibrary.wiley.com/doi/epdf/10.1002/jnm.594
+    """
 
+    # Babic and Aykel example (single trapezoidal prism)
+    trap = TrapezoidalPrismCurrentSource(
+        np.array([0, 0, 0]),
+        np.array([2 * 2.154700538379251, 0, 0]),  # This gives b=1
+        np.array([0, 1, 0]),
+        np.array([0, 0, 1]),
+        1,
+        1,
+        60.0,
+        30.0,
+        4e5,
+    )
+    poly = PolyhedralPrismCurrentSource(
+        np.array([0, 0, 0]),
+        np.array([2 * 2.154700538379251, 0, 0]),  # This gives b=1
+        np.array([0, 1, 0]),
+        np.array([0, 0, 1]),
+        make_xs_from_bd(1, 1),
+        60.0,
+        30.0,
+        4e5,
+    )
+    poly2 = deepcopy(poly)
+    poly2._kernel = Bottura()
+
+    @pytest.mark.parametrize("poly", [poly, poly2])
     @pytest.mark.parametrize("plane", ["x", "y", "z"])
-    def test_plot(self, plane):
+    def test_plot(self, poly, plane):
         xx, yy, zz, i, j, k = plane_setup(plane)
 
         f = plt.figure()
@@ -255,8 +285,8 @@ class TestPolyhedralPrismBabicAykel:
 
         ax = f.add_subplot(1, 3, 2, projection="3d")
         ax.set_title("PolyhedralPrism")
-        self.poly.plot(ax)
-        Bx, By, Bz = self.poly.field(xx, yy, zz)
+        poly.plot(ax)
+        Bx, By, Bz = poly.field(xx, yy, zz)
         B_new = np.sqrt(Bx**2 + By**2 + Bz**2)
         args_new = [xx, yy, zz, B_new]
         cm = ax.contourf(args_new[i], args_new[j], args_new[k], zdir=plane, offset=0)
@@ -265,26 +295,28 @@ class TestPolyhedralPrismBabicAykel:
         ax = f.add_subplot(1, 3, 3, projection="3d")
         ax.set_title("difference [%]")
         args_diff = [xx, yy, zz, 100 * (B - B_new) / B]
-        self.poly.plot(ax)
+        poly.plot(ax)
         cm = ax.contourf(args_diff[i], args_diff[j], args_diff[k], zdir=plane, offset=0)
         f.colorbar(cm)
         plt.show()
         np.testing.assert_allclose(B_new, B)
 
+    @pytest.mark.parametrize("poly", [poly, poly2])
     @pytest.mark.parametrize(
         ("point", "value", "precision"),
         [((2, 2, 2), 15.5533805, 7), ((1, 1, 1), 53.581000397, 9)],
     )
-    def test_paper_singularity_values(self, point, value, precision):
-        field = self.poly.field(*point)
+    def test_paper_singularity_values(self, poly, point, value, precision):
+        field = poly.field(*point)
         abs_field = raw_uc(np.sqrt(sum(field**2)), "T", "mT")  # Field in mT
         # As per Babic and Aykel paper
         # Assume truncated last digit and not rounded...
         field_ndecimals = np.trunc(abs_field * 10**precision) / 10**precision
         assert field_ndecimals == pytest.approx(value, rel=0, abs=EPS)
 
-    def test_paper_inside_conductor(self):
-        field = self.poly.field(0.5, 0.5, 0.5)
+    @pytest.mark.parametrize("poly", [poly, poly2])
+    def test_paper_inside_conductor(self, poly):
+        field = poly.field(0.5, 0.5, 0.5)
         abs_field = raw_uc(np.sqrt(sum(field**2)), "T", "mT")  # Field in mT
         # As per Babic and Aykel paper
         # Assume truncated last digit and not rounded...
