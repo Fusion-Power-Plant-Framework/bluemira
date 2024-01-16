1cd4c3a3a7540c517fdf003f7f82146aa7255b63
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2024-01-16 11:46:35 +0100

    minor corrections

--- bluemira/power_cycle/tools.py
+++ bluemira/power_cycle/tools.py
@@ -9,15 +9,15 @@ Utility functions for the power cycle model.
 """
 
 import json
+import pprint
+from dataclasses import asdict, is_dataclass
 from typing import Any, Dict
 
 import matplotlib.pyplot as plt
 
 
 def read_json(file_path) -> Dict[str, Any]:
-    """
-    Returns the contents of a 'json' file.
-    """
+    """Return the contents of a 'json' file."""
     with open(file_path) as json_file:
         return json.load(json_file)
 
@@ -31,3 +31,10 @@ def create_axes(ax=None):
     if ax is None:
         _, ax = plt.subplots()
     return ax
+
+
+def pp(obj):
+    """Prety Printer compatible with dataclasses."""
+    if is_dataclass(obj):
+        return pprint.pp(asdict(obj), sort_dicts=False, indent=4)
+    return pprint.pp(obj, indent=4)
