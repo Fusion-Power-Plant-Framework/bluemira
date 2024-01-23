b9b6486045285c24a7c99bf3928f3fe39b0a69a1
james <james.cook1@ukaea.uk>
2024-01-23 15:52:48 +0000

    🧪 Failing test

--- eudemo/eudemo_tests/maintenance/test_lower_port.py
+++ eudemo/eudemo_tests/maintenance/test_lower_port.py
@@ -68,8 +68,10 @@ class TestLowerPort:
         )
 
     @pytest.mark.parametrize("duct_angle", [0, -30, -45, -60, -90])
-    def test_duct_angle(self, duct_angle):
+    @pytest.mark.parametrize("tf_wp_depth", np.linspace(0, 1, 5))
+    def test_duct_angle(self, duct_angle, tf_wp_depth):
         self.duct_des_params.lower_port_angle.value = duct_angle
+        self.duct_des_params.tf_wp_depth.value = tf_wp_depth
 
         (
             lp_duct_xz_void_space,
@@ -123,3 +125,70 @@ class TestLowerPort:
         angled_face = duct_xyz_cad.faces[0]  # this was an angled face when I tested it
 
         np.testing.assert_allclose(angled_face.normal_at(), pl.normal_at(), atol=1e-8)
+
+
+class TestLowerPortKOZDesigner:
+    @classmethod
+    def setup_class(cls):
+        cls.divertor_xz_silhouette = BluemiraWire([
+            make_polygon([
+                [4, 6],
+                [0, 0],
+                [0, 0],
+            ]),
+            make_circle(
+                1, center=(5, 0, 0), start_angle=-180, end_angle=0, axis=(0, -1, 0)
+            ),
+        ])
+        cls.tf_coils_outer_boundary = BluemiraWire([
+            make_polygon([
+                [3, 3],
+                [0, 0],
+                [8, -8],
+            ]),
+            make_circle(
+                8, center=(3, 0, 0), start_angle=-90, end_angle=90, axis=(0, -1, 0)
+            ),
+        ])
+
+    def setup_method(self):
+        self.duct_des_params = make_parameter_frame(
+            {
+                "n_TF": {"value": 10, "unit": "dimensionless"},
+                "n_div_cassettes": {"value": 3, "unit": "dimensionless"},
+                "lower_port_angle": {"value": -30, "unit": "degrees"},
+                "g_ts_tf": {"value": 0.05, "unit": "m"},
+                "tk_ts": {"value": 0.05, "unit": "m"},
+                "g_vv_ts": {"value": 0.05, "unit": "m"},
+                "tk_vv_single_wall": {"value": 0.06, "unit": "m"},
+                "tf_wp_depth": {"value": 0.5, "unit": "m"},
+                "lp_duct_div_pad_ob": {"value": 0.3, "unit": "m"},
+                "lp_duct_div_pad_ib": {"value": 0.1, "unit": "m"},
+                "lp_height": {"value": 4.5, "unit": "m"},
+                "lp_width": {"value": 3, "unit": "m"},
+            },
+            LowerPortKOZDesignerParams,
+        )
+
+    @pytest.mark.parametrize("duct_angle", [0, -30, -45, -60, -90])
+    @pytest.mark.parametrize("tf_wp_depth", np.linspace(0, 1, 5))
+    def test_straight_duct_boundingbox_is_larger_than_angled_duct(self, duct_angle):
+        self.duct_des_params.lower_port_angle.value = duct_angle
+
+        (
+            _,
+            _,
+            lp_duct_angled_nowall_extrude_boundary,
+            lp_duct_straight_nowall_extrude_boundary,
+        ) = LowerPortKOZDesigner(
+            self.duct_des_params,
+            {},
+            self.divertor_xz_silhouette,
+            (5.5, 0),
+            self.tf_coils_outer_boundary,
+        ).execute()
+
+        angle_bb = lp_duct_angled_nowall_extrude_boundary.bounding_box
+        straight_bb = lp_duct_straight_nowall_extrude_boundary.bounding_box
+        assert angle_bb.y_max <= straight_bb.y_max
+        assert angle_bb.y_min >= straight_bb.y_min
