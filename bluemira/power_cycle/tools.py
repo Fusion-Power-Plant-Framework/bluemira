21102434d0571a86080660ba1f8e451477b7a626
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2024-01-26 16:10:29 +0100

    add `unique_domains` function to `tools.py`

--- bluemira/power_cycle/tools.py
+++ bluemira/power_cycle/tools.py
@@ -165,11 +165,17 @@ def match_domains(
     """
     Match the domains of multiple functions, each represented by 2 vectors.
 
-    For each pair of vectors (x,y), interpolate values in every 'y' vector,
+    First, for each pair of vectors (x,y) that define a function, the domain
+    x is ensured to be unique with 'unique_domain' (otherwise, calling 'unique'
+    on 'matched_x' later can neglect step functions).
+
+    Then, for each pair (x,y), values in every 'y' vector are interpolated,
     to ensure that all of them have one element associated to every element
     of the union of all distinct 'x' vectors.
     """
     n_vectors = len(x_set)
+    for v in range(n_vectors):
+        x_set[v], y_set[v] = unique_domain(x_set[v], y_set[v])
 
     matched_x = np.concatenate(x_set)
     matched_x = np.unique(matched_x)
@@ -182,3 +188,34 @@ def match_domains(
         y_set[v] = y
 
     return matched_x, y_set
+
+
+def unique_domain(x, y, epslon=1e-10):
+    """
+    Ensure x has only unique values to make (Domain: x -> Image: y) a function.
+
+    Nudge forward each non-unique element in x by (N * epslon), given N times,
+    after the first appearance, that value has appeared in x before.
+
+    Epslon must be small enough so that consecutive values can be considered
+    equal within the context/scale in which the function is defined.
+    """
+    n_points = len(x)
+    if len(y) != n_points:
+        raise ValueError("x and y must have the same number of elements.")
+    new_x = [x[0]]
+    new_y = [y[0]]
+
+    nudge = 0
+    for p in range(1, n_points):
+        x_last = x[p - 1]
+        x_this = x[p]
+        y_this = y[p]
+        if x_last == x_this:
+            nudge += epslon
+            x_this = x_last + nudge
+        else:
+            nudge = 0
+        new_x.append(x_this)
+        new_y.append(y_this)
+    return new_x, new_y
