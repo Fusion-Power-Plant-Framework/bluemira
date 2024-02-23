120d8c5ba4622aa3e22918853acdba777c2fa254
ocean <OceanWongUK@gmail.com>
2024-02-23 13:25:38 +0000

    Updated script for creating neutronics csg

--- examples/radiation_transport/test_script.py
+++ examples/radiation_transport/test_script.py
@@ -9,13 +9,18 @@ import json
 from pathlib import Path
 
 import numpy as np
+from numpy import typing as npt
+import matplotlib.pyplot as plt
 
 import bluemira.neutronics.make_geometry as mg
 from bluemira.base.constants import raw_uc
-from bluemira.display import plot_2d, show_cad  # noqa: F401
-from bluemira.geometry.coordinates import Coordinates
-from bluemira.geometry.tools import deserialize_shape, make_polygon
-from bluemira.neutronics.make_csg import ThicknessFractions
+from bluemira.display import plot_2d, show_cad, plot_3d # noqa: F401
+from bluemira.geometry.coordinates import Coordinates, vector_intersect
+from bluemira.geometry.tools import deserialize_shape, make_polygon  # make_circle_arc_3P
+from bluemira.geometry.wire import BluemiraWire
+from bluemira.neutronics.make_csg import (ThicknessFractions,
+                                        fill_xz_to_3d,
+                                        split_blanket_into_precells,)
 from bluemira.neutronics.make_materials import BlanketType
 from bluemira.neutronics.neutronics_axisymmetric import (
     PlasmaSourceParametersPPS,
@@ -98,29 +103,67 @@ else:
         source_parameters.plasma_physics_units.shaf_shift,
     )
 setup_openmc(source, tbr_heat_sim.runtime_variables)
-blanket_points, div_points, num_inboard_points = mg.load_fw_points(
-    source_parameters,
-    make_polygon(Coordinates(np.load("blanket_face.npy"))),
-    make_polygon(Coordinates(np.load("divertor_face.npy"))),
-    new_major_radius=raw_uc(9.0, "m", "cm"),
-    new_aspect_ratio=3.10344,  # [dimensionless]
-    new_elong=1.792,  # [dimensionless]
-    save_plots=True,
-)  # TODO: improve here
+thickness_fractions = ThicknessFractions.from_TokamakGeometry(tokamak_geometry)
+# blanket_points, div_points, num_inboard_points = mg.load_fw_points(
+#     source_parameters,
+#     make_polygon(Coordinates(np.load("blanket_face.npy"))),
+#     make_polygon(Coordinates(np.load("divertor_face.npy"))),
+#     new_major_radius=raw_uc(9.0, "m", "cm"),
+#     new_aspect_ratio=3.10344,  # [dimensionless]
+#     new_elong=1.792,  # [dimensionless]
+#     save_plots=True,
+# data loading begins here.
+from time import time
+def elapsed(start_time=time()):
+    return str(time()-start_time)+"s"
+print(elapsed())
 with open("data/inner_boundary") as j:
     inner_boundary = deserialize_shape(json.load(j))
 with open("data/outer_boundary") as j:
     outer_boundary = deserialize_shape(json.load(j))
-from data.load_divertor import divertor_bmwire
-fw_panel_list = [
+with open("data/divertor_face.correct.json") as j:
+    divertor_bmwire = deserialize_shape(json.load(j))
+fw_panel_bp_list = [
     np.load("data/fw_panels_10_0.1.npy"),
     np.load("data/fw_panels_25_0.1.npy"),
     np.load("data/fw_panels_25_0.3.npy"),
     np.load("data/fw_panels_50_0.3.npy"),
     np.load("data/fw_panels_50_0.5.npy"),
 ]
+panel_breakpoint_T = fw_panel_bp_list[1].T
+# TODO: MANUAL FIX begins here
+panel_breakpoint_T[0] = vector_intersect(panel_breakpoint_T[0], panel_breakpoint_T[1], divertor_bmwire.edges[0].start_point()[::2].flatten(), divertor_bmwire.edges[0].end_point()[::2].flatten())
+panel_breakpoint_T[-1]= vector_intersect(panel_breakpoint_T[-2],panel_breakpoint_T[-1],divertor_bmwire.edges[-1].start_point()[::2].flatten(), divertor_bmwire.edges[-1].end_point()[::2].flatten())
+# TODO: MANUALLY determine where on the outer boundary we want to set the Z points.
+
+last_point = divertor_bmwire.edges[-1].end_point()
+
+def polygon_from2D(xarray: npt.NDArray[float], zarray: npt.NDArray[float], **kwargs):
+    """Create BluemiraWire from  x coordinates and z coordinates.
+    Parameters
+    ----------
+    xarray: np.ndarray of shape (N,)
+    zarray: np.ndarray of shape (N,)
+
+    Returns
+    -------
+    BluemiraWire:
+        wire made of straight lines joint by vertices specified by the input parameters.
+    """
+    return make_polygon(fill_xz_to_3d([xarray, zarray]), **kwargs)
+
+blanket_panels_bmwire = polygon_from2D(
+                    *panel_breakpoint_T.T,
+                    label="blanket panels",
+                    closed=False,
+                )
+
+print(elapsed())
+precell_list = split_blanket_into_precells(outer_boundary, panel_breakpoint_T,
+                                            ending_cut=last_point.xz.flatten())
+print(elapsed())
+plot_2d([c.outline for c in precell_list])
 import sys; sys.exit()
-thickness_fractions = ThicknessFractions.from_TokamakGeometry(tokamak_geometry)
 
 tbr_heat_sim.cells, tbr_heat_sim.universe = mg.make_neutronics_geometry(
     tokamak_geometry,
