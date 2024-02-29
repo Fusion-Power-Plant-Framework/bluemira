6f888bff75c8c77242ae8cabe980ceaad58ce6a5
je-cook <81617086+je-cook@users.noreply.github.com>
2024-02-29 12:56:32 +0000

    Apply suggestions from code review

--- bluemira/geometry/tools.py
+++ bluemira/geometry/tools.py
@@ -734,7 +734,7 @@ def convex_hull_wires_2d(
         plane_idxs = (0, 2)
     elif hull_plane is HullPlane.XY:
         plane_idxs = (0, 1)
-    else:  # YZ
+    elif hull_plane is HullPlaneType.YZ:
         plane_idxs = (1, 2)
     else:
         raise NotImplementedError
