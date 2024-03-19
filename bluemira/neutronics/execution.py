886426d7f53aa7864466e93052e950ea2870193b
ocean <OceanWongUK@gmail.com>
2024-03-19 02:40:02 +0000

    Updated the run modes into context managers.

--- bluemira/neutronics/execution.py
+++ bluemira/neutronics/execution.py
@@ -3,29 +3,57 @@
 # SPDX-FileCopyrightText: 2021-present J. Morris, D. Short
 #
 # SPDX-License-Identifier: LGPL-2.1-or-later
-"""Different ways of running openmc"""
+"""
+Different ways of running openmc, written as context managers so that we clean up the
+files created by openmc.
+"""
+
+from __future__ import annotations
+
 from pathlib import Path
+from typing import TYPE_CHECKING, Iterable, Union
 
 import openmc
 
 from bluemira.base.look_and_feel import bluemira_debug
 from bluemira.base.tools import _timing
+from bluemira.neutronics.neutronics_axisymmetric import (
+    create_parametric_plasma_source,
+    create_ring_source,
+)
+
+if TYPE_CHECKING:
+    from bluemira.neutronics.make_materials import MaterialsLibrary
+    from bluemira.neutronics.params import (
+        OpenMCSimulationRuntimeParameters,
+        PlasmaSourceParametersPPS,
+    )
 
 
+# TODO rewrite this whole module as context managers
 class RunMode:
-    """Generic run method"""
+    """
+    Generic run method.
+    Usage:
+    with ChildClassOfRunMode(cross_section_xml, cells, material_lib) as run_mode:
+        run_mode.setup(...)
+        run_mode.run()
+    """
 
-    xml_files_created = ()
     run_mode_str = ""
 
-    def __init__(self): # noqa: D105
-        if isinstance(self, RunMode):
+    def __init__(
+        self,
+        cross_section_xml: Union[Path, str],
+        cells: Iterable[openmc.Cell],
+        material_lib: MaterialsLibrary,
+    ):
+        """Basic set-up to openmc applicable to all run modes."""
+        self.files_created = set()
+        if type(self) == RunMode:
             raise TypeError(
                 "RunMode is a baseclass that is not meant to be initialized!"
             )
-
-    def set_up(self, cross_section_xml):
-        """Set up openmc"""
         try:
             from openmc.config import config  # noqa: PLC0415
 
@@ -37,58 +65,153 @@ class RunMode:
 
             os.environ["OPENMC_CROSS_SECTIONS"] = str(cross_section_xml)
 
-        self.settings = openmc.Settings()
-        self.settings.run_mode = self.run_mode_str
-        self.settings.output = {"summary":False}
+        self.settings = openmc.Settings(
+            run_mode=self.run_mode_str, output={"summary": False}
+        )
+        self.universe = openmc.Universe(cells=cells)
+        self.cells = cells
+        self.geometry = openmc.Geometry(self.universe)
+        self.material_lib = material_lib
+        self._setup = False
 
-    @classmethod
-    def clean_up_directory(cls, base_path):
+    def __enter__(self):  # noqa: D105
+        return self
+
+    def setup(self):
+        """
+        Set up basic xml files used in every simulation, i.e. settings, geometry and
+        materials.
+        """
+        self._setup = True
+        self.settings.export_to_xml()
+        self.files_created.add("settings.xml")
+        self.geometry.export_to_xml()
+        self.files_created.add("geometry.xml")
+        self.material_lib.export()
+        self.files_created.add("materials.xml")
+
+    def __exit__(self, exception_type, exception_value, traceback):
         """Remove files generated during the run (mainly .xml files.)"""
-        for file_name in cls.xml_files_created:
-            Path(base_path, file_name).unlink(missing_ok=False)
+        base_path = Path.cwd()
+        for file_name in self.files_created:
+            if (f := Path(base_path, file_name)).exists():
+                f.unlink()
+                bluemira_debug(f"Removed file {f}.")
 
     def run(self, *args, output=False, **kwargs) -> None:
         """Complete the run"""
-        self.settings.export_to_xml()
+        if not self._setup:
+            raise RuntimeError("Must ")
         _timing(openmc.run, "Executed in", "Running OpenMC", debug_info_str=False)(
             *args, output=output, **kwargs
         )
-        bluemira_debug(f"Cleaning up files ({', '.join(self.xml_files_created)}).")
-        self.clean_up_directory(Path.cwd())
 
 
 class Plotting(RunMode):
     """Plotting only"""
-    xml_files_created = ("settings.xml", "geometry.xml", "materials.xml")
+
+    run_mode_str = "plot"
+
+    def setup(self, plot_widths, pixel_per_meter):
+        """Set up the plot parameters"""
+        super().setup()
+        self.plot = openmc.Plot()
+        self.plot.basis = "xz"
+        self.plot.pixels = [
+            int(plot_widths[0] * pixel_per_meter),
+            int(plot_widths[1] * pixel_per_meter),
+        ]
+        self.plot.width = plot_widths
+        self.plot_list = openmc.Plots([self.plot])
+
+        self.plot_list.export_to_xml()
+        self.files_created.add("plots.xml")
 
 
-class PlasmaSourceSimulation(RunMode):
-    """Run with our standard plasma source"""
-    xml_files_created = ("settings.xml", "geometry.xml", "materials.xml", "tallies.xml")
+class SourceSimulation(RunMode):
+    """Generic base class for running a fixed source."""
 
-    def set_up(self, cross_section_xml):
-        super().set_up(cross_section_xml)
-        self.settings.particles = ...
-        self.settings.batches = ...
-        self.settings.photon_transport = ...
-        self.settings.electron_treatment = ...
-        self.settings.source = ...
+    "tallies.xml"
+    run_mode_str = "fixed source"
+
+    @staticmethod
+    def make_source(source_parameters: PlasmaSourceParametersPPS) -> openmc.Source:  # noqa: ARG004
+        """Abstract base class method that returns a dummy openmc source."""
+        return openmc.Source(...)
+
+    def setup(
+        self,
+        source_parameters: PlasmaSourceParametersPPS,
+        runtime_variables: OpenMCSimulationRuntimeParameters,
+    ) -> None:
+        """
+        Break open the :class:`~bluemira.neutronics.params.PlasmaSourceParametersPPS`
+        and the :class:`~bluemira.neutronics.params.OpenMCSimulationRuntimeParameters`
+        to enter the corresponding values into openmc.Settings
+        """
+        self.settings.particles = runtime_variables.particles
+        self.settings.source = self.make_source(source_parameters)
+        self.settings.batches = runtime_variables.batches
+        self.settings.photon_transport = runtime_variables.photon_transport
+        self.settings.electron_treatment = runtime_variables.electron_treatment
+        super().setup()
+        self.files_created.add("summary.h5")
+        self.files_created.add("statepoint.1.h5")
 
 
-class RingSourceSimulation(RunMode):
-    xml_files_created = ("settings.xml", "geometry.xml", "materials.xml", "tallies.xml")
+class PlasmaSourceSimulation(SourceSimulation):
+    """Run with our standard pps_isotropic source"""
 
-    def set_up(self, cross_section_xml):
-        super().set_up(cross_section_xml)
-        self.settings.particles = ...
-        self.settings.batches = ...
-        self.settings.photon_transport = ...
-        self.settings.electron_treatment = ...
-        self.settings.source = ...
+    @staticmethod
+    def make_source(source_parameters: PlasmaSourceParametersPPS) -> openmc.Source:
+        """Make a plasma source"""
+        return create_parametric_plasma_source(
+            # tokamak geometry
+            major_r=source_parameters.major_radius,
+            minor_r=source_parameters.minor_radius,
+            elongation=source_parameters.elongation,
+            triangularity=source_parameters.triangularity,
+            # plasma geometry
+            peaking_factor=source_parameters.peaking_factor,
+            temperature=source_parameters.temperature,
+            radial_shift=source_parameters.shaf_shift,
+            vertical_shift=source_parameters.vertical_shift,
+            # plasma type
+            mode="DT",
+        )
 
-class CalculateVolume(RunMode):
+
+class RingSourceSimulation(SourceSimulation):
+    """Run with a simple annular (ring) source"""
+
+    @staticmethod
+    def make_source(source_parameters: PlasmaSourceParametersPPS) -> openmc.Source:
+        """Create the ring source"""
+        return create_ring_source(
+            source_parameters.major_radius, source_parameters.shaf_shift
+        )
+
+
+class VolumeCalculation(RunMode):
     """Run Monte Carlo to get the volume"""
-    xml_files_created = ("settings.xml", "geometry.xml", "volume_settings.xml")
 
-    def set_up(self, cross_section_xml):
-        super().set_up(cross_section_xml)
+    run_mode_str = "volume"
+
+    def setup(
+        self,
+        num_particles: int,
+        min_xyz: Iterable[float],
+        max_xyz: Iterable[float],
+    ):
+        """Set up openmc for volume calculation"""
+        Path("summary.h5").unlink(missing_ok=True)
+        Path("statepoint.1.h5").unlink(missing_ok=True)
+        self.settings.volume_calculations = openmc.VolumeCalculation(
+            self.cells,
+            num_particles,
+            min_xyz,
+            max_xyz,
+        )
+        super().setup()
+        self.files_created.add("summary.h5")
+        self.files_created.add("statepoint.1.h5")
--- examples/radiation_transport/test_script.py
+++ examples/radiation_transport/test_script.py
@@ -6,7 +6,6 @@
 """Test script to make the CSG branch work."""
 
 import json
-import sys
 from itertools import chain
 from pathlib import Path
 from time import time
@@ -18,16 +17,18 @@ from bluemira.base.constants import raw_uc
 from bluemira.display import plot_2d, plot_3d, show_cad  # noqa: F401
 from bluemira.geometry.coordinates import vector_intersect
 from bluemira.geometry.tools import deserialize_shape, make_polygon
+from bluemira.neutronics import result_presentation as present
+from bluemira.neutronics.execution import (
+    PlasmaSourceSimulation,
+    Plotting,
+    VolumeCalculation,
+)
 from bluemira.neutronics.make_csg import BlanketCellArray
 from bluemira.neutronics.make_materials import BlanketType
 from bluemira.neutronics.neutronics_axisymmetric import (
     PlasmaSourceParametersPPS,
-    TBRHeatingSimulation,
     TokamakGeometry,
-    create_and_export_materials,
-    create_parametric_plasma_source,
-    create_ring_source,
-    setup_openmc,
+    create_materials,
 )
 from bluemira.neutronics.params import (
     BlanketLayers,
@@ -36,30 +37,31 @@ from bluemira.neutronics.params import (
     ThicknessFractions,
     get_preset_physical_properties,
 )
-
-# from bluemira.neutronics.execution import Plotting
+from bluemira.neutronics.radial_wall import CellWalls
 from bluemira.neutronics.slicing import PanelsAndExteriorCurve
-from bluemira.neutronics.tallying import create_tallies
 from bluemira.plasma_physics.reactions import n_DT_reactions
 
+CHOSEN_RUNMODE = Plotting
+
+# Parameters initialization
 CROSS_SECTION_XML = str(
     Path(
         "~/Others/cross_section_data/cross_section_data/cross_sections.xml"
     ).expanduser()
 )
-breeder_materials, _tokamak_geometry = get_preset_physical_properties(BlanketType.HCPB)
+_breeder_materials, _tokamak_geometry = get_preset_physical_properties(BlanketType.HCPB)
 
 runtime_variables = OpenMCSimulationRuntimeParameters(
-    cross_section_xml=CROSS_SECTION_XML,
-    particles=100000,  # 16800 takes 5 seconds,  1000000 takes 280 seconds.
+    cross_section_xml=CROSS_SECTION_XML,  # TODO: obsolete
+    particles=100000,  # TODO: obsolete # 16800 takes 5 seconds,  1000000 takes 280 seconds. # noqa: E501
     batches=2,
     photon_transport=True,
     electron_treatment="ttb",
-    run_mode="plot",
-    openmc_write_summary=False,
-    parametric_source=True,
+    run_mode="plot",  # TODO: obsolete
+    openmc_write_summary=False,  # TODO: obsolete
+    parametric_source=True,  # TODO: obsolete
     # only used if stochastic_volume_calculation is turned on.
-    volume_calc_particles=int(4e8),
+    volume_calc_particles=int(4e8),  # TODO: obsolete
 )
 
 _source_parameters = PlasmaSourceParameters(
@@ -74,56 +76,13 @@ _source_parameters = PlasmaSourceParameters(
     vertical_shift=0.0,  # [m]
 )
 
-tbr_heat_sim = TBRHeatingSimulation(
-    runtime_variables,
-    _source_parameters,
-    breeder_materials,
-    _tokamak_geometry,
-)
-# read in wires
 tokamak_geometry = TokamakGeometry.from_si(_tokamak_geometry)
 source_parameters = PlasmaSourceParametersPPS.from_si(_source_parameters)
+mat_lib = create_materials(_breeder_materials)
 
-tbr_heat_sim.material_lib = create_and_export_materials(tbr_heat_sim.breeder_materials)
-if tbr_heat_sim.runtime_variables.parametric_source:
-    source = create_parametric_plasma_source(
-        # tokamak geometry
-        major_r=source_parameters.plasma_physics_units.major_radius,
-        minor_r=source_parameters.plasma_physics_units.minor_radius,
-        elongation=source_parameters.plasma_physics_units.elongation,
-        triangularity=source_parameters.plasma_physics_units.triangularity,
-        # plasma geometry
-        peaking_factor=source_parameters.plasma_physics_units.peaking_factor,
-        temperature=source_parameters.plasma_physics_units.temperature,
-        radial_shift=source_parameters.plasma_physics_units.shaf_shift,
-        vertical_shift=source_parameters.plasma_physics_units.vertical_shift,
-        # plasma type
-        mode="DT",
-    )
-else:
-    source = create_ring_source(
-        source_parameters.plasma_physics_units.major_radius,
-        source_parameters.plasma_physics_units.shaf_shift,
-    )
-setup_openmc(source, tbr_heat_sim.runtime_variables)
 thickness_fractions = ThicknessFractions.from_TokamakGeometry(tokamak_geometry)
-# blanket_points, div_points, num_inboard_points = mg.load_fw_points(
-#     source_parameters,
-#     make_polygon(Coordinates(np.load("blanket_face.npy"))),
-#     make_polygon(Coordinates(np.load("divertor_face.npy"))),
-#     new_major_radius=raw_uc(9.0, "m", "cm"),
-#     new_aspect_ratio=3.10344,  # [dimensionless]
-#     new_elong=1.792,  # [dimensionless]
-#     save_plots=True,
-# data loading begins here.
 
-START_TIME = time()
-
-
-def elapsed(start_time=START_TIME):  # noqa: D103
-    return f"t={time() - start_time:9.6f}s"
-
-
+# Loading data
 with open("data/inner_boundary") as j:
     inner_boundary = deserialize_shape(json.load(j))
 with open("data/outer_boundary") as j:
@@ -138,7 +97,7 @@ fw_panel_bp_list = [
     np.load("data/fw_panels_50_0.5.npy"),
 ]
 panel_breakpoint_t = fw_panel_bp_list[0].T
-# TODO: MANUAL FIX the coordinates
+# TODO: MANUAL FIX of the coordinates
 panel_breakpoint_t[0] = vector_intersect(
     panel_breakpoint_t[0],
     panel_breakpoint_t[1],
@@ -154,83 +113,106 @@ panel_breakpoint_t[-1] = vector_intersect(
 
 last_point = divertor_bmwire.edges[-1].end_point()
 
-
 blanket_panels_bmwire = make_polygon(
     np.insert(panel_breakpoint_t, 1, 0, axis=1).T,
     label="blanket panels",
     closed=False,
 )
 
-print(elapsed(), ": Before creating pre-cells")
+if __name__ == "__main__":  # begin computation
+    START_TIME = time()
 
-panel_and_exterior = PanelsAndExteriorCurve(panel_breakpoint_t, outer_boundary)
-pca = panel_and_exterior.make_quadrilateral_pre_cell_array(
-    snap_to_horizontal_angle=45, ending_cut=last_point.xz.flatten()
-)
-print(elapsed(), ": Aftere pre-cell creations, before straightening.")
-mat_lib = tbr_heat_sim.material_lib
-mat_dict = {
-    BlanketLayers.Surface.name: mat_lib.outb_sf_mat,
-    BlanketLayers.FirstWall.name: mat_lib.outb_fw_mat,
-    BlanketLayers.BreedingZone.name: mat_lib.outb_bz_mat,
-    BlanketLayers.Manifold.name: mat_lib.outb_mani_mat,
-    BlanketLayers.VacuumVessel.name: mat_lib.outb_vv_mat,
-}
-pca2 = pca.straighten_exterior(preserve_volume=True)
-print(elapsed(), ": After straightening.")
-for i, (v1, v2) in enumerate(zip(pca.volumes, pca2.volumes)):
-    print(
-        f"Cell {i:<2}: Volume change = {(v2 / v1 - 1) * 100:6.3f}% , "
-        f"with initial volume = {v1:8.3f} m³"
+    def elapsed(text, start_time=START_TIME):  # noqa: D103
+        new_time = time()
+        print(f"t={new_time - start_time:9.6f}s: " + text)
+        return new_time
+
+    elapsed("Before creating pre-cells")
+
+    panel_and_exterior = PanelsAndExteriorCurve(panel_breakpoint_t, outer_boundary)
+    pca = panel_and_exterior.make_quadrilateral_pre_cell_array(
+        snap_to_horizontal_angle=45, ending_cut=last_point.xz.flatten()
     )
-print(f"Before = {sum(pca.volumes):8.3f} m³; Total: After = {sum(pca2.volumes):8.3f} m³")
-# plot_2d([c.outline for c in pca] + [c.outline for c in pca2])
-# tbr_heat_sim.run()
 
-blanket_cell_array = BlanketCellArray.from_pre_cell_array(
-    pca2, mat_dict, thickness_fractions
-)
-print("The number of surfaces created =", len(openmc.Surface.used_ids))
+    elapsed("Aftere pre-cell creations, before straightening.")
+    mat_dict = {
+        BlanketLayers.Surface.name: mat_lib.outb_sf_mat,
+        BlanketLayers.FirstWall.name: mat_lib.outb_fw_mat,
+        BlanketLayers.BreedingZone.name: mat_lib.outb_bz_mat,
+        BlanketLayers.Manifold.name: mat_lib.outb_mani_mat,
+        BlanketLayers.VacuumVessel.name: mat_lib.outb_vv_mat,
+    }
 
-_all_cells = list(chain.from_iterable(blanket_cell_array))
-plasma_void_upper = blanket_cell_array.make_plasma_void_region()
-selected_cells = [
-    *_all_cells,
-    openmc.Cell(region=plasma_void_upper, fill=None, name="Plasma cell"),
-]
-universe = openmc.Universe(cells=selected_cells)
-geometry = openmc.Geometry(universe)
-geometry.export_to_xml()
-plot = openmc.Plot()
-plot.basis = "xz"
-plot_dims = (
-    outer_boundary.bounding_box.x_max * 2.1,
-    outer_boundary.bounding_box.z_max * 3.1,
-)
-ppm = 100
-plot.pixels = [int(plot_dims[0] * ppm), int(plot_dims[1] * ppm)]
-plot.width = plot_dims
-openmc.Plots([plot]).export_to_xml()
-openmc.plot_geometry(output=True)
+    pca2 = pca.straighten_exterior(preserve_volume=True)
+    elapsed("After straightening.")
 
-# tbr_heat_sim.cells, tbr_heat_sim.universe = mg.make_neutronics_geometry(
-#     tokamak_geometry,
-#     blanket_points,
-#     div_points,
-#     num_inboard_points,
-#     tbr_heat_sim.material_lib,
-# )  # TODO: improve here
-sys.exit()
+    for i, (v1, v2) in enumerate(zip(pca.volumes, pca2.volumes)):
+        print(
+            f"Cell {i:<2}: Volume change = {(v2 / v1 - 1) * 100:6.3f}% , "
+            f"with initial volume = {v1:8.3f} m³"
+        )
+    print(
+        f"Total: Before ={sum(pca.volumes):8.3f} m³; After ={sum(pca2.volumes):8.3f} m³"
+    )
+    # plot_2d([c.outline for c in pca] + [c.outline for c in pca2])
 
-tbr_heat_sim.src_rate = n_DT_reactions(
-    source_parameters.plasma_physics_units.reactor_power  # [MW]
-    # TODO: when issue #2858 is fixed,
-    # it will change the definition of n_DT_reactions from [MW] to [W].
-    # in which which case, we use tbr_heat_sim.source_parameters.reactor_power.
-)
-create_tallies(tbr_heat_sim.cells, tbr_heat_sim.material_lib)  # TODO: improve here
+    blanket_cell_array = BlanketCellArray.from_pre_cell_array(
+        pca2, mat_dict, thickness_fractions
+    )
+    print("The number of surfaces created =", len(openmc.Surface.used_ids))
 
-if False:
-    tbr_heat_sim.calculate_volume_stochastically()
-results = tbr_heat_sim.get_result()
-print(results)
+    _all_cells = list(chain.from_iterable(blanket_cell_array))
+    plasma_void_upper = blanket_cell_array.make_plasma_void_region()
+    cells = [
+        *_all_cells,
+        openmc.Cell(region=plasma_void_upper, fill=None, name="Plasma cell"),
+    ]
+
+    # using openmc
+    if Plotting == CHOSEN_RUNMODE:
+        with Plotting(
+            runtime_variables.cross_section_xml, cells, mat_lib
+        ) as chosen_runmode:
+            chosen_runmode.setup(
+                [
+                    outer_boundary.bounding_box.x_max * 2.1,
+                    outer_boundary.bounding_box.z_max * 3.1,
+                ],
+                100,
+            )
+            chosen_runmode.run()
+
+    elif VolumeCalculation == CHOSEN_RUNMODE:
+        with VolumeCalculation(
+            runtime_variables.cross_section_xml, cells, mat_lib
+        ) as chosen_runmode:
+            cell_walls = CellWalls.from_pre_cell_array(pca2)
+
+            _r, _z = cell_walls[:, :, 0].flatten(), cell_walls[:, :, -1].flatten()
+            r_max = max(_r)
+            r_min = -r_max
+            z_min, z_max = min(_z), max(_z)
+
+            min_xyz = (r_min, r_min, z_min)
+            max_xyz = (r_max, r_max, z_max)
+
+            chosen_runmode.setup(
+                runtime_variables.volume_calc_particles, min_xyz, max_xyz
+            )
+            chosen_runmode.run()
+
+    elif PlasmaSourceSimulation == CHOSEN_RUNMODE:
+        with PlasmaSourceSimulation(
+            runtime_variables.cross_section_xml, cells, mat_lib
+        ) as chosen_runmode:
+            # create_tallies(cells, mat_lib)  # TODO: improve here
+            chosen_runmode.setup(source_parameters, runtime_variables)
+            chosen_runmode.run()
+            src_rate = n_DT_reactions(
+                source_parameters.plasma_physics_units.reactor_power  # [MW]
+                # TODO: when issue #2858 is fixed,
+                # it will change the definition of n_DT_reactions from [MW] to [W].
+                # in which which case, we use source_parameters.reactor_power.
+            )
+            results = present.OpenMCResult.from_run(chosen_runmode.universe, src_rate)
+            print(results)
