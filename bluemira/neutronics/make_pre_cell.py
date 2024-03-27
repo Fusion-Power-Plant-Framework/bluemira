e15572296580e7b337465a0a3aa7386da7fef409
ocean <OceanWongUK@gmail.com>
2024-03-27 16:17:50 +0000

    Improved the interior_wire_offset method

--- bluemira/neutronics/make_pre_cell.py
+++ bluemira/neutronics/make_pre_cell.py
@@ -9,22 +9,28 @@
 from __future__ import annotations
 
 from collections import abc
-from typing import TYPE_CHECKING, List, Union
+from typing import List, Tuple, Union
 
 import numpy as np
 from numpy import typing as npt
 
 from bluemira.display import plot_2d, show_cad
-from bluemira.geometry.constants import EPS_FREECAD
+from bluemira.geometry.constants import EPS_FREECAD, choose_direction
 from bluemira.geometry.coordinates import Coordinates, get_bisection_line
 from bluemira.geometry.error import GeometryError
 from bluemira.geometry.solid import BluemiraSolid
 from bluemira.geometry.tools import make_polygon, raise_error_if_overlap, revolve_shape
 from bluemira.geometry.wire import BluemiraWire
 from bluemira.neutronics.radial_wall import CellWalls, Vertices, VerticesCoordinates
+from bluemira.neutronics.wires import (
+    CircleInfo,
+    StraightLineInfo,
+    WireInfo,
+    WireInfoList,
+)
 
-if TYPE_CHECKING:
-    from bluemira.neutronics.slicing import WireInfoList
+CCW_90 = np.array([[0, 0, -1], [0, 1, 0], [1, 0, 0]])
+CW_90 = np.array([[0, 0, 1], [0, 1, 0], [-1, 0, 0]])
 
 
 class PreCell:  # TODO: Rename this as BlanketPreCell
@@ -141,9 +147,7 @@ class PreCell:  # TODO: Rename this as BlanketPreCell
 
         return self._normal_to_interior
 
-    def get_cell_wall_cut_points_by_fraction(
-        self, fraction: float
-    ) -> npt.NDArray[float]:
+    def get_cell_wall_cut_points_by_fraction(self, fraction: float) -> npt.NDArray:
         """
         Find the cut points on the cell's side walls by multiplying the original lengths
         by a fraction. When fraction=0, this returns the interior_start and interior_end.
@@ -247,11 +251,11 @@ class PreCellArray(abc.Sequence):
             new_pre_cells.append(PreCell(self[i].interior_wire, exterior))
         return PreCellArray(new_pre_cells)
 
-    def plot_2d(self) -> None:  # noqa: D102
-        plot_2d([c.outline for c in self])
+    def plot_2d(self, *args, **kwargs) -> None:  # noqa: D102
+        plot_2d([pc.outline for pc in self], *args, **kwargs)
 
-    def show_cad(self) -> None:  # noqa: D102
-        show_cad([c.half_solid for c in self])
+    def show_cad(self, *args, **kwargs) -> None:  # noqa: D102
+        show_cad([pc.half_solid for pc in self], *args, **kwargs)
 
     def __len__(self) -> int:
         return self.pre_cells.__len__()
@@ -272,6 +276,114 @@ class PreCellArray(abc.Sequence):
         return super().__repr__().replace(" at ", f" of {len(self)} PreCells at ")
 
 
+def ratio_of_distances(
+    point_of_interest: npt.NDArray[float],
+    anchor1: npt.NDArray[float],
+    normal1: npt.NDArray[float],
+    anchor2: npt.NDArray[float],
+    normal2: npt.NDArray[float],
+) -> npt.NDArray[float]:
+    """
+    Find how close a point is to line 1 and line 2, and then express that ratio as a
+    tuple of floats that sums up to unit.
+    Each line is given by the user by specifying any point on that line, and a direction
+    NORMAL to that line. The point_of_interest must lie on the positive side of the line.
+
+    Parameters
+    ----------
+    point_of_interest
+        point to which we want to calculate the ratio of distances.
+    anchor1, anchor2:
+        Any point on line 1 and line 2 respectively.
+    normal1, normal2:
+        The positive distance direction of line 1 and line 2 respectively.
+
+    Returns
+    -------
+    dist_to_1, dist_to_2:
+        ratio of distances. Sum of these two numbers should yield unity (1.0).
+    """
+    dist_to_1 = (point_of_interest - anchor1) @ normal1
+    dist_to_2 = (point_of_interest - anchor2) @ normal2
+    if dist_to_1 <= 0 or dist_to_2 <= 0:
+        raise GeometryError(
+            "Expecting point_of_interest to lie on the positive side of " "both lines!"
+        )
+    total_dist = dist_to_1 + dist_to_2
+    return np.array([dist_to_1, dist_to_2]) / total_dist
+
+
+def find_equidistant_point(
+    point1: npt.NDArray[float], point2: npt.NDArray[float], distance: float
+) -> Tuple[npt.NDArray[float], npt.NDArray[float]]:
+    """Find the two (or 0) points on a 2D plane that are equidistant to each other.
+
+    Parameters
+    ----------
+    point1, point2:
+        2D points, each with shape (2,)
+    distance:
+        the distance that both points must obey by.
+
+    Returns
+    -------
+    intersection1, intersection2:
+        The two intersection points of circle1 and circle2.
+    """
+    mid_point = (point1 + point2) / 2
+    half_sep = np.linalg.norm(mid_point - point1)  # scalar
+    if half_sep > distance:
+        raise GeometryError("The two points are separated by > 2 * distance!")
+    orth_length = np.sqrt(distance**2 - half_sep**2)
+    orth_dir = np.array([-half_sep[1], half_sep[0]])
+    orth_dir /= np.linalg.norm(orth_dir)
+    return mid_point + (orth_dir * orth_length), mid_point - (orth_dir * orth_length)
+
+
+def pick_higher_point(
+    point1: npt.NDArray[float], point2: npt.NDArray[float], vector: npt.NDArray[float]
+):
+    """Pick the point that, when projected onto `vector`, gives a higher value."""
+    if (vector @ point1) > (vector @ point2):
+        return point1
+    return point2
+
+
+def calculate_new_circle(
+    old_circle_info: CircleInfo, new_points: npt.NDArray
+) -> CircleInfo:
+    """
+    Calculate how far does the new circle get shifted.
+
+    Parameters
+    ----------
+    old_circle_info:
+        an object accessed by WireInfoList[i].key_points.
+        info on circle where the start_point and end_point are each of shape (3,).
+    new_points
+        array of shape (2, 3)
+
+    Returns
+    -------
+    new_circle_info
+        An instance of CircleInfo representing the new (scaled) arc of circle.
+    """
+    new_chord_vector = np.diff(new_points, axis=0)
+    old_chord_vector = np.diff(old_circle_info[:2], axis=0)
+    scale_factor = np.linalg.norm(new_chord_vector) / np.linalg.norm(old_chord_vector)
+    new_radius = old_circle_info.radius * scale_factor
+    possible_centers = find_equidistant_point(*new_points[:, ::2], new_radius)
+    center1, center2 = (np.insert(possible_centers, 1, 0, axis=1),)
+
+    old_chord_mid_point = np.mean(old_circle_info[:2], axis=0)
+    old_radius_vector = np.array(old_circle_info.center) - old_chord_mid_point
+    # assuming the transformation is not too dramatic, then the new radius_vector would
+    # point in pretty much the same direction.
+    new_center = pick_higher_point(center1, center2, old_radius_vector)
+
+    return CircleInfo(*new_points, new_center, new_radius)
+
+
 class DivertorPreCell:
     """
     An intermediate class between the bluemira wire and the final csg product.
@@ -282,25 +394,28 @@ class DivertorPreCell:
         self,
         interior_wire: WireInfoList,
         exterior_wire: WireInfoList,
-        cw_wall: WireInfoList,
-        ccw_wall: WireInfoList,
     ):
         self.interior_wire = interior_wire
         self.exterior_wire = exterior_wire
-        self.cw_wall = cw_wall
-        self.ccw_wall = ccw_wall
-        for wall in (self.cw_wall, self.ccw_wall):
-            # TODO: uncomment the check below once the code is refactored.
-            if len(wall) != 1:  # or not isinstance(wall.key_points, StraightLineInfo):
-                raise GeometryError(
-                    "Expected only a single straight line in cw_wall and" " ccw_wall."
-                )
+        self.cw_wall = WireInfoList([
+            WireInfo.from_2P(
+                self.exterior_wire.end_point, self.interior_wire.start_point
+            )
+        ])
+        self.ccw_wall = WireInfoList([
+            WireInfo.from_2P(
+                self.interior_wire.end_point, self.exterior_wire.start_point
+            )
+        ])
         self.vertex = Vertices(
             self.cw_wall.start_point,
             self.cw_wall.end_point,
             self.ccw_wall.start_point,
             self.ccw_wall.end_point,
         )
+
+    def plot_2d(self, *args, **kwargs) -> None:  # noqa: D102
+        plot_2d([self.outline], *args, **kwargs)
 
     @property
     def outline(self):
@@ -317,6 +432,54 @@ class DivertorPreCell:
             ])
         return self._outline
 
+    def interior_wire_offset(self, thickness: float) -> WireInfoList:
+        """
+        Offset the interior wire towards the exterior_wire.
+        The true problem of expanding/shrinking a wire is a much more difficult one, so
+        I've only opted for a simpler (but incorrect) approach of pushing the wire to a
+        desired direction determined by how close it is to the wall.
+        """
+        int_wire_pts = [w.key_points[0] for w in self.interior_wire]
+        int_wire_pts.append(self.interior_wire[-1].key_points[1])
+        int_wire_pts = np.array(int_wire_pts)  # shape (N+1, 3)
+        cw_dir = choose_direction(
+            self.cw_wall[0].tangents[0], self.cw_wall.end_point, self.cw_wall.start_point
+        )  # assumed normalized
+        ccw_dir = choose_direction(
+            self.ccw_wall[0].tangents[1],
+            self.ccw_wall.start_point,
+            self.ccw_wall.end_point,
+        )  # assumed normalized
+        cw_norm = CCW_90 @ cw_dir
+        cw_anchor = self.cw_wall.start_point
+        ccw_norm = CW_90 @ ccw_dir
+        ccw_anchor = self.ccw_wall.end_point
+
+        shifted_pts = []
+        for pt in zip(int_wire_pts):
+            weights = ratio_of_distances(pt, cw_norm, cw_anchor, ccw_norm, ccw_anchor)[
+                ::-1
+            ]
+            new_dir = np.array([cw_dir, ccw_dir]).T @ weights
+            step = new_dir / np.linalg.norm(new_dir) * thickness
+            shifted_pts.append(pt + step)
+
+        info_list = []
+        for i, (new_start, new_end) in enumerate(zip(shifted_pts[:-1], shifted_pts[1:])):
+            old_kp = self.interior_wire[i].key_points
+            if isinstance(old_kp, StraightLineInfo):
+                info_list.append(WireInfo.from_2P(new_start, new_end))
+            else:
+                info_list.append(
+                    WireInfo(
+                        calculate_new_circle(old_kp, np.array([new_start, new_end])),
+                        # copy the old tangents, which are not going to be correct,
+                        # but we won't be using them anyways.
+                        self.interior_wire[i].tangents,
+                    )
+                )
+        return WireInfoList(info_list)
+
 
 class DivertorPreCellArray(abc.Sequence):
     """An array of Divertor pre-cells"""
@@ -331,6 +494,7 @@ class DivertorPreCellArray(abc.Sequence):
                 atol=0,
                 rtol=EPS_FREECAD,
             ):
+                # # Don't need this check below, thus commented out.
                 # and np.allclose(prev_cell.vertex.interior_end,
                 #                 curr_cell.vertex.interior_start,
                 #                 atol=0, rtol=EPS_FREECAD)
@@ -356,3 +520,6 @@ class DivertorPreCellArray(abc.Sequence):
         return (
             super().__repr__().replace(" at ", f" of {len(self)} DivertorPreCells at ")
         )
+
+    def plot_2d(self, *args, **kwargs) -> None:  # noqa: D102
+        plot_2d([dpc.outline for dpc in self], *args, **kwargs)
