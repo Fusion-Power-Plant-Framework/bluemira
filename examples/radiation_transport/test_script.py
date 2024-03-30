60fd649bf034947db1bd81a9c75fae584bc6853e
ocean <OceanWongUK@gmail.com>
2024-03-30 04:30:05 +0000

    updated test script to plot better.

--- examples/radiation_transport/test_script.py
+++ examples/radiation_transport/test_script.py
@@ -6,13 +6,11 @@
 """Test script to make the CSG branch work."""
 
 import json
-import sys
 from itertools import chain
 from pathlib import Path
 from time import time
 
 import numpy as np
-import openmc
 
 from bluemira.base.constants import raw_uc
 from bluemira.display import plot_2d, plot_3d, show_cad  # noqa: F401
@@ -24,24 +22,19 @@ from bluemira.neutronics.execution import (
     Plotting,
     VolumeCalculation,
 )
-from bluemira.neutronics.make_csg import BlanketCellArray
 from bluemira.neutronics.make_materials import BlanketType
 from bluemira.neutronics.neutronics_axisymmetric import (
     PlasmaSourceParametersPPS,
     TokamakGeometry,
     create_materials,
 )
+from bluemira.neutronics.overseer import OpenMCModelGenerator
 from bluemira.neutronics.params import (
     BlanketLayers,
     OpenMCSimulationRuntimeParameters,
     PlasmaSourceParameters,
     ThicknessFractions,
     get_preset_physical_properties,
-)
-from bluemira.neutronics.radial_wall import CellWalls
-from bluemira.neutronics.slicing import (
-    DivertorWireAndExteriorCurve,
-    PanelsAndExteriorCurve,
 )
 from bluemira.plasma_physics.reactions import n_DT_reactions
 
@@ -135,56 +128,40 @@ if __name__ == "__main__":  # begin computation
 
     elapsed("Before creating pre-cells")
 
-    panel_and_exterior = PanelsAndExteriorCurve(panel_breakpoint_t, outer_boundary)
-    pca = panel_and_exterior.make_quadrilateral_pre_cell_array(
-        snap_to_horizontal_angle=45, ending_cut=last_point.xz.flatten()
-    )
-
-    elapsed("Aftere pre-cell creations, before straightening.")
+    generator = OpenMCModelGenerator(panel_breakpoint_t, divertor_bmwire, outer_boundary)
+    generator.make_pre_cell_arrays(preserve_volume=False)
     mat_dict = {
         BlanketLayers.Surface.name: mat_lib.outb_sf_mat,
         BlanketLayers.FirstWall.name: mat_lib.outb_fw_mat,
         BlanketLayers.BreedingZone.name: mat_lib.outb_bz_mat,
         BlanketLayers.Manifold.name: mat_lib.outb_mani_mat,
         BlanketLayers.VacuumVessel.name: mat_lib.outb_vv_mat,
+        # TODO: make these two Divertor names into Enum
+        "Divertor": mat_lib.div_fw_mat,
+        "DivertorSurface": mat_lib.div_fw_mat,
     }
-
-    pca2 = pca.straighten_exterior(preserve_volume=True)
-    elapsed("After straightening.")
-
-    for i, (v1, v2) in enumerate(zip(pca.volumes, pca2.volumes)):
-        print(
-            f"Cell {i:<2}: Volume change = {(v2 / v1 - 1) * 100:6.3f}% , "
-            f"with initial volume = {v1:8.3f} m³"
-        )
-    print(
-        f"Total: Before ={sum(pca.volumes):8.3f} m³; After ={sum(pca2.volumes):8.3f} m³"
+    blanket_cell_array, div_cell_array = generator.make_cell_arrays(
+        mat_dict, thickness_fractions
     )
-    # plot_2d([c.outline for c in pca] + [c.outline for c in pca2])
 
-    blanket_cell_array = BlanketCellArray.from_pre_cell_array(
-        pca2, mat_dict, thickness_fractions
-    )
-    print("The number of surfaces created =", len(openmc.Surface.used_ids))
-
-    _all_cells = list(chain.from_iterable(blanket_cell_array))
-    plasma_void_upper = blanket_cell_array.make_plasma_void_region()
+    # plasma_void_upper = blanket_cell_array.make_plasma_void_region()
     cells = [
-        *_all_cells,
-        openmc.Cell(region=plasma_void_upper, fill=None, name="Plasma cell"),
+        *list(chain.from_iterable(blanket_cell_array)),
+        *list(chain.from_iterable(div_cell_array)),
+        generator.make_plasma_cell(),
     ]
-    divertor_wire_and_ext = DivertorWireAndExteriorCurve(divertor_bmwire, outer_boundary)
-    div_pca = (
-        divertor_wire_and_ext.make_divertor_pre_cell_array()
-    )  # DivertorPreCellArray
-    sys.exit()
+    # plot_2d([*[pc.outline for pc in pca2], *[pc.outline for pc in div_pca]])
 
     # using openmc
     if Plotting == CHOSEN_RUNMODE:
         with Plotting(
-            runtime_variables.cross_section_xml, cells, mat_lib
-        ) as chosen_runmode:
-            chosen_runmode.run(
+            runtime_variables.cross_section_xml,
+            cells,
+            # [openmc.Cell(region=s.get_overall_region()) for s in blanket_cell_array]
+            # [openmc.Cell(region=s.get_overall_region()) for s in div_cell_array]
+            mat_lib,
+        ) as plotting:
+            plotting.run(
                 [
                     outer_boundary.bounding_box.x_max * 2.1,
                     outer_boundary.bounding_box.z_max * 3.1,
@@ -195,18 +172,19 @@ if __name__ == "__main__":  # begin computation
     elif VolumeCalculation == CHOSEN_RUNMODE:
         with VolumeCalculation(
             runtime_variables.cross_section_xml, cells, mat_lib
-        ) as chosen_runmode:
-            cell_walls = CellWalls.from_pre_cell_array(pca2)
-
-            _r, _z = cell_walls[:, :, 0].flatten(), cell_walls[:, :, -1].flatten()
-            r_max = max(_r)
+        ) as vol_calc:
+            all_ext_vertices = generator.get_coordinates_from_pre_cell_arrays(
+                generator.pre_cell_array.blanket, generator.pre_cell_array.divertor
+            )
+            z_min = all_ext_vertices[:, -1].min()
+            z_max = all_ext_vertices[:, -1].max()
+            r_max = max(abs(all_ext_vertices[:, 0]))
             r_min = -r_max
-            z_min, z_max = min(_z), max(_z)
 
             min_xyz = (r_min, r_min, z_min)
             max_xyz = (r_max, r_max, z_max)
 
-            chosen_runmode.run(
+            vol_calc.run(
                 runtime_variables.volume_calc_particles,
                 min_xyz,
                 max_xyz,
@@ -217,15 +195,13 @@ if __name__ == "__main__":  # begin computation
     elif PlasmaSourceSimulation == CHOSEN_RUNMODE:
         with PlasmaSourceSimulation(
             runtime_variables.cross_section_xml, cells, mat_lib
-        ) as chosen_runmode:
-            chosen_runmode.run(
-                source_parameters, runtime_variables, blanket_cell_array, mat_dict
-            )
+        ) as pss:
+            pss.run(source_parameters, runtime_variables, blanket_cell_array, mat_dict)
             src_rate = n_DT_reactions(
                 source_parameters.plasma_physics_units.reactor_power  # [MW]
                 # TODO: when issue #2858 is fixed,
                 # it will change the definition of n_DT_reactions from [MW] to [W].
                 # in which which case, we use source_parameters.reactor_power.
             )
-            results = present.OpenMCResult.from_run(chosen_runmode.universe, src_rate)
+            results = present.OpenMCResult.from_run(pss.universe, src_rate)
             print(results)
