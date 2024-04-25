3118c22511e77b9cda88a4a2faff0af7aac69534
Jonathan Matthews <115085094+kj5248@users.noreply.github.com>
2024-04-25 14:40:30 +0100

    Fix to incorrect PolyhedralPrismCurrentSource behaviour when inputted XS is in global not local coordinates (#3188)
    * fix to polycurrentsource XS when not local coords
    
    * updating of tests and adding new test
    
    * XS fixes

--- tests/magnetostatics/test_polyhedral_prism.py
+++ tests/magnetostatics/test_polyhedral_prism.py
@@ -289,9 +289,11 @@ class TestCombinedShapes:
             current,
         )
         coords = Coordinates({"x": [-1, -1, 1], "z": [1, -1, -1]})
+        offset_yz = 1 / 3
+        offset_x = offset_yz * np.tan(10 * np.pi / 180)
         cls.triangle1 = PolyhedralPrismCurrentSource(
-            [0, 0, 0],
-            [10, 0, 0],
+            [0, -offset_yz, -offset_yz],
+            [10 - 2 * offset_x, 0, 0],
             [0, 1, 0],
             [0, 0, 1],
             coords,
@@ -301,8 +303,8 @@ class TestCombinedShapes:
         )
         coords = Coordinates({"x": [-1, 1, 1], "z": [1, -1, 1]})
         cls.triangle2 = PolyhedralPrismCurrentSource(
-            [0, 0, 0],
-            [10, 0, 0],
+            [0, offset_yz, offset_yz],
+            [10 + 2 * offset_x, 0, 0],
             [0, 1, 0],
             [0, 0, 1],
             coords,
@@ -343,3 +345,37 @@ class TestCombinedShapes:
         cm = ax.contourf(args_diff[i], args_diff[j], args_diff[k], zdir=plane, offset=0)
         f.colorbar(cm)
         np.testing.assert_allclose(B_new, B)
+
+
+class TestXSCoordinateInputting:
+    @classmethod
+    def setup_class(cls):
+        current = 1e6
+        xs = Coordinates({"x": [-1, -1, 1, 1], "z": [1, -1, -1, 1]})
+        xs2 = Coordinates({"x": [1, 1, 3, 3], "z": [3, 1, 1, 3]})
+        cls.source1 = PolyhedralPrismCurrentSource(
+            [0, 2, 2],
+            [10, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            xs,
+            10,
+            10,
+            current,
+        )
+        cls.source2 = PolyhedralPrismCurrentSource(
+            [0, 2, 2],
+            [10, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            xs2,
+            10,
+            10,
+            current,
+        )
+
+    def test_coords(self):
+        for face_source1, face_source2 in zip(
+            self.source1._points, self.source2._points, strict=False
+        ):
+            np.testing.assert_allclose(face_source1, face_source2)
