562816bd48ad8b10a2e99d5b7fdd5471b7a2b8f8
ocean <OceanWongUK@gmail.com>
2024-05-13 16:52:26 +0100

    Changed tallies to get the required data (need to multiply by appropriate constants in output.py to finish the job)

--- bluemira/codes/openmc/tallying.py
+++ bluemira/codes/openmc/tallying.py
@@ -9,38 +9,83 @@ from itertools import chain
 
 import openmc
 
-from bluemira.codes.openmc.make_csg import BlanketCellArray
+from bluemira.codes.openmc.make_csg import BlanketCellArray, DivertorCellArray
+from bluemira.radiation_transport.neutronics import eurofer_mat
 
 
 def filter_cells(
     material_list,
     blanket_cell_array: BlanketCellArray,
-    divertor_cells=None,  # noqa: ARG001
-    plasma_void=None,  # noqa: ARG001
+    divertor_cell_array: DivertorCellArray,
 ):
-    """Bodge method to filter new cells. To be fixed later."""
-    # TODO: make prettier! Delete unwanted parts.
-    cells = chain.from_iterable(blanket_cell_array)
-    fw_surf_cells = [
-        *(stack[0] for stack in blanket_cell_array),
-        *(stack[1] for stack in blanket_cell_array),
+    """
+    Create scores and the filter for the scores. Give them names.
+
+    Returns
+    -------
+    TBR
+        Achieved by (n,Xt) reaction, which counts the number of tritium-producing
+        nuclear reactions per neutron emitted at the source.
+
+        We used the (n,Xt) score because the Lithium produces a maximum of 1 Tritium per
+        reaction, so there won't be any concerns about uncer-counting the TBR.
+
+    Powers
+        Measures the nuclear heating in various locations and materials, and interpret
+        this as power. "damage-energy" is given by eV per source neutron.
+        Multiply by neutron source rate, and then divide by (number of atoms and
+        threshold displacement energy) to get the DPA.
+
+    Fluence
+        Measures # of neutrons streaming through.
+        "flux" is given in # per source particle, so multiply by # of source neutrons to
+        get the total fluence over the simulation.
+        Divide by area to get fluence in unit: cm^-2.
+
+    """
+    blanket_cells = [*chain.from_iterable(blanket_cell_array)]
+    div_cells = [*chain.from_iterable(divertor_cell_array)]
+    cells = blanket_cells + div_cells
+    # fw_surf_cells = [
+    #     *(stack[0] for stack in blanket_cell_array),
+    #     *(stack[1] for stack in blanket_cell_array),
+    # # TODO: ask Matti if we need stack[1] too or not
+    # ]
+    vv_cells = [
+        *(stack[-1] for stack in blanket_cell_array),
+        *(stack[-1] for stack in divertor_cell_array),
     ]
-    cell_filter = openmc.CellFilter(list(cells))
-    fw_surf_filter = openmc.CellFilter(fw_surf_cells)
+    bz_cells = [stack[2] for stack in blanket_cell_array]
+
+    # Cell filters
+    # blanket_cell_filter = openmc.CellFilter(blanket_cells)
+    div_cell_filter = openmc.CellFilter(div_cells)
+    cell_filter = openmc.CellFilter(cells)
+    # fw_surf_filter = openmc.CellFilter(fw_surf_cells)
+    vv_filter = openmc.CellFilter(vv_cells)
+    bz_filter = openmc.CellFilter(bz_cells)
+
+    # material filters
     mat_filter = openmc.MaterialFilter(material_list)
+    eurofer_filter = openmc.MaterialFilter(eurofer_mat)
     neutron_filter = openmc.ParticleFilter(["neutron"])
-    photon_filter = openmc.ParticleFilter(["photon"])
+    # photon_filter = openmc.ParticleFilter(["photon"])
 
     # name, scores, filters
     return (
-        ("TBR", "(n,Xt)", []),
-        # this is only the THEORETICAL TBR, does not account for extraction losses.
-        # we can use the (n,Xt) score because the Lithium produces a maximum of 1 Tritium
-        # per reaction, so there won't be issue of under-counting.
-        ("material heating", "heating", [mat_filter]),  # eV per sp
-        ("neutron wall load", "damage-energy", [fw_surf_filter, neutron_filter]),
-        ("photon heating", "heating", [fw_surf_filter, photon_filter]),
-        # skipped
-        # ("neutron flux in every cell", "flux", [cell_filter, neutron_filter]),
+        ("TBR", "(n,Xt)", []),  # theoretical maximum TBR only, obviously.
+        # Powers
+        ("Total power", "heating", []),
+        ("divertor power", "heating", []),
+        ("vacuum vessel power", "heating", [vv_filter]),
+        ("breeding blanket power", "heating", [bz_filter]),
+        # Fluence
+        ("neutron flux in every cell", "flux", [cell_filter, neutron_filter]),
         # ("neutron flux in 2d mesh", "flux", [cyl_mesh_filter, neutron_filter]),
+        # TF winding pack does not exits yet, so this will have to wait
+        # DPA
+        ("eurofer damage", "damage-energy", [cell_filter, eurofer_filter]),
+        # used to get the EUROFER OBMP
+        ("divertor damage", "damage-energy", [div_cell_filter, mat_filter]),
+        ("vacuum vessel damage", "damage-energy", [vv_filter]),
     )
