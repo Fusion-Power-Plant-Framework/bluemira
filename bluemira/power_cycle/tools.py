c85815a66fcfe93f5f41171be2ddf95b24a2e893
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2024-06-04 09:27:26 +0200

    bring current state of functions from `powercycle_tools` without merge

--- bluemira/power_cycle/tools.py
+++ bluemira/power_cycle/tools.py
@@ -14,13 +14,16 @@ from pprint import PrettyPrinter
 from typing import Any, Dict, List
 
 import matplotlib.pyplot as plt
+import numba as nb
 import numpy as np
 
 from bluemira.base.constants import EPS
 
 
 def read_json(file_path) -> Dict[str, Any]:
-    """Return the contents of a 'json' file."""
+    """
+    Returns the contents of a 'json' file.
+    """
     with open(file_path) as json_file:
         return json.load(json_file)
 
@@ -34,6 +37,92 @@ def create_axes(ax=None):
     if ax is None:
         _, ax = plt.subplots()
     return ax
+
+
+@nb.jit
+def _nudge_check(x_last, x_this):
+    is_close = np.isclose(x_last, x_this, rtol=EPS)
+    is_decreasing = x_last > x_this
+    return is_close or is_decreasing
+
+
+@nb.jit
+def validate_monotonic_increase(x, strict_flag):
+    """Validate that vector is (strictly) monotonically increasing."""
+    for x_last, x_this in zip(x[:-1], x[1:]):
+        if strict_flag and not (x_this > x_last):
+            raise ValueError("Vector is not strictly monotonically increasing.")
+        if not strict_flag and not (x_this >= x_last):
+            raise ValueError("Vector is not monotonically increasing.")
+
+
+@nb.jit
+def unique_domain(x: np.ndarray, epsilon: float = 1e-10):
+    """
+    Ensure x has only unique values to make (Domain: x -> Image: y) a function.
+
+    To be a function domain, x must be strictly monotonically increasing. So x
+    must start at least as a monotonically increasing vector. The function then
+    ensures strict monotonicity by nudging forward repeated values by a small
+    epsilon.
+
+    Epsilon must be small enough so that consecutive values can be considered
+    equal within the context/scale in which the function is defined.
+
+    The nudge forward for each non-unique element in x is (N * epsilon), given
+    N times, after the first appearance, that value has appeared in x before.
+    """
+    validate_monotonic_increase(x, strict_flag=False)
+    new_x = [x[0]]
+    if len(x) > 1:
+        for x_this in x[1:]:
+            nudge = 0
+            new_x_this = x_this
+            needs_nudge = _nudge_check(new_x[-1], x_this)
+            if needs_nudge:
+                while needs_nudge:
+                    nudge += epsilon
+                    new_x_this = x_this + nudge
+                    needs_nudge = _nudge_check(new_x[-1], new_x_this)
+            else:
+                new_x_this = x_this
+            new_x.append(new_x_this)
+    validate_monotonic_increase(new_x, strict_flag=True)
+    return np.asarray(new_x)
+
+
+def match_domains(
+    all_x: List[np.ndarray],
+    all_y: List[np.ndarray],
+    epsilon: float = 1e-10,
+):
+    """
+    Match the domains of multiple functions, each represented by 2 vectors.
+
+    First, for each pair of vectors (x,y) that define a function, the domain
+    x is ensured to be unique with 'unique_domain' (otherwise, calling 'unique'
+    on 'x_matched' later can neglect step functions).
+
+    Then, for each pair (x,y), values in every 'y' vector are interpolated,
+    to ensure that all of them have one element associated to every element
+    of the union of all distinct 'x' vectors. Values defined before and after
+    the original domain 'x' of the image 'y' are set to zero.
+    """
+    n_vectors = len(all_x)
+    for v in range(n_vectors):
+        all_x[v] = unique_domain(all_x[v], epsilon=epsilon)
+    x_matched = np.unique(np.concatenate(all_x))
+
+    all_y_matched = all_y.copy()
+    for v in range(n_vectors):
+        all_y_matched[v] = np.interp(
+            x_matched,
+            all_x[v],
+            all_y[v],
+            left=0,
+            right=0,
+        )
+    return x_matched, all_y_matched
 
 
 def symmetrical_subplot_distribution(n_plots, direction="row"):
@@ -56,96 +145,6 @@ def symmetrical_subplot_distribution(n_plots, direction="row"):
             "'row' or 'col'."
         )
     return n_rows, n_cols
-
-
-def match_domains(
-    x_set: List[np.ndarray],
-    y_set: List[np.ndarray],
-    epslon=1e-10,
-    mode="careful",
-):
-    """
-    Match the domains of multiple functions, each represented by 2 vectors.
-
-    First, for each pair of vectors (x,y) that define a function, the domain
-    x is ensured to be unique with 'unique_domain' (otherwise, calling 'unique'
-    on 'matched_x' later can neglect step functions).
-
-    Then, for each pair (x,y), values in every 'y' vector are interpolated,
-    to ensure that all of them have one element associated to every element
-    of the union of all distinct 'x' vectors. Values defined before and after
-    the original domain 'x' of the image 'y' are set to zero.
-    """
-    n_vectors = len(x_set)
-    for v in range(n_vectors):
-        x_set[v], y_set[v] = unique_domain(x_set[v], y_set[v], epslon, mode)
-
-    matched_x = np.concatenate(x_set)
-    matched_x = np.unique(matched_x)
-
-    for v in range(n_vectors):
-        x = x_set[v]
-        y = y_set[v]
-
-        y = np.interp(matched_x, x, y, left=0, right=0)
-        y_set[v] = y
-
-    return matched_x, y_set
-
-
-def unique_domain(x, y, epslon=1e-10, mode="careful"):
-    """
-    Ensure x has only unique values to make (Domain: x -> Image: y) a function.
-
-    Epslon must be small enough so that consecutive values can be considered
-    equal within the context/scale in which the function is defined.
-
-    Careful mode:
-    -------------
-    Nudge forward each non-unique element in x by (N * epslon), given N times,
-    after the first appearance, that value has appeared in x before.
-
-    Fast mode:
-    ---------
-    Nudge forward every element in x by (N * epslon), with N being the index
-    of that element in x. More prone to errors if the number of elements in
-    x is large.
-    """
-    n_points = len(x)
-    if len(y) != n_points:
-        raise ValueError("x and y must have the same number of elements.")
-
-    slow_identifiers = {"careful", "c", "slow", "s"}
-    fast_identifiers = {"fast", "f"}
-
-    if mode in slow_identifiers:
-        new_x = [x[0]]
-        new_y = [y[0]]
-        nudge = 0
-        for p in range(1, n_points):
-            x_last = x[p - 1]
-            x_this = x[p]
-            y_this = y[p]
-            if np.isclose(x_last, x_this, rtol=EPS):
-                nudge += epslon
-                x_this = x_last + nudge
-            else:
-                nudge = 0
-            new_x.append(x_this)
-            new_y.append(y_this)
-
-    elif mode in fast_identifiers:
-        nudge_vector = np.arange(n_points) * epslon
-        new_x = x.copy() + nudge_vector
-        new_y = y.copy()
-
-    else:
-        raise ValueError(
-            f"Invalid argument: '{mode}'. The parameter 'mode' can only"
-            f"assume one of the following values: {slow_identifiers} or "
-            f"{fast_identifiers}."
-        )
-    return new_x, new_y
 
 
 def recursive_value_types_in_dict(dictionary):
