ce75ac5df004ecb52a7fafa8775cd4b9267afeae
Georgina Graham <georgina.graham@ukaea.uk>
2024-06-18 13:15:33 +0100

    format

--- tests/radiation_transport/test_flux_surface_maker.py
+++ tests/radiation_transport/test_flux_surface_maker.py
@@ -29,8 +29,8 @@ TEST_PATH = get_bluemira_path("equilibria/test_data", subfolder="tests")
 
 def test_process_first_wall():
     converted_from_open_cw = _process_first_wall(
-        Coordinates({"x" : [1, 1, 2, 2], "z" : [1, 2, 2, 1]})
-        )
+        Coordinates({"x": [1, 1, 2, 2], "z": [1, 2, 2, 1]})
+    )
     closed_ccw_x = [2, 2, 1, 1, 2]
     closed_ccw_z = [1, 2, 2, 1, 1]
     for x1, x2, z1, z2 in zip(
@@ -38,17 +38,16 @@ def test_process_first_wall():
         converted_from_open_cw.x,
         closed_ccw_z,
         converted_from_open_cw.z,
-        strict=False):
+        strict=False,
+    ):
         assert x1 == x2
         assert z1 == z2
 
 
 def test_gets():
-    coords = Coordinates({"x" : [0, 1, 1, 2, 2, 1.2, 2],
-                          "z" : [3, 2, 1, 1, 2, 2, 3]})
+    coords = Coordinates({"x": [0, 1, 1, 2, 2, 1.2, 2], "z": [3, 2, 1, 1, 2, 2, 3]})
     pofs = PartialOpenFluxSurface(coords)
-    fw = Coordinates({"x": [0, 3, 3, 0, 0],
-                      "z": [0, 0, 2.5, 2.5, 0]})
+    fw = Coordinates({"x": [0, 3, 3, 0, 0], "z": [0, 0, 2.5, 2.5, 0]})
 
     xmp = coords.x[0]
     zmp = coords.z[0]
@@ -71,14 +70,11 @@ class TestMakeFS:
         cls.eq = Equilibrium.from_eqdsk(Path(TEST_PATH, eq_name))
         cls.o_point = cls.eq.get_OX_points()[0][0]  # 1st o_point
         cls.yz_plane = BluemiraPlane.from_3_points(
-            [0, 0, cls.o_point.z],
-            [1, 0, cls.o_point.z],
-            [1, 1, cls.o_point.z]
-            )
-        cls.first_wall = Coordinates({
-            "x": [5.5, 15, 15, 5.5, 5.5],
-            "z": [-7.5, -7.5, 7.5, 7.5, -7.5]
-        })
+            [0, 0, cls.o_point.z], [1, 0, cls.o_point.z], [1, 1, cls.o_point.z]
+        )
+        cls.first_wall = Coordinates(
+            {"x": [5.5, 15, 15, 5.5, 5.5], "z": [-7.5, -7.5, 7.5, 7.5, -7.5]}
+        )
 
     def test_make_flux_surfaces_ibob(self):
         x_sep_omp, x_out_omp = _get_sep_out_intersection(
@@ -94,7 +90,7 @@ class TestMakeFS:
             self.yz_plane,
             x_sep_omp,
             x_out_omp,
-            outboard=True
+            outboard=True,
         )
         lower_fs_ib, upper_fs_ib = _make_flux_surfaces_ibob(
             0.001,
@@ -103,7 +99,7 @@ class TestMakeFS:
             self.yz_plane,
             x_sep_imp,
             x_out_imp,
-            outboard=False
+            outboard=False,
         )
         assert len(lower_fs_ob) == 1070
         assert len(upper_fs_ob) == 1070
