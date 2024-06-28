d86d64bacd32983994b049e61de0eaadcc54edc6
je-cook <81617086+je-cook@users.noreply.github.com>
2024-06-28 13:39:32 +0100

    ✨ Add Neutronics Manager to eudemo (#3396)
    * 🍻 Neutronics manager etc
    
    * 🚨 Linter fixes
    
    * 🎨 Review comments
    
    * 🎨 Show neutronics data
    
    * 🎨 Review suggestions

--- eudemo/eudemo/model_managers.py
+++ eudemo/eudemo/model_managers.py
@@ -9,12 +9,19 @@ EUDEMO model manager classes
 
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from bluemira.equilibria.run import Snapshot
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 from bluemira.base.look_and_feel import bluemira_warn
+
+if TYPE_CHECKING:
+    from bluemira.codes.openmc.output import OpenMCResult
+    from bluemira.equilibria.run import Snapshot
+    from eudemo.eudemo.neutronics.run import EUDEMONeutronicsCSGReactor
 
 
 class EquilibriumManager:
@@ -48,3 +55,48 @@ class EquilibriumManager:
         Get an equilibrium state from the Equilibrium manager.
         """
         return self.states.get(name, None)
+
+
+class NeutronicsManager:
+    """Manager for neutronics"""
+
+    def __init__(
+        self,
+        csg_reactor: EUDEMONeutronicsCSGReactor,
+        results: OpenMCResult | dict[int, float],
+    ):
+        self.csg_reactor = csg_reactor
+        self.results = results
+
+    def plot(self):
+        """Plot neutronics results"""
+        _f, ax = plt.subplots()
+        ax.axis("off")
+        ax.imshow(
+            np.asarray(
+                Image.open(
+                    Path(__file__).parent.parent
+                    / "config"
+                    / "neutronics"
+                    / "plot"
+                    / "plot_1.png"
+                )
+            )
+        )
+
+        self.csg_reactor.plot_2d()
+
+        plt.show()
+
+    def tabulate(self) -> str:
+        """Tabulate results"""
+        if hasattr(self.results, "_tabulate"):
+            return str(self.results)
+        raise NotImplementedError("Tabulate not available for volume calculation")
+
+    def __str__(self) -> str:
+        """String Representation"""
+        try:
+            return self.tabulate()
+        except NotImplementedError:
+            return super().__str__()
