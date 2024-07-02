ff8b09fbe0f212b7966e24ffa18de3305a808250
Tiago <40567154+tplobo@users.noreply.github.com>
2024-07-02 15:02:47 +0200

    add functions to manipulate (x,y) sets of vectors to enable representation of time-dependent mathematical functions (#3250)
    * add functions to manipulate (x,y) sets of vectors to enable representation of time-dependent mathematical functions
    
    * add local gitignore for SVG files in powercycle tests
    
    * re-written `unique_domains` with enumerator class
    
    * change `unique_domain` fast mode argument to boolean
    
    * shift `nudge = 0` line
    
    * fix bugs to make tests run
    
    * remove fast mode of `unique_domain`
    
    * remove pairwise, add numba.jit for speed increase to `unique_domain`
    
    * remove 'y' argument from `unique_domain`
    
    * rename arguments in `match_domains`
    
    * implement review comments, add tests and fix for `unique_domain` for extreme cases
    
    * clean-up and make compatible with `numba`
    
    * erase timing test to comply with review
    
    * add test for function that checks monotonic increase
    
    * rename function that checks monotonic increase
    
    * make `validate_monotonic_increase` function public
    
    * modify `match_domains` to type-cast list to avoid issues with numba
    
    * fix deprecation warnings from numba
    
    * ammend previous fix for `numba` deprecation warnings
    
    * alter `unique_domains` to have a maximum number of nudges per point
    
    * relocate error check in `unique_domain`
    
    * pre-fix for numpy 2.0 in `match_domains`
    
    * removed unnecessary `.gitignore`
    
    * remove unnecessary check for strict monotonically increase in `unique_domain`

---
+++ tests/power_cycle/test_tools.py
@@ -0,0 +1,89 @@
+# SPDX-FileCopyrightText: 2021-present M. Coleman, J. Cook, F. Franza
+# SPDX-FileCopyrightText: 2021-present I.A. Maione, S. McIntosh
+# SPDX-FileCopyrightText: 2021-present J. Morris, D. Short
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import numpy as np
+import pytest
+
+from bluemira.power_cycle.tools import (
+    match_domains,
+    unique_domain,
+    validate_monotonic_increase,
+)
+
+
+def test_validate_monotonic_increase():
+    x_strict_monotonic = np.array([1, 2, 3, 4, 5])
+    x_monotonic = np.array([1, 2, 2, 4, 5])
+    x_not_increasing = np.array([1, 2, 3, 2, 1])
+    msg_not_strict = "Vector is not strictly monotonically increasing."
+    msg_not_increasing = "Vector is not monotonically increasing."
+
+    validate_monotonic_increase(x_strict_monotonic, strict_flag=True)
+    validate_monotonic_increase(x_monotonic, strict_flag=False)
+
+    with pytest.raises(ValueError, match=msg_not_strict):
+        validate_monotonic_increase(x_monotonic, strict_flag=True)
+
+    with pytest.raises(ValueError, match=msg_not_increasing):
+        validate_monotonic_increase(x_not_increasing, strict_flag=False)
+
+
+@pytest.mark.parametrize("epsilon", [1e-10, 1e-7, 1e-5])
+def test_unique_domain(epsilon):
+    x = np.array([1, 1, 2, 2, 3, 3])
+    new_x = unique_domain(x, epsilon=epsilon)
+    expected_x = np.array([
+        1,
+        1 + epsilon,
+        2,
+        2 + epsilon,
+        3,
+        3 + epsilon,
+    ])
+    assert all(np.isclose(new_x, expected_x))
+
+    x = np.array([2, 2, 2, 2 + (epsilon / 2)])
+    new_x = unique_domain(x, epsilon=epsilon)
+    expected_x = np.array([
+        2,
+        2 + 1 * epsilon,
+        2 + 2 * epsilon,
+        2 + 2 * epsilon + (epsilon / 2),
+    ])
+    assert all(np.isclose(new_x, expected_x))
+
+    x = np.array([2, 2, 2, 2 + epsilon, 3, 3])
+    new_x = unique_domain(x, epsilon=epsilon)
+    expected_x = np.array([
+        2,
+        2 + 1 * epsilon,
+        2 + 2 * epsilon,
+        2 + 3 * epsilon,
+        3,
+        3 + 1 * epsilon,
+    ])
+    assert all(np.isclose(new_x, expected_x))
+
+    x = np.array([1, 1, 1, 1, 1])
+    max_iterations = 3
+    with pytest.raises(ValueError, match="Maximum number of iterations"):
+        unique_domain(x, max_iterations=max_iterations)
+
+
+def test_match_domains():
+    all_x = [np.array([1, 2, 3]), np.array([2, 3, 4])]
+    all_y = [np.array([1, 2, 3]), np.array([4, 6, 8])]
+    out_of_bounds_value = 0
+
+    x_expected = np.array([1, 2, 3, 4])
+    all_y_expected = [
+        np.array([1, 2, 3, out_of_bounds_value]),
+        np.array([out_of_bounds_value, 4, 6, 8]),
+    ]
+    x_matched, all_y_matched = match_domains(all_x, all_y)
+    assert np.array_equal(x_matched, x_expected)
+    assert np.array_equal(all_y_matched[0], all_y_expected[0])
+    assert np.array_equal(all_y_matched[1], all_y_expected[1])
