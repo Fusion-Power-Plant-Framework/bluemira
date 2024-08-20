25822f3672e709e598b9a7b361e41b23acca8ccd
Georgina Graham <georgina.graham@ukaea.uk>
2024-08-20 14:56:58 +0100

    convert exmple type

---
+++ examples/equilibria/anaylsis_toolbox_examples.ex.py
@@ -0,0 +1,61 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from pathlib import Path
+import pandas as pd
+from bluemira.base.file import get_bluemira_path
+from bluemira.equilibria.diagnostics import EqDiagnosticOptions
+from bluemira.equilibria.analysis import select_eq, EqAnalysis, MultiEqAnalysis
+
+# %pdb
+
+# %%
+ref_path = Path("../../tests/equilibria/test_data/SH_test_file.json")
+ref_eq = select_eq(ref_path)
+eq_path = Path("../../tests/equilibria/test_data/eqref_OOB.json")
+eq = select_eq(eq_path, from_cocos=7)
+
+# %%
+diag_ops = EqDiagnosticOptions(
+    psi_diff=True, split_psi_plots=False, reference_eq=ref_eq,
+)
+
+analysis = EqAnalysis(diag_ops, eq)
+
+# %%
+analysis.plot_equilibria_with_profiles()
+
+# %%
+analysis.plot_compare_psi()
+
+# %%
+analysis.plot_compare_profiles()
+
+# %%
+p1 = ref_path
+p2 = eq_path
+
+equilibrium_names = ['MASTy Eq', 'DEMOish Eq']
+
+multi_analysis = MultiEqAnalysis([p1, p2], equilibrium_names=equilibrium_names, from_cocos=[3, 7])
+
+# %%
+pd = multi_analysis.coilset_info_table()
+
+# %%
+pd.style.set_caption("Current (MA)")
+
+# %%
+multi_analysis.plot_compare_profiles()
