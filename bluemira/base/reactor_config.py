6ce3e63e5cff9af497fafa7d08e4c718b12b11c8
Oliver Funk <oli.funk@gmail.com>
2024-09-02 17:31:04 +0100

    did rest

--- bluemira/base/reactor_config.py
+++ bluemira/base/reactor_config.py
@@ -132,7 +132,13 @@ class ReactorConfig:
             bluemira_warn("Empty global params")
 
     def __str__(self) -> str:
-        """Returns config_data as a nicely pretty formatted string"""
+        """Returns config_data as a nicely pretty formatted string.
+
+        Returns
+        -------
+        :
+            The pretty formatted string of the config_data.
+        """
         return self._pprint_dict(self.config_data)
 
     @staticmethod
@@ -292,6 +298,10 @@ class ReactorConfig:
 
         rel_path is the path to the file that the value is in.
 
+        Returns
+        -------
+            Tuple of the file data and the path to the file if value is a path.
+
         Raises
         ------
         FileNotFoundError
@@ -299,7 +309,7 @@ class ReactorConfig:
 
         Notes
         -----
-        If the value is not a path, returns the value and the passed in rel_path.
+            If the value is not a path, returns the value and the passed in rel_path.
         """
         if not isinstance(value, str):
             return value, rel_path
