a3702b03ec675d164ae0e103a0cfe8601399afba
james <james.cook1@ukaea.uk>
2024-09-13 07:58:55 +0100

    ✨ Coil resistance

--- bluemira/equilibria/coils/_grouping.py
+++ bluemira/equilibria/coils/_grouping.py
@@ -699,6 +699,11 @@ class CoilGroup(CoilGroupFieldsMixin):
         return self.__getter("b_max")
 
     @property
+    def resistance(self) -> np.ndarray:
+        """Get coil resistance"""
+        return self.__getter("resistance")
+
+    @property
     def discretisation(self) -> np.ndarray:
         """Get coil discretisations"""
         return self.__getter("discretisation")
@@ -827,6 +832,11 @@ class CoilGroup(CoilGroupFieldsMixin):
     @b_max.setter
     def b_max(self, values: float | Iterable[float]):
         """Set coil max fields"""
+        self.__setter("b_max", values)
+
+    @resistance.setter
+    def resistance(self, values: float | Iterable[float]):
+        """Set coil resistance"""
         self.__setter("b_max", values)
 
     @discretisation.setter
