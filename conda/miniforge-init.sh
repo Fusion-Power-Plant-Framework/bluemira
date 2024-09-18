f3f7edde520c4ba941470a405cbc8f5453df6e81
james <81617086+je-cook@users.noreply.github.com>
2024-09-18 06:32:27 +0100

    🐛 Fix miniforge init script

--- conda/miniforge-init.sh
+++ conda/miniforge-init.sh
@@ -1,12 +1,12 @@
 # >>> conda initialize >>>
-__conda_setup="$('$HOME/mambaforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('$HOME/miniforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "$HOME/mambaforge/etc/profile.d/conda.sh" ]; then
-        . "$HOME/mambaforge/etc/profile.d/conda.sh"
+    if [ -f "$HOME/miniforge/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniforge/etc/profile.d/conda.sh"
     else
-        export PATH="$HOME/mambaforge/bin:$PATH"
+        export PATH="$HOME/miniforge/bin:$PATH"
     fi
 fi
 unset __conda_setup
