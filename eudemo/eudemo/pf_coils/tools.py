a48529f6dc19c55a7334541933024f204f6c3337
james <81617086+je-cook@users.noreply.github.com>
2024-10-17 14:04:09 +0100

    🎨 Rebase fixes

--- eudemo/eudemo/pf_coils/tools.py
+++ eudemo/eudemo/pf_coils/tools.py
@@ -386,7 +386,7 @@ def make_pf_coil_path(tf_boundary: BluemiraWire, offset_value: float) -> Bluemir
     )
 
     # # Find top-left and bottom-left "corners"
-    # coordinates = tf_offset.discretize(byedges=True, ndiscr=200)
+    # coordinates = tf_offset.discretise(byedges=True, ndiscr=200)
     # #x_min = np.min(coordinates.x)
     # z_min, z_max = 0.0, 0.0
     # eps = 0.0
