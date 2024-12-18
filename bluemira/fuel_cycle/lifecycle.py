a4a2e9099fbb2f5b264703a2a12472308c2e384b
james <81617086+je-cook@users.noreply.github.com>
2024-12-18 14:07:21 +0000

    🐛 List entry

--- bluemira/fuel_cycle/lifecycle.py
+++ bluemira/fuel_cycle/lifecycle.py
@@ -297,7 +297,7 @@ class LifeCycle:
             self.params.vv_dmg,
             self.params.vv_dpa,
             self.availability_strategy,
-            self.rng.spawn(1),
+            self.rng.spawn(1)[0],
         )
         self.T = timeline
         self.t_unplanned_m = self.T.t_unplanned_m
