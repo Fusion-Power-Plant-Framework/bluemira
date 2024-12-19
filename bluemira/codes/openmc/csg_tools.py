bf1c09ca5fc80c93ef0c6184bf348b3e37a60331
ocean <OceanWongUK@gmail.com>
2024-12-19 14:00:40 +0000

    Adding docstrings

--- bluemira/codes/openmc/csg_tools.py
+++ bluemira/codes/openmc/csg_tools.py
@@ -7,6 +7,7 @@
 The OpenMC Environment, and tools to interact with it by creating surfaces and defining
 regions inside of this environment.
 """
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
