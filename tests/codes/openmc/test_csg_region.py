ecb337fa32d420db76241b408335eba5752bbb10
ocean <OceanWongUK@gmail.com>
2024-12-19 14:07:17 +0000

    Created a test file for each of the three new openmc-csg-making modules in codes/openmc/*.py

---
+++ tests/codes/openmc/test_csg_region.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2021-present M. Coleman, J. Cook, F. Franza
+# SPDX-FileCopyrightText: 2021-present I.A. Maione, S. McIntosh
+# SPDX-FileCopyrightText: 2021-present J. Morris, D. Short
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+from bluemira.codes.openmc.make_csg import (
+    OpenMCEnvironment,
+)
+
+
+def test_cnf():
+    OpenMCEnvironment
---
+++ tests/codes/openmc/test_make_cell.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2021-present M. Coleman, J. Cook, F. Franza
+# SPDX-FileCopyrightText: 2021-present I.A. Maione, S. McIntosh
+# SPDX-FileCopyrightText: 2021-present J. Morris, D. Short
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+
+def test_convexity_cells(): ...
+
+
+def test_convexity_cell_stacks(): ...
+
+
+def test_convexity_cell_arrays(): ...
+
+
+def test_in_cell():
+    """Checking the functionality of each cell"""
+    ...
