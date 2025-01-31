dd74fac4aee438ed75d30a9b55ab6c9ea06c6d0b
james <81617086+je-cook@users.noreply.github.com>
2025-01-31 08:04:17 +0000

    👽️ More...

--- tests/codes/process/test_template_builder.py
+++ tests/codes/process/test_template_builder.py
@@ -384,7 +384,7 @@ class TestInDatOneForOne:
             "m_s_limit": 0.1,
             "triang": 0.5,
             "q0": 1.0,
-            "ssync": 0.6,
+            "f_sync_reflect": 0.6,
             "plasma_res_factor": 0.66,
             "gamma": 0.3,
             "hfact": 1.1,
