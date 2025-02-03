9346d61f1822b68e17f1fd01ac9150f5f6972305
james <81617086+je-cook@users.noreply.github.com>
2025-02-03 12:01:11 +0000

    🐛 Fix rename

--- tests/codes/process/test_template_builder.py
+++ tests/codes/process/test_template_builder.py
@@ -400,7 +400,7 @@ class TestInDatOneForOne:
             "blnkoth": 0.982,
             "d_vv_out": 0.3,
             "shldoth": 0.8,
-            "z_cryostat_half_inside": 0.15,
+            "dr_cryostat": 0.15,
             "gapomin": 0.2,
             # Vertical build inputs
             "d_vv_top": 0.3,
