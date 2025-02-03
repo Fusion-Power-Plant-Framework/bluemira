b82f3d02c2838edf0257f55a025c5f30bc89a6b4
james <81617086+je-cook@users.noreply.github.com>
2025-02-03 15:54:24 +0000

    👽️ Scrapli

--- examples/codes/run_process_example.ex.py
+++ examples/codes/run_process_example.ex.py
@@ -229,7 +229,7 @@ template_builder.add_input_values({
     "dr_shld_inboard": 0.3,
     "vvblgap": 0.02,
     "dr_blkt_inboard": 0.755,
-    "scrapli": 0.225,
+    "dr_fw_plasma_gap_inboard": 0.225,
     "scraplo": 0.225,
     "dr_blkt_outboard": 0.982,
     "d_vv_out": 0.3,
