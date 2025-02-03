6329adad81d0bf35378fbe045b78698ba84a4e12
james <81617086+je-cook@users.noreply.github.com>
2025-02-03 15:45:30 +0000

    👽️ Tftsgap

--- bluemira/codes/process/mapping.py
+++ bluemira/codes/process/mapping.py
@@ -100,7 +100,7 @@ IO_mappings = {
     "tk_sol_ib": ("scrapli", "m"),
     "tk_sol_ob": ("scraplo", "m"),
     "g_cs_tf": ("dr_cs_tf_gap", "m"),
-    "g_ts_tf": ("tftsgap", "m"),
+    "g_ts_tf": ("dr_tf_shld_gap", "m"),
     "g_vv_bb": ("vvblgap", "m"),
 }
 
