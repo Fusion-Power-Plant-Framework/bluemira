fdc72ab47130d16357234869ac3cf044f8346c7b
james <81617086+je-cook@users.noreply.github.com>
2025-02-18 13:44:40 +0000

    👽️ Process 3108

--- bluemira/codes/process/mapping.py
+++ bluemira/codes/process/mapping.py
@@ -127,13 +127,7 @@ NONE_mappings = {
     "tk_ts": ("dr_shld_thermal_inboard", "m"),
     # "tk_ts": ("dr_shld_thermal_outboard", "m"),
     # "tk_ts": ("thshield_vb", "m"),
-    # TODO @CoronelBuendia: q is not properly put in the MFILE output
-    # This should be ok OK most of the time as q_95 is input and then
-    # used as the lower bound of the q iteration variable, but this
-    # should be fixed as soon as PROCESS deal with this issue on
-    # their side
-    # 3650
-    "q_95": ("q", "dimensionless"),
+    "q_95": ("q95", "dimensionless"),
 }
 
 mappings = create_mapping(IN_mappings, OUT_mappings, IO_mappings, NONE_mappings)
