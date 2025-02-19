01dcb1c7c67b00891b87d1a579374110462c3da7
Nilima, Athoy <athoy.nilima@ukaea.uk>
2025-02-19 13:39:20 +0000

    move the function to read radial build for plotting to teardown

--- bluemira/codes/process/_solver.py
+++ bluemira/codes/process/_solver.py
@@ -234,7 +234,7 @@ class Solver(CodesSolver):
         if not self._teardown:
             return None
 
-        radial_build = self._teardown._mfile_wrapper.get_radial_build_dict()
+        radial_build = self._teardown.get_radial_build_for_plotting()
         if not radial_build:
             bluemira_warn("MFILE.DAT file in old format. Cannot plot radial build.")
             return None
