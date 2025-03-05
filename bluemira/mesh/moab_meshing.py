19ea1df610d3cf1a87e49e27d5b999cee54d405b
Oliver Funk <oli.funk@gmail.com>
2025-03-05 16:55:43 +0000

    commiting at imporant milestone. I figured out how to do the solid <-> face mapping

--- bluemira/mesh/moab_meshing.py
+++ bluemira/mesh/moab_meshing.py
@@ -1,14 +1,21 @@
 from collections.abc import Iterable
+from copy import deepcopy
 from pathlib import Path
 
+from rich.progress import track
+
 from bluemira.geometry.base import BluemiraGeoT
-from bluemira.geometry.overlap_checking import find_approx_overlapping_pairs
-from bluemira.geometry.solid import BluemiraSolid
+from bluemira.geometry.imprint_solids import imprint_solids
+
+try:
+    from pymoab import core, types
+
+    pymoab_available = True
+except ImportError:
+    pymoab_available = False
 
 
-def imprint_solids(solids: Iterable[BluemiraSolid]):
-    """Imprints solids together."""
-    pairs = find_approx_overlapping_pairs(solids)
+# def moab_mesh():
 
 
 def save_cad_to_dagmc_model(
@@ -19,3 +26,45 @@ def save_cad_to_dagmc_model(
     faceting_tolerance=0.001,
 ):
     """Converts the shapes with their associated names to a dagmc file using PyMOAB."""
+    if not pymoab_available:
+        raise ImportError("PyMOAB is required to convert CAD to DAGMC.")
+
+    imprinted_solids = []
+
+    # do a per compound imprint for now.
+    # In the future, one should extract all solids then do the imprint on _all_ of them
+    for shape in track(shapes):
+        if isinstance(shape, BluemiraCompound):
+            imprinted_solids.extend(imprint_solids(shape.solids))
+        else:
+            imprinted_solids.append(shape)
+
+    # for each solid, extract the face
+    # look up the face to see if it exists
+    # if it does, associate the face with the solid (this one)
+    # if it doesn't, create the face and associate it with the solid
+    # it's mesh (faceting)
+
+
+if __name__ == "__main__":
+    from bluemira.geometry.base import BluemiraGeoT
+    from bluemira.geometry.compound import BluemiraCompound
+    from bluemira.geometry.face import BluemiraFace
+    from bluemira.geometry.tools import (
+        extrude_shape,
+        make_polygon,
+    )
+
+    # Create a box
+    box_a = BluemiraFace(
+        make_polygon([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 0, 0]])
+    )
+    box_a = extrude_shape(box_a, [0, 0, 1])
+    box_b = deepcopy(box_a)
+    box_b.translate([-0.6, -0.6, 1])
+    box_c = deepcopy(box_a)
+    box_c.translate([0.6, 0.6, 1])
+
+    pre_imps = [box_a, box_b, box_c]
+    # show_cad(pre_imps)
+    imps, imp_faces = imprint_solids(pre_imps)
