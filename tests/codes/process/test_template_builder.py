f753c6dba023e8fe063c8a6fa40312734f4715e5
ocean <OceanWongUK@gmail.com>
2025-04-15 15:06:13 +0100

    Process 3624

--- tests/codes/process/test_template_builder.py
+++ tests/codes/process/test_template_builder.py
@@ -474,7 +474,7 @@ class TestInDatOneForOne:
             "abktflnc": 15.0,
             "adivflnc": 20.0,
             # For sanity...
-            "hldivlim": 10,
+            "pflux_div_heat_load_max_mw": 10,
             "prn1": 0.4,
             "bmxlim": 11.2,
             "ffuspow": 1.0,
