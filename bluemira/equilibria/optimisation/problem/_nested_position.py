ce97006ee301e1d995dbbb0fc8eb654424bd7124
Matti <matti.coleman@gmail.com>
2025-04-22 16:59:49 +0200

    imports

--- bluemira/equilibria/optimisation/problem/_nested_position.py
+++ bluemira/equilibria/optimisation/problem/_nested_position.py
@@ -29,20 +29,20 @@ import numpy as np
 import numpy.typing as npt
 
 from bluemira.base.look_and_feel import bluemira_print_flush
-from bluemira.equilibria.coils import CoilSet
-from bluemira.equilibria.equilibrium import Equilibrium
-from bluemira.equilibria.optimisation.constraints import (
-    UpdateableConstraint,
-)
 from bluemira.equilibria.optimisation.problem.base import (
     CoilsetOptimisationProblem,
     CoilsetOptimiserResult,
 )
 from bluemira.optimisation import Algorithm, AlgorithmType, optimise
-from bluemira.utilities.positioning import PositionMapper
 
 if TYPE_CHECKING:
+    from bluemira.equilibria.coils import CoilSet
+    from bluemira.equilibria.equilibrium import Equilibrium
+    from bluemira.equilibria.optimisation.constraints import (
+        UpdateableConstraint,
+    )
     from bluemira.equilibria.run import MovingCurrentBoundStrategy
+    from bluemira.utilities.positioning import PositionMapper
 
 
 class NestedCoilsetPositionCOP(CoilsetOptimisationProblem):
