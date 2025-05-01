00d645f39118494e886d6a9294992c6915c39e8b
Oliver Funk <oli.funk@gmail.com>
2025-05-01 16:43:00 +0100

    ruff

--- tests/geometry/test_imprint_solids.py
+++ tests/geometry/test_imprint_solids.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2021-present M. Coleman, J. Cook, F. Franza
+# SPDX-FileCopyrightText: 2021-present I.A. Maione, S. McIntosh
+# SPDX-FileCopyrightText: 2021-present J. Morris, D. Short
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
 from copy import deepcopy
 
 from bluemira.geometry.face import BluemiraFace
