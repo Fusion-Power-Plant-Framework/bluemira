0aa8c1bad2ed5e2c52a6cce6aac41265a5cb7d55
Oliver Funk <oli.funk@gmail.com>
2025-05-21 15:05:41 +0100

    updated guards

--- bluemira/codes/fast_ctd/_guard.py
+++ bluemira/codes/fast_ctd/_guard.py
@@ -11,5 +11,7 @@ from bluemira.codes.utilities import code_guard
 
 fast_ctd_guard = code_guard(
     "fast_ctd",
-    "fast_ctd is not available. To install, go to https://github.com/Fusion-Power-Plant-Framework/fast_ctd",
+    "To install, go to https://github.com/Fusion-Power-Plant-Framework/fast_ctd "
+    "or install directly (occt>=7.8.0 is required) "
+    "run `pip install fast_ctd@git+https://github.com/Fusion-Power-Plant-Framework/fast_ctd@main`",
 )
