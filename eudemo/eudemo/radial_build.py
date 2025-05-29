19c4d4434e8e309eab932cf5523206ee9bb280e0
james <81617086+je-cook@users.noreply.github.com>
2025-05-29 13:39:05 +0100

    🎨 More changes

--- eudemo/eudemo/radial_build.py
+++ eudemo/eudemo/radial_build.py
@@ -289,7 +289,7 @@ template_builder.add_input_values({
     "abktflnc": 15.0,
     "adivflnc": 20.0,
     # For sanity...
-    "pflux_div_heat_loadfp_fusion_total_max_mw": 10,
+    "pflux_div_heat_load_max_mw": 10,
     "prn1": 0.4,
     "bmxlim": 11.2,
     "fp_fusion_total_max_mw": 1.0,
