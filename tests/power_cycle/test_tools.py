55e41947ecf453382103cae9d3bd2b2448a30b5e
james <81617086+je-cook@users.noreply.github.com>
2025-06-02 10:24:20 +0100

    🐛 Fix a couple of bugs, linter warnings

--- tests/power_cycle/test_tools.py
+++ tests/power_cycle/test_tools.py
@@ -18,13 +18,12 @@ def test_validate_monotonic_increase():
     x_strict_monotonic = np.array([1, 2, 3, 4, 5])
     x_monotonic = np.array([1, 2, 2, 4, 5])
     x_not_increasing = np.array([1, 2, 3, 2, 1])
-    msg_not_strict = "Vector is not strictly monotonically increasing."
     msg_not_increasing = "Vector is not monotonically increasing."
 
     validate_monotonic_increase(x_strict_monotonic, strict_flag=True)
     validate_monotonic_increase(x_monotonic, strict_flag=False)
 
-    with pytest.raises(ValueError, match=msg_not_strict):
+    with pytest.raises(ValueError, match=msg_not_increasing):
         validate_monotonic_increase(x_monotonic, strict_flag=True)
 
     with pytest.raises(ValueError, match=msg_not_increasing):
