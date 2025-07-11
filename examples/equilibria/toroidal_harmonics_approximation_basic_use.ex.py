6f1b17c33224cc63eac2a070a7e628ce28a9c22d
clmould <86794332+clmould@users.noreply.github.com>
2025-07-11 12:59:31 +0100

    Toroidal harmonics constraints (#3867)
    * working on pr comments
    
    * changes based on pr review comments
    
    - added tests for legendre functions
    - improved explanations
    
    * changes based on review comments
    
    * Update bluemira/equilibria/optimisation/harmonics/toroidal_harmonics_approx_functions.py
    
    Co-authored-by: geograham <g.a.graham09@gmail.com>
    
    * Update bluemira/equilibria/optimisation/harmonics/toroidal_harmonics_approx_functions.py
    
    Co-authored-by: geograham <g.a.graham09@gmail.com>
    
    * Single coil and full coilset TH notebooks
    
    * Add TH constraint function
    Add/update necessary TH approx functions
    Update TH full coilset notebook to use new functions
    
    * TH work end of day 18th Feb
    
    - add dataclass ToroidalHarmonicsParams
    - added notebook TH approx basic use (have errors so need to investigate)
    
    * tidying up docstrings
    
    * fixing calls to fns with removed args
    
    * single null keeping legs the same works, committing before trying to figure out new legs that work in optimisation
    
    * changes from georgie on teams
    
    * add name back in after accidentally deleting
    
    * committing before pulling eq analysis branch
    
    - added notebooks for toroidal harmonic basic use, and double circle example
    - few changes in other notebooks
    - fixme in base.py
    
    * changes from before conference
    
    * todo comments to add ToroidalHarmonicConstraintFunction and SphericalHarmonicConstraintFunction
    
    * pr review comments, sentence highlighting zero difference in the core, add sentence explaining fit metric, note about slightly higher fit metric tolerance (copied from changes made on clair/toroidal-approx-nb-single-wire-nb)
    
    * add back SphericalHarmonicConstraintFunction, rename HarmonicConstraintFunction to ToroidalHarmonicConstraintFunction, and fix references to both
    
    * add toroidal harmonic functions to harmonics __init__.py, remove trial notebook
    
    * update return types in docstring for toroidal_harmonic_approximation fn
    
    * Add in relevant equations and benefits of using TH as constraints to the full coilset approximation notebook, and fix legend location to make it more visible
    
    * Tidy up duplicate working notebooks, improve title on a graph to make it clearer
    
    * rename psi_approx to approx_coilset_psi
    
    * tidying up toroidal_harmonics_approximation_basic_use.ex.py notebook
    
    * adding th regression tests
    
    * add test_ToroidalHarmonicConstraint function
    
    * add test_ToroidalHarmonicConstraintFunction
    
    * fix docstring
    
    * fix plotting in ToroidalHarmonicConstraint
    
    * remove unnecessary fixme
    
    * fix todo message to reference issue
    
    * fix linting issues
    
    * add test for df_constraint for TH
    
    * add explanation of tau_max and d2_min
    
    * remove masking
    
    * use effective_centre for focus point, remove remaining interpolation, change graphs to subplots in component function walkthrough example
    
    * Changes to degree number and threshold
    
    * remove invert parameter from harmonics_constraints, and add in inequality constraints to the basic_use example notebook
    
    * remove unused plot arg from call to picarditerator
    
    * use diagnostic_plotting options in picarditerator
    
    * add algorithm variable to pick between slsqp and cobyla
    
    * fix references in examples.rst
    
    * remove invert parameter from toroidal harmonics tests
    
    * remove unnecessary text in basic_use example
    
    * remove coilset from TikhonovCurrentCOP call
    
    * changes to make sonarcloud ci check pass
    
    * use np.einsum to make summation clearer for approximate coilset psi
    
    * fix dtype=o error
    
    * move deepcopy outside of loop
    
    ---------
    
    Co-authored-by: geograham <g.a.graham09@gmail.com>

---
+++ examples/equilibria/toroidal_harmonics_approximation_basic_use.ex.py
@@ -0,0 +1,421 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: tags,title,-all
+#     notebook_metadata_filter: -jupytext.text_representation.jupytext_version
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% tags=["remove-cell"]
+# SPDX-FileCopyrightText: 2021-present M. Coleman, J. Cook, F. Franza
+# SPDX-FileCopyrightText: 2021-present I.A. Maione, S. McIntosh
+# SPDX-FileCopyrightText: 2021-present J. Morris, D. Short
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+"""
+Usage of the 'toroidal_harmonic_approximation' function.
+"""
+
+# %% [markdown]
+# # toroidal_harmonic_approximation Function
+#
+# This example illustrates the input and output of the
+# bluemira toroidal harmonics approximation function
+# (toroidal_harmonic_approximation) which can be used
+# in coilset current and position optimisation for conventional aspect ratio tokamaks.
+
+# %%
+from copy import deepcopy
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from bluemira.base.file import get_bluemira_path
+from bluemira.equilibria.diagnostics import PicardDiagnostic, PicardDiagnosticOptions
+from bluemira.equilibria.equilibrium import Equilibrium
+from bluemira.equilibria.optimisation.constraints import (
+    FieldNullConstraint,
+    IsofluxConstraint,
+    MagneticConstraintSet,
+)
+from bluemira.equilibria.optimisation.harmonics.harmonics_approx_functions import (
+    fs_fit_metric,
+)
+from bluemira.equilibria.optimisation.harmonics.harmonics_constraints import (
+    ToroidalHarmonicConstraint,
+)
+from bluemira.equilibria.optimisation.harmonics.toroidal_harmonics_approx_functions import (  # noqa: E501
+    toroidal_harmonic_approximation,
+)
+from bluemira.equilibria.optimisation.problem._tikhonov import (
+    TikhonovCurrentCOP,  # noqa: PLC2701
+)
+from bluemira.equilibria.plotting import PLOT_DEFAULTS
+from bluemira.equilibria.solve import (
+    DudsonConvergence,
+    PicardIterator,
+)
+
+# %%
+# Data from EQDSK file
+EQDATA = get_bluemira_path("equilibria/test_data", subfolder="tests")
+
+eq_name = "DN-DEMO_eqref.json"
+eq_name = Path(EQDATA, eq_name)
+eq = Equilibrium.from_eqdsk(
+    eq_name, from_cocos=3, qpsi_positive=False, force_symmetry=True
+)
+
+
+# %%
+# Plot equilibrium
+f, ax = plt.subplots()
+eq.plot(ax=ax)
+eq.coilset.plot(ax=ax)
+plt.show()
+
+
+# %% [markdown]
+# ## Inputs
+#
+# ### Required
+#
+# - eq = Our chosen bluemira equilibrium
+#
+# ### Optional
+#
+# - th_params: dataclass containing necessary parameters for use in TH approximation.
+#   'None' will default to using the toroidal_grid_and_coil_setup function with the
+#   input equilibrium to generate the th_params
+# - acceptable_fit_metric: how 'good' we require the approximation to be
+# - psi_norm: 'None' will default to LCFS, otherwise choose the desired
+#   normalised psi value of a closed flux surface that contains the core plasma
+# - plot: Whether or not to plot the results
+
+# %%
+# Information needed for TH Approximation
+# The acceptable fit metric value used here forces the approximation to use 10 degrees
+psi_norm = 0.95
+th_params, Am_cos, Am_sin, degree, fit_metric, approx_total_psi, approx_coilset_psi = (
+    toroidal_harmonic_approximation(eq=eq, psi_norm=psi_norm, acceptable_fit_metric=0.01)
+)
+
+# %% [markdown]
+# ## Outputs
+#
+# ### Results for use in optimisation
+# - th_params: dataclass containing necessary parameters for use in TH approximation
+# - Am_cos: TH cos amplitudes for required number of degrees
+# - Am_sin: TH sin amplitudes for required number of degrees
+#
+# ### Informative outputs
+#
+# - degree: number of degrees required for a TH approx with the desired fit metric
+# - fit_metric_value: fit metric achieved
+# - approx_total_psi: the total psi obtained using the TH approximation
+# - approx_coilset_psi: the coilset psi obtained using the TH approximation
+
+# %%
+# Print the outputs from the toroidal_harmonic_approximation function
+print(f"Coils used in TH approximation: \n {th_params.th_coil_names}")
+# %%
+print(f"TH cos coefficients/amplitudes for required number of degrees: \n{Am_cos}")
+print(f"TH sin coefficients/amplitudes for required number of degrees: \n{Am_sin}")
+# %%
+print(
+    f"Number of degrees required for a TH approx with the desired fit metric: {degree}"
+)
+print(f"Fit metric achieved: {fit_metric}")
+
+# %% [markdown]
+# ## Use in Optimisation Problem
+#
+# Now we will use the approximation to set up constraints for an optimisation problem.
+# We use Tikhonov coilset optimisation problem with TH as constraints.
+# This will try to minimise the sum of the currents squared while constraining the coil
+# contribution to the core psi.
+# %%
+# Use results of the toroidal harmonic approximation to create a set of coil constraints
+th_constraint_equal = ToroidalHarmonicConstraint(
+    ref_harmonics_cos=Am_cos,
+    ref_harmonics_sin=Am_sin,
+    th_params=th_params,
+    tolerance=None,
+    constraint_type="equality",
+)
+
+th_constraint = ToroidalHarmonicConstraint(
+    ref_harmonics_cos=Am_cos,
+    ref_harmonics_sin=Am_sin,
+    th_params=th_params,
+    tolerance=None,
+    constraint_type="inequality",
+)
+th_constraint_inverted = ToroidalHarmonicConstraint(
+    ref_harmonics_cos=Am_cos,
+    ref_harmonics_sin=Am_sin,
+    th_params=th_params,
+    tolerance=None,
+    constraint_type="inequality",
+)
+
+# Make sure we only optimise with coils outside the sphere containing the core plasma by
+# setting control coils using the list of appropriate coils
+eq.coilset.control = list(th_params.th_coil_names)
+
+# %%
+# Add an x point constraint
+lcfs = eq.get_LCFS()
+x_bdry, z_bdry = lcfs.x, lcfs.z
+xp_idx = np.argmin(z_bdry)
+x_point = FieldNullConstraint(
+    x_bdry[xp_idx],
+    z_bdry[xp_idx],
+    tolerance=1e-1,
+)
+
+# %% [markdown]
+# We are aiming to move the inner leg of the divertor, and we will use isoflux
+# constraints to do so.
+# %%
+# Want to move the inner leg
+# Create isoflux constraints for the target inner leg
+arg_inner = np.argmin(x_bdry)
+
+# Define points to use for the isoflux constraints
+inner_leg_points_x = (
+    np.array([
+        7.0,
+        7.5,
+        8.0,
+    ])
+    - 0.5
+)
+
+inner_leg_points_z = (
+    np.array([
+        6.05,
+        5.75,
+        5.5,
+    ])
+    + 0.2
+)
+
+outer_legs_x_unmoved = np.array([
+    9.7,
+    9.89,
+    10.1,
+])
+
+outer_legs_z_unmoved = np.array([6.5, 7.0, 7.5])
+
+# Create the necessary isoflux constraints
+DN_unmoved_outer_leg_upper = IsofluxConstraint(
+    outer_legs_x_unmoved,
+    outer_legs_z_unmoved,
+    ref_x=x_bdry[arg_inner],
+    ref_z=z_bdry[arg_inner],
+    tolerance=1e-6,
+)
+
+DN_unmoved_outer_leg_lower = IsofluxConstraint(
+    outer_legs_x_unmoved,
+    -outer_legs_z_unmoved,
+    ref_x=x_bdry[arg_inner],
+    ref_z=z_bdry[arg_inner],
+    tolerance=1e-6,
+)
+
+DN_inner_leg_upper = IsofluxConstraint(
+    inner_leg_points_x,
+    inner_leg_points_z,
+    ref_x=x_bdry[arg_inner],
+    ref_z=z_bdry[arg_inner],
+    tolerance=1e-6,
+)
+
+DN_inner_leg_lower = IsofluxConstraint(
+    inner_leg_points_x,
+    -inner_leg_points_z,
+    ref_x=x_bdry[arg_inner],
+    ref_z=z_bdry[arg_inner],
+    tolerance=1e-6,
+)
+
+# Plot the isoflux points and the starting equilibrium
+f, ax = plt.subplots()
+eq.plot(ax=ax)
+DN_inner_leg_upper.plot(ax=ax)
+DN_inner_leg_lower.plot(ax=ax)
+DN_unmoved_outer_leg_upper.plot(ax=ax)
+DN_unmoved_outer_leg_lower.plot(ax=ax)
+plt.show()
+
+
+# %%
+# DOUBLE NULL OPTIMISATION
+
+# Define the constraints to use in our optimisation problem
+# NOTE Added inequality constraints, comment in/out ones to use
+# NOTE If using inequality, need to use SLSQP in the COP, if equality use COBYLA
+# NOTE using slsqp w/ equality constraints means that the coil currents are not changed
+# NOTE using cobyla with inequality constraints makes the plasma drift
+constraints = [
+    th_constraint_equal,
+    # th_constraint,
+    # th_constraint_inverted,
+    x_point,
+    DN_inner_leg_upper,
+    DN_inner_leg_lower,
+]
+# comment out as required:
+# algorithm = "SLSQP"
+algorithm = "COBYLA"
+# %%
+# Make a copy of the equilibria
+th_eq = deepcopy(eq)
+# Set up a coilset optimisation problem using the toroidal harmonic constraints
+th_con_len_opt = TikhonovCurrentCOP(
+    th_eq,
+    targets=MagneticConstraintSet([
+        DN_inner_leg_upper,
+        DN_inner_leg_lower,
+        DN_unmoved_outer_leg_upper,
+        DN_unmoved_outer_leg_lower,
+    ]),
+    gamma=1e-4,
+    opt_algorithm=algorithm,
+    opt_conditions={"max_eval": 1000, "ftol_rel": 1e-4},
+    opt_parameters={"initial_step": 0.1},
+    max_currents=3e10,
+    constraints=constraints,
+)
+# Find the optimised coilset
+_ = th_con_len_opt.optimise()
+
+# Update plasma - one solve
+th_eq.solve()
+
+# %%
+# We should not need to solve the GS equation while optimising if the TH approximation
+# is sufficiently good, but we can have a look at what happens.
+
+# Make a copy of the equilibria
+th_current_opt_eq = deepcopy(eq)
+
+# Set up a coilset optimisation problem using the toroidal harmonic constraint
+current_opt_problem = TikhonovCurrentCOP(
+    th_current_opt_eq,
+    targets=MagneticConstraintSet([
+        DN_inner_leg_upper,
+        DN_inner_leg_lower,
+        DN_unmoved_outer_leg_upper,
+        DN_unmoved_outer_leg_lower,
+    ]),
+    gamma=1e-4,
+    opt_algorithm=algorithm,
+    opt_conditions={"max_eval": 1000, "ftol_rel": 1e-4},
+    opt_parameters={"initial_step": 0.1},
+    max_currents=3e10,
+    constraints=constraints,
+)
+
+diagnostic_plotting = PicardDiagnosticOptions(plot=PicardDiagnostic.EQ)
+# SOLVE
+program = PicardIterator(
+    th_current_opt_eq,
+    current_opt_problem,
+    diagnostic_plotting=diagnostic_plotting,
+    fixed_coils=True,
+    convergence=DudsonConvergence(1e-3),
+    relaxation=0.1,
+    maxiter=30,
+)
+program()
+
+# %%
+# Plot the two approches
+f, (ax_1, ax_2) = plt.subplots(1, 2)
+
+th_eq.plot(ax=ax_1)
+ax_1.set_title("Coils Optimised")
+
+th_current_opt_eq.plot(ax=ax_2)
+ax_2.set_title("Coils Optimised while GS solved")
+plt.show()
+
+# %%
+# Now we want to plot the starting equilibrium and the optimised
+# equilibrium after using our TH constraints. We also show the
+# isoflux points in purple, which are used to alter the shape of the divertor legs.
+
+f, (ax_1, ax_2) = plt.subplots(1, 2)
+
+eq.plot(ax=ax_1)
+DN_inner_leg_upper.plot(ax=ax_1)
+DN_inner_leg_lower.plot(ax=ax_1)
+DN_unmoved_outer_leg_upper.plot(ax=ax_1)
+DN_unmoved_outer_leg_lower.plot(ax=ax_1)
+ax_1.set_title("Starting Equilibrium")
+
+th_current_opt_eq.plot(ax=ax_2)
+DN_inner_leg_upper.plot(ax=ax_2)
+DN_inner_leg_lower.plot(ax=ax_2)
+DN_unmoved_outer_leg_upper.plot(ax=ax_2)
+DN_unmoved_outer_leg_lower.plot(ax=ax_2)
+ax_2.set_title("TH Used to Optimise Leg Shaping")
+plt.show()
+
+
+# %%
+# Now we create a difference plot to show the absolute relative difference in total psi
+# between the starting equilibrium and the optimised equilibrium. We also show the
+# last closed flux surfaces for each equilibrium.
+
+original_FS = (  # noqa: N816
+    eq.get_LCFS() if np.isclose(psi_norm, 1.0) else eq.get_flux_surface(psi_norm)
+)
+approx_FS = th_current_opt_eq.get_flux_surface(psi_norm)  # noqa: N816
+
+total_psi_diff = np.abs(eq.psi() - th_current_opt_eq.psi()) / np.max(
+    np.abs(th_current_opt_eq.psi())
+)
+
+f, ax = plt.subplots()
+nlevels = PLOT_DEFAULTS["psi"]["nlevels"]
+cmap = PLOT_DEFAULTS["psi"]["cmap"]
+ax.plot(
+    approx_FS.x,
+    approx_FS.z,
+    color="red",
+    label="Approximate FS after \noptimising using TH",
+)
+ax.plot(
+    original_FS.x,
+    original_FS.z,
+    color="blue",
+    label="Original equilibrium FS \nfrom Bluemira",
+)
+im = ax.contourf(eq.grid.x, eq.grid.z, total_psi_diff, levels=nlevels, cmap=cmap)
+f.colorbar(mappable=im)
+ax.set_title(
+    "Absolute relative difference between total psi and TH approximation psi", y=1.05
+)
+ax.legend(bbox_to_anchor=(1.1, 1.05))
+eq.coilset.plot(ax=ax)
+plt.show()
+
+
+# %%
+# Compare starting equilibrium and optimised equilibrium using fit metric
+fit_metric = fs_fit_metric(original_FS, approx_FS)
+print(f"fit metric = {fit_metric}")
---
+++ examples/equilibria/toroidal_harmonics_full_coilset_approximation_bluemira_comparison.ex.py
@@ -0,0 +1,302 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: tags,-all
+#     notebook_metadata_filter: -jupytext.text_representation.jupytext_version
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% tags=["remove-cell"]
+# SPDX-FileCopyrightText: 2021-present M. Coleman, J. Cook, F. Franza
+# SPDX-FileCopyrightText: 2021-present I.A. Maione, S. McIntosh
+# SPDX-FileCopyrightText: 2021-present J. Morris, D. Short
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+"""
+Calculate solution due to coilset as sum of toroidal harmonics, and compare to solution
+from Bluemira.
+"""
+# %% [markdown]
+# # Example of using Toroidal Harmonic Approximation
+#
+# This example illustrates the inner workings of the bluemira toroidal harmonics
+# approximation function (toroidal_harmonic_approximation) which can be used in
+# coilset current and position optimisation for conventional aspect ratio tokamaks.
+# For full details and all equations, look in the
+# notebook toroidal_harmonics_component_function_walkthrough_and_verification.ex.py.
+#
+# ## Premise:
+#
+# - Our equilibrium solution will not change if the coilset contribution
+# to the poloidal field (vacuum field) is kept the same in the region
+# occupied by the core plasma (i.e. the region characterised by closed flux surfaces).
+#
+# - If we constrain the core plasma while altering (optimising) other aspects
+#   of the magnetic configuration, then we will not need to re-solve for
+#   the plasma equilibrium at each iteration.
+#
+# - We can decompose the vacuum field into Toroidal Harmonics (TH)
+#   to create a minimal set of constraints for use in optimisation.
+#
+# There are benefits to using TH as a minimal set of constraints:
+# - We can choose not to re-solve for the plasma equilibrium at each step, since the
+# coilset contribution to the core plasma (within the LCFS) is constrained.
+# - We have a minimal set of constraints (a set of harmonic amplitudes) for the core
+# plasma contribution, which can reduce the dimensionality of the problem we are
+# considering.
+#
+# We get the TH amplitudes/coefficients, $A(\tau, \sigma)$, from the following equations:
+#
+# $$ A(\tau, \sigma) = \sum_{m=0}^{\infty} A_m^{\cos} \epsilon_m m! \sqrt{\frac{2}{\pi}}
+# \Delta^{\frac{1}{2}} \textbf{Q}_{m-\frac{1}{2}}^{1}(\cosh \tau) \cos(m \sigma) + A_m^
+# {\sin}
+# \epsilon_m m! \sqrt{\frac{2}{\pi}} \Delta^{\frac{1}{2}}
+# \textbf{Q}_{m-\frac{1}{2}}^{1}(\cosh \tau) \sin(m \sigma) $$
+#
+# where
+#
+# $$ A_m^{\cos, \sin} = \frac{\mu_0 I_c}{2^{\frac{5}{2}}} factorial\_term \frac{\sinh(
+# \tau_c)}
+# {\Delta_c^{\frac{1}{2}}} P_{m - \frac{1}{2}}^{-1}(\cosh(\tau_c)) ^{\cos}_{\sin}(m
+# \sigma_c) $$
+#
+# where
+# - $A_m^{\cos, \sin}$ are coefficients for a single coil
+# - subscript $c$ refers to a single coil
+# - $I_c, \tau_c, \sigma_c$ are the coil current, and coil position in toroidal
+# coordinates $(\tau, \sigma)$
+# - $m$ is the poloidal mode number
+# - $P_{\nu}^{\mu}$ is the associated Legendre function of the first kind of degree $\nu$
+#  and order $\mu$
+# - $\textbf{Q}_{\nu}^{\mu}$ is Olver's definition of the associated Legendre function of
+# the second kind. See [here](https://dlmf.nist.gov/14) or F. W. J. Olver (1997b)
+# Asymptotics and Special Functions. A. K. Peters, Wellesley, MA. for more information.
+# - $\varepsilon_m = 1 $ for $m = 0$ and $\varepsilon_m = 2$ for $m \ge 1$
+# - $ \Delta = \cosh(\tau) - \cos(\sigma) $
+# - $ \Delta_c = \cosh(\tau_c) - \cos(\sigma_c) $
+# - $ factorial\_term = \prod_{i=0}^{m-1} \left( 1 + \frac{1}{2(m-i)}\right) $
+#
+# %% [markdown]
+# ## Imports
+
+# %%
+from copy import deepcopy
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from bluemira.base.file import get_bluemira_path
+from bluemira.equilibria.equilibrium import Equilibrium
+from bluemira.equilibria.find import find_flux_surf
+from bluemira.equilibria.optimisation.harmonics.harmonics_approx_functions import (
+    fs_fit_metric,
+)
+from bluemira.equilibria.optimisation.harmonics.toroidal_harmonics_approx_functions import (  # noqa: E501
+    toroidal_harmonic_approximate_psi,
+    toroidal_harmonic_approximation,
+    toroidal_harmonic_grid_and_coil_setup,
+)
+from bluemira.equilibria.plotting import PLOT_DEFAULTS
+from bluemira.geometry.coordinates import Coordinates
+
+# %% [markdown]
+# Get equilibrium from EQDSK file and plot
+
+# %%
+# Get equilibrium
+EQDATA = get_bluemira_path("equilibria/test_data", subfolder="tests")
+
+# eq_name = "eqref_OOB.json"
+# eq_name = Path(EQDATA, eq_name)
+# eq = Equilibrium.from_eqdsk(eq_name, from_cocos=7)
+
+eq_name = "DN-DEMO_eqref.json"
+eq_name = Path(EQDATA, eq_name)
+eq = Equilibrium.from_eqdsk(eq_name, from_cocos=3, qpsi_positive=False)
+
+# Plot the equilibrium
+f, ax = plt.subplots()
+eq.plot(ax=ax)
+eq.coilset.plot(ax=ax)
+plt.show()
+
+# %% [markdown]
+# Set the focus point
+#
+# Note: there is the possibility to experiment with the position of the focus, but
+# the default is to use the plasma o point.
+# %%
+# Set the focus point
+R_0, Z_0 = eq.effective_centre()
+
+# %% [markdown]
+# Use the `toroidal_harmonic_grid_and_coil_setup` to obtain the necessary
+# parameters required for the TH approximation, such as the R and Z coordinates of the
+# TH grid.
+#
+# We then use the `toroidal_harmonic_approximate_psi` function to approximate the coilset
+# contribution to the flux using toroidal harmonics. We need to provide this function
+# with the equilibrium, eq, and the ToroidalHarmonicsParams dataclass, which contains
+# necessary parameters for the TH approximation, such as the relevant coordinates
+# and coil names for use in the approximation. The
+# function returns the approx_coilset_psi array and the TH coefficient matrix A_m.
+# The default focus point is the plasma o point.
+# The white dot in the plot shows the focus point.
+# %%
+# Approximate psi and plot
+th_params = toroidal_harmonic_grid_and_coil_setup(eq=eq, R_0=R_0, Z_0=Z_0)
+
+R_approx = th_params.R
+Z_approx = th_params.Z
+
+approx_coilset_psi, _, _ = toroidal_harmonic_approximate_psi(
+    eq=eq, th_params=th_params, max_degree=5
+)
+
+nlevels = PLOT_DEFAULTS["psi"]["nlevels"]
+cmap = PLOT_DEFAULTS["psi"]["cmap"]
+plt.contourf(R_approx, Z_approx, approx_coilset_psi, nlevels, cmap=cmap)
+plt.xlabel("R")
+plt.ylabel("Z")
+plt.title("TH Approximation for Coilset Psi")
+plt.show()
+
+
+# %% [markdown]
+# Now we want to compare this approximation to the solution from bluemira.
+
+# %%
+# Plot total psi using approximate coilset psi from TH, and plasma psi from bluemira
+
+plasma_psi = eq.plasma.psi(R_approx, Z_approx)
+
+total_psi = approx_coilset_psi + plasma_psi
+
+# Find LCFS from TH approx
+approx_eq = deepcopy(eq)
+o_points, x_points = approx_eq.get_OX_points(total_psi)
+
+# The fit metric is a measure of how 'good' the approximation is.
+# Fit metric value = total area within one but not both FSs /
+#                    (input FS area + approximation FS area)
+psi_norm = 0.95
+f_s = find_flux_surf(
+    R_approx, Z_approx, total_psi, psi_norm, o_points=o_points, x_points=x_points
+)
+approx_fs = Coordinates({"x": f_s[0], "z": f_s[1]})
+original_fs = (
+    eq.get_LCFS() if np.isclose(psi_norm, 1.0) else eq.get_flux_surface(psi_norm)
+)
+
+# Plot
+plt.contourf(R_approx, Z_approx, total_psi, nlevels, cmap=cmap)
+plt.xlabel("R")
+plt.ylabel("Z")
+plt.plot(
+    approx_fs.x,
+    approx_fs.z,
+    color="red",
+    label=f"Closed Flux Surface (psi_n={psi_norm}) from TH approximation",
+)
+plt.title("Total Psi using TH approximation for coilset psi")
+plt.legend(loc="upper right")
+plt.show()
+
+# %%
+# Plot bluemira total psi
+# Obtain psi from Bluemira coilset
+bm_coil_psi = np.zeros(np.shape(eq.grid.x))
+for n in eq.coilset.name:
+    bm_coil_psi = np.sum([bm_coil_psi, eq.coilset[n].psi(eq.grid.x, eq.grid.z)], axis=0)
+
+
+# Obtain total psi from Bluemira
+bluemira_total_psi = eq.psi(R_approx, Z_approx)
+
+# Plotting
+plt.contourf(R_approx, Z_approx, bluemira_total_psi, levels=nlevels, cmap=cmap)
+plt.xlabel("R")
+plt.ylabel("Z")
+plt.title("Total Bluemira Psi")
+plt.show()
+# %%
+# Plot bluemira coilset psi
+coilset_psi = eq.coilset.psi(R_approx, Z_approx)
+
+# Plotting
+plt.contourf(R_approx, Z_approx, coilset_psi, nlevels, cmap=cmap)
+plt.xlabel("R")
+plt.ylabel("Z")
+plt.title("Bluemira Coilset Psi")
+plt.show()
+# %%
+# Difference plot to compare TH approximation to Bluemira coilset psi
+# We see zero difference in the core region, which we expect as we are constraining
+# the flux in this region, and we see larger differences outside of the approximation
+# region.
+coilset_psi_diff = np.abs(approx_coilset_psi - coilset_psi) / np.max(np.abs(coilset_psi))
+coilset_psi_diff_plot = coilset_psi_diff
+f, ax = plt.subplots()
+ax.plot(approx_fs.x, approx_fs.z, color="red", label="Approximate LCFS from TH")
+ax.plot(original_fs.x, original_fs.z, color="blue", label="LCFS from Bluemira")
+im = ax.contourf(R_approx, Z_approx, coilset_psi_diff_plot, levels=nlevels, cmap=cmap)
+f.colorbar(mappable=im)
+ax.set_title("Absolute relative difference between coilset psi and TH approximation psi")
+ax.legend(loc="upper right", bbox_to_anchor=(1.1, 1.0))
+eq.coilset.plot(ax=ax)
+plt.show()
+
+
+# %%
+# Difference plot to compare TH approximation to Bluemira total psi
+total_psi_diff = np.abs(total_psi - bluemira_total_psi) / np.max(
+    np.abs(bluemira_total_psi)
+)
+total_psi_diff_plot = total_psi_diff
+f, ax = plt.subplots()
+ax.plot(approx_fs.x, approx_fs.z, color="red", label="Approx FS from TH")
+ax.plot(original_fs.x, original_fs.z, color="blue", label="FS from Bluemira")
+im = ax.contourf(R_approx, Z_approx, total_psi_diff_plot, levels=nlevels, cmap=cmap)
+f.colorbar(mappable=im)
+ax.set_title("Absolute relative difference between total psi and TH approximation psi")
+ax.legend(loc="upper right")
+eq.coilset.plot(ax=ax)
+plt.show()
+
+# %% [markdown]
+# We use a fit metric to evaluate the TH approximation.
+
+# %%
+# Fit metric to evaluate TH approximation
+fit_metric_value = fs_fit_metric(original_fs, approx_fs)
+print(f"fit metric value = {fit_metric_value}")
+
+# %% [markdown]
+# All of these TH functions are combined in the `toroidal_harmonic_approximation`
+# function, which takes the equilibrium, eq, and the TH parameters, th_params, and
+# approximates psi using TH. The function uses a fit metric to find the appropriate
+# number of degrees to use for the approximation.
+#
+# Here is an example of using the function, setting plot to True outputs a graph of the
+# difference in total psi between the TH approximation and bluemira.
+# %%
+(
+    toroidal_harmonics_params,
+    Am_cos,
+    Am_sin,
+    degree,
+    fit_metric,
+    approx_total_psi,
+    approx_coilset_psi,
+) = toroidal_harmonic_approximation(
+    eq=eq, th_params=th_params, plot=True, psi_norm=psi_norm
+)
