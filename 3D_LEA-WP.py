08d3731ce1dc03d0d7a247e27361752ee0eaf30f
gvverma <gaurav91verma@gmail.com>
2025-07-29 16:35:00 +0200

    structural analysis of a 3D for dolfinx 0.9 version weak formulation

--- 3D_LEA-WP.py
+++ 3D_LEA-WP.py
@@ -16,28 +16,14 @@ from dolfinx.io import XDMFFile
 from ufl import (FacetNormal, sqrt, as_matrix, Identity, Measure, TestFunction, tr, TrialFunction, as_vector, div, dot, ds, dx, inner, lhs, grad, nabla_grad, nabla_div, rhs, sym)
 import pyvista
 
-# Mesh
-mesh_path = "3d-bar.msh"
-domain, cell_markers, facet_markers = gmshio.read_from_msh(mesh_path, MPI.COMM_WORLD, gdim=3)
+L = 1
+W = 0.2
+delta = W / L
+gamma = 0.4 * delta**2
+beta = 1.25
+g = gamma
 
-pyvista.set_jupyter_backend("static")
-
-topology, cell_types, geometry = plot.vtk_mesh(domain, 3)
-grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
-
-p = pyvista.Plotter()
-p.add_mesh(grid,show_edges=True)
-p.view_xy()
-p.show_axes()
-p.show()
-
-from dolfinx.plot import vtk_mesh
-from dolfinx.io import XDMFFile
-
-with XDMFFile(MPI.COMM_WORLD, "solid_bar.xdmf", "w") as xdmf:
-    xdmf.write_mesh(domain)
-    xdmf.write_meshtags(cell_markers, domain.geometry)
-
+domain = mesh.create_box(MPI.COMM_WORLD, [np.array([0, 0, 0]), np.array([L, W, W])],[20, 6, 6], cell_type=mesh.CellType.hexahedron)
 
 
 E = fem.Constant(domain, 200000000000.0)
@@ -55,17 +41,19 @@ V = fem.functionspace(domain, ("Lagrange", 1, (domain.geometry.dim, )))
 
 
 def clamped_boundary(x):
-    return np.isclose(x[2], 0)
+    return np.isclose(x[0], 0)
 
 
-u_zero = np.array((0,) * domain.geometry.dim, dtype=default_scalar_type)
-bc = fem.dirichletbc(u_zero, fem.locate_dofs_geometrical(V, clamped_boundary), V)
+fdim = domain.topology.dim - 1
+boundary_facets = mesh.locate_entities_boundary(domain, fdim, clamped_boundary)
+
+u_D = np.array([0, 0, 0], dtype=default_scalar_type)
+bc = fem.dirichletbc(u_D, fem.locate_dofs_topological(V, fdim, boundary_facets), V)
 bcs = [bc]
 
-
-dx = Measure("dx", domain=domain, subdomain_data=cell_markers)
-dS = Measure("dS", domain=domain, subdomain_data=facet_markers)
-ds = Measure("ds", domain=domain, subdomain_data=facet_markers)
+dx = Measure("dx", domain=domain)
+#dS = Measure("dS", domain=domain, subdomain_data=facet_markers)
+ds = Measure("ds", domain=domain)
 
 
 T = fem.Constant(domain, default_scalar_type((0, 0, 0)))
@@ -80,9 +68,9 @@ def sigma(u):
 
 u = TrialFunction(V)
 v = TestFunction(V)
-f = fem.Constant(domain, default_scalar_type((0, -rho * g, 0)))
+f = fem.Constant(domain, default_scalar_type((0, 0, -rho * g)))
 a = inner(sigma(u), epsilon(v)) * dx
-L = dot(f, v) * dx
+L = dot(f, v) * dx + dot(T, v) * ds
 
 
 problem = LinearProblem(a, L, bcs, petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
@@ -99,18 +87,14 @@ s = sigma(uh) - 1. / 3 * tr(sigma(uh)) * Identity(len(uh))
 von_Mises = sqrt(3. / 2 * inner(s, s))
 
 V_von_mises = fem.functionspace(domain, ("DG", 0))
-element = V_von_mises.element
-stress_expr = fem.Expression(von_Mises, element.interpolation_points())
-#stress_expr = fem.Expression(von_Mises, V_von_mises.element.interpolation_points())
+stress_expr = fem.Expression(von_Mises, V_von_mises.element.interpolation_points())
 stresses = fem.Function(V_von_mises)
 stresses.interpolate(stress_expr)
 
 
-
-with io.XDMFFile(domain.comm, "fry2.xdmf", "w") as xdmf:
+with io.XDMFFile(domain.comm, "fry3.xdmf", "w") as xdmf:
     xdmf.write_mesh(domain)
-    uh.name = "Displacement"
+    uh.name = "Deformation"
     xdmf.write_function(uh)
     stresses.name = "Von_Mises"
     xdmf.write_function(stresses)
-
