2b50a57f5455e09a5fb7cdaaca5fc6da712a10bd
clmould <86794332+clmould@users.noreply.github.com>
2025-08-11 15:35:24 +0100

    Tidying up toroidal harmonics notebooks on develop for release (#4039)
    * Tidying up toroidal harmonics notebooks on develop for release
    
    * fix test failures
    
    * Changes based on review comments

--- examples/equilibria/Toroidal_Harmonics_Optimisation_Set_Up.ex.py
+++ examples/equilibria/Toroidal_Harmonics_Optimisation_Set_Up.ex.py
@@ -25,12 +25,18 @@ Usage of the 'toroidal_harmonic_approximation' function.
 """
 
 # %% [markdown]
-# # toroidal_harmonic_approximation Function
+# # Example of using Toroidal Harmonic Constraints in a Coil Current Optimisation
 #
-# This example illustrates the input and output of the
-# bluemira toroidal harmonics approximation function
-# (toroidal_harmonic_approximation) which can be used
-# in coilset current and position optimisation for conventional aspect ratio tokamaks.
+# This example illustrates the usage of the bluemira
+# toroidal_harmonics_approximation function to create Toroidal
+# Harmonic (TH) constraints to be used in a coil current optimisation
+# problem for a double null DEMO-like tokamak.
+#
+# For more details about the inner workings of this function,
+# see Toroidal_Approximation_Explained.ex.py.
+
+# %% [markdown]
+# ## Imports
 
 # %%
 from copy import deepcopy
@@ -67,6 +73,7 @@ from bluemira.equilibria.solve import (
 
 # %%
 # Data from EQDSK file
+# Using a double null DEMO-like equilibria here
 EQDATA = get_bluemira_path("equilibria/test_data", subfolder="tests")
 
 eq_name = "DN-DEMO_eqref.json"
@@ -75,14 +82,11 @@ eq = Equilibrium.from_eqdsk(
     eq_name, from_cocos=3, qpsi_positive=False, force_symmetry=True
 )
 
-
-# %%
 # Plot equilibrium
 f, ax = plt.subplots()
 eq.plot(ax=ax)
 eq.coilset.plot(ax=ax)
 plt.show()
-
 
 # %% [markdown]
 # ## Inputs
@@ -103,7 +107,6 @@ plt.show()
 
 # %%
 # Information needed for TH Approximation
-# The acceptable fit metric value used here forces the approximation to use 10 degrees
 psi_norm = 0.95
 th_params, Am_cos, Am_sin, degree, fit_metric, approx_total_psi, approx_coilset_psi = (
     toroidal_harmonic_approximation(eq=eq, psi_norm=psi_norm, acceptable_fit_metric=0.01)
@@ -127,45 +130,38 @@ th_params, Am_cos, Am_sin, degree, fit_metric, approx_total_psi, approx_coilset_
 # %%
 # Print the outputs from the toroidal_harmonic_approximation function
 print(f"Coils used in TH approximation: \n {th_params.th_coil_names}")
-# %%
-print(f"TH cos coefficients/amplitudes for required number of degrees: \n{Am_cos}")
-print(f"TH sin coefficients/amplitudes for required number of degrees: \n{Am_sin}")
+
 # %%
 print(
     f"Number of degrees required for a TH approx with the desired fit metric: {degree}"
 )
+# %%
+print(f"TH cos coefficients/amplitudes for {degree} degrees: \n{Am_cos}")
+print(f"TH sin coefficients/amplitudes for {degree} degrees: \n{Am_sin}")
+# %%
 print(f"Fit metric achieved: {fit_metric}")
 
 # %% [markdown]
 # ## Use in Optimisation Problem
 #
 # Now we will use the approximation to set up constraints for an optimisation problem.
-# We use Tikhonov coilset optimisation problem with TH as constraints.
+# We use a Tikhonov coilset optimisation problem with TH as constraints.
 # This will try to minimise the sum of the currents squared while constraining the coil
 # contribution to the core psi.
+# Note: there is a limit to how many equality constraints you can use, so if you
+# encounter any associated errors then try setting constraint_type="inequality".
+# This will create 2 inequality constraints that are equivalent to the equality
+# constraint.
+#
+# In this example we use an equality constraint.
 # %%
 # Use results of the toroidal harmonic approximation to create a set of coil constraints
-th_constraint_equal = ToroidalHarmonicConstraint(
-    ref_harmonics_cos=Am_cos,
-    ref_harmonics_sin=Am_sin,
-    th_params=th_params,
-    tolerance=None,
-    constraint_type="equality",
-)
-
 th_constraint = ToroidalHarmonicConstraint(
     ref_harmonics_cos=Am_cos,
     ref_harmonics_sin=Am_sin,
     th_params=th_params,
-    tolerance=None,
-    constraint_type="inequality",
-)
-th_constraint_inverted = ToroidalHarmonicConstraint(
-    ref_harmonics_cos=Am_cos,
-    ref_harmonics_sin=Am_sin,
-    th_params=th_params,
-    tolerance=None,
-    constraint_type="inequality",
+    tolerance=1e-3,
+    constraint_type="equality",
 )
 
 # Make sure we only optimise with coils outside the sphere containing the core plasma by
@@ -185,30 +181,25 @@ x_point = FieldNullConstraint(
 
 # %% [markdown]
 # We are aiming to move the inner leg of the divertor, and we will use isoflux
-# constraints to do so.
+# constraints to do so. Here we set up these isoflux constraints and
+# show them plotted on our equilibria.
 # %%
 # Want to move the inner leg
 # Create isoflux constraints for the target inner leg
 arg_inner = np.argmin(x_bdry)
 
 # Define points to use for the isoflux constraints
-inner_leg_points_x = (
-    np.array([
-        7.0,
-        7.5,
-        8.0,
-    ])
-    - 0.5
-)
+inner_leg_points_x = np.array([
+    6.5,
+    7.0,
+    7.5,
+])
 
-inner_leg_points_z = (
-    np.array([
-        6.05,
-        5.75,
-        5.5,
-    ])
-    + 0.2
-)
+inner_leg_points_z = np.array([
+    6.25,
+    5.95,
+    5.7,
+])
 
 outer_legs_x_unmoved = np.array([
     9.7,
@@ -218,13 +209,14 @@ outer_legs_x_unmoved = np.array([
 
 outer_legs_z_unmoved = np.array([6.5, 7.0, 7.5])
 
-# Create the necessary isoflux constraints
+# Create the necessary isoflux constraints for the inner and outer legs, for
+# the upper and lower divertors.
 DN_unmoved_outer_leg_upper = IsofluxConstraint(
     outer_legs_x_unmoved,
     outer_legs_z_unmoved,
     ref_x=x_bdry[arg_inner],
     ref_z=z_bdry[arg_inner],
-    tolerance=1e-6,
+    tolerance=1e-3,
 )
 
 DN_unmoved_outer_leg_lower = IsofluxConstraint(
@@ -232,7 +224,7 @@ DN_unmoved_outer_leg_lower = IsofluxConstraint(
     -outer_legs_z_unmoved,
     ref_x=x_bdry[arg_inner],
     ref_z=z_bdry[arg_inner],
-    tolerance=1e-6,
+    tolerance=1e-3,
 )
 
 DN_inner_leg_upper = IsofluxConstraint(
@@ -240,7 +232,7 @@ DN_inner_leg_upper = IsofluxConstraint(
     inner_leg_points_z,
     ref_x=x_bdry[arg_inner],
     ref_z=z_bdry[arg_inner],
-    tolerance=1e-6,
+    tolerance=1e-3,
 )
 
 DN_inner_leg_lower = IsofluxConstraint(
@@ -248,10 +240,10 @@ DN_inner_leg_lower = IsofluxConstraint(
     -inner_leg_points_z,
     ref_x=x_bdry[arg_inner],
     ref_z=z_bdry[arg_inner],
-    tolerance=1e-6,
+    tolerance=1e-3,
 )
 
-# Plot the isoflux points and the starting equilibrium
+# Plot the isoflux points and the starting equilibrium for reference
 f, ax = plt.subplots()
 eq.plot(ax=ax)
 DN_inner_leg_upper.plot(ax=ax)
@@ -260,25 +252,22 @@ DN_unmoved_outer_leg_upper.plot(ax=ax)
 DN_unmoved_outer_leg_lower.plot(ax=ax)
 plt.show()
 
+# %% [markdown]
+# Now we perform the Tikhonov coilset optimisation using our toroidal
+# harmonic and isoflux leg constraints.
+# Note that the constraints are violated in this example.
 
 # %%
-# DOUBLE NULL OPTIMISATION
+# OPTIMISATION
 
 # Define the constraints to use in our optimisation problem
-# NOTE Added inequality constraints, comment in/out ones to use
-# NOTE If using inequality, need to use SLSQP in the COP, if equality use COBYLA
-# NOTE using slsqp w/ equality constraints means that the coil currents are not changed
-# NOTE using cobyla with inequality constraints makes the plasma drift
 constraints = [
-    th_constraint_equal,
-    # th_constraint,
-    # th_constraint_inverted,
+    th_constraint,
     x_point,
     DN_inner_leg_upper,
     DN_inner_leg_lower,
 ]
-# comment out as required:
-# algorithm = "SLSQP"
+
 algorithm = "COBYLA"
 # %%
 # Make a copy of the equilibria
@@ -305,10 +294,11 @@ _ = th_con_len_opt.optimise()
 # Update plasma - one solve
 th_eq.solve()
 
-# %%
+# %% [markdown]
 # We should not need to solve the GS equation while optimising if the TH approximation
 # is sufficiently good, but we can have a look at what happens.
 
+# %%
 # Make a copy of the equilibria
 th_current_opt_eq = deepcopy(eq)
 
@@ -353,11 +343,12 @@ th_current_opt_eq.plot(ax=ax_2)
 ax_2.set_title("Coils Optimised while GS solved")
 plt.show()
 
-# %%
+# %% [markdown]
 # Now we want to plot the starting equilibrium and the optimised
 # equilibrium after using our TH constraints. We also show the
 # isoflux points in purple, which are used to alter the shape of the divertor legs.
 
+# %%
 f, (ax_1, ax_2) = plt.subplots(1, 2)
 
 eq.plot(ax=ax_1)
@@ -376,11 +367,13 @@ ax_2.set_title("TH Used to Optimise Leg Shaping")
 plt.show()
 
 
-# %%
+# %% [markdown]
 # Now we create a difference plot to show the absolute relative difference in total psi
 # between the starting equilibrium and the optimised equilibrium. We also show the
-# last closed flux surfaces for each equilibrium.
+# flux surface for each equilibrium for the psi_norm chosen - in this example it is
+# set to 0.95.
 
+# %%
 original_FS = (  # noqa: N816
     eq.get_LCFS() if np.isclose(psi_norm, 1.0) else eq.get_flux_surface(psi_norm)
 )
@@ -415,7 +408,14 @@ eq.coilset.plot(ax=ax)
 plt.show()
 
 
+# %% [markdown]
+# Compare starting equilibrium and optimised equilibrium using fit metric to see how
+# "good" our approximation is.
+# Fit metric value = total area within one but not both FSs /
+#                    (input FS area + approximation FS area)
+# A value of 0 would correspond to no differences, a value
+# of 1 would correspond to no overlap between the flux surfaces.
+
 # %%
-# Compare starting equilibrium and optimised equilibrium using fit metric
 fit_metric = fs_fit_metric(original_FS, approx_FS)
 print(f"fit metric = {fit_metric}")
