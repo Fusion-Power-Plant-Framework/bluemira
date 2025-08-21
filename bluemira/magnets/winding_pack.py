003e780ecfdb87f285893ffd56993ff6a24c0464
Jonathan Matthews <jonathan.matthews@ukaea.uk>
2025-08-21 13:43:48 +0100

    addition of ParaterFrame dataclasses for classes in winding_pack.py

--- bluemira/magnets/winding_pack.py
+++ bluemira/magnets/winding_pack.py
@@ -6,17 +6,32 @@
 
 """Winding pack module"""
 
+from dataclasses import dataclass
 from typing import Any, ClassVar
 
 import matplotlib.pyplot as plt
 import numpy as np
 from matproplib import OperationalConditions
 
+from bluemira.base.parameter_frame import Parameter, ParameterFrame
+from bluemira.base.parameter_frame.typed import ParameterFrameLike
 from bluemira.magnets.conductor import Conductor, create_conductor_from_dict
 from bluemira.magnets.registry import RegistrableMeta
 
 # Global registries
 WINDINGPACK_REGISTRY = {}
+
+
+@dataclass
+class WindingPackParams(ParameterFrame):
+    """
+    Parameters needed for the Winding Pack
+    """
+
+    nx: Parameter[int]
+    """Number of conductors along the x-axis."""
+    ny: Parameter[int]
+    """Number of conductors along the y-axis."""
 
 
 class WindingPack(metaclass=RegistrableMeta):
@@ -35,9 +50,10 @@ class WindingPack(metaclass=RegistrableMeta):
 
     _registry_: ClassVar[dict] = WINDINGPACK_REGISTRY
     _name_in_registry_: ClassVar[str] = "WindingPack"
+    param_cls: type[WindingPackParams] = WindingPackParams
 
     def __init__(
-        self, conductor: Conductor, nx: int, ny: int, name: str = "WindingPack"
+        self, conductor: Conductor, params: ParameterFrameLike, name: str = "WindingPack"
     ):
         """
         Initialize a WindingPack instance.
@@ -54,19 +70,18 @@ class WindingPack(metaclass=RegistrableMeta):
             Name of the winding pack instance.
         """
         self.conductor = conductor
-        self.nx = int(nx)
-        self.ny = int(ny)
+        self.params = params
         self.name = name
 
     @property
     def dx(self) -> float:
         """Return the total width of the winding pack [m]."""
-        return self.conductor.dx * self.nx
+        return self.conductor.dx * self.params.nx.value
 
     @property
     def dy(self) -> float:
         """Return the total height of the winding pack [m]."""
-        return self.conductor.dy * self.ny
+        return self.conductor.dy * self.params.ny.value
 
     @property
     def area(self) -> float:
@@ -76,7 +91,7 @@ class WindingPack(metaclass=RegistrableMeta):
     @property
     def n_conductors(self) -> int:
         """Return the total number of conductors."""
-        return self.nx * self.ny
+        return self.params.nx.value * self.params.ny.value
 
     @property
     def jacket_area(self) -> float:
@@ -98,7 +113,7 @@ class WindingPack(metaclass=RegistrableMeta):
         float
             Stiffness along the x-axis [N/m].
         """
-        return self.conductor.Kx(op_cond) * self.ny / self.nx
+        return self.conductor.Kx(op_cond) * self.params.ny.value / self.params.nx.value
 
     def Ky(self, op_cond: OperationalConditions) -> float:  # noqa: N802
         """
@@ -115,7 +130,7 @@ class WindingPack(metaclass=RegistrableMeta):
         float
             Stiffness along the y-axis [N/m].
         """
-        return self.conductor.Ky(op_cond) * self.nx / self.ny
+        return self.conductor.Ky(op_cond) * self.params.nx.value / self.params.ny.value
 
     def plot(
         self,
@@ -165,8 +180,8 @@ class WindingPack(metaclass=RegistrableMeta):
         ax.plot(points_ext[:, 0], points_ext[:, 1], "k")
 
         if not homogenized:
-            for i in range(self.nx):
-                for j in range(self.ny):
+            for i in range(self.params.nx.value):
+                for j in range(self.params.ny.value):
                     xc_c = xc - self.dx / 2 + (i + 0.5) * self.conductor.dx
                     yc_c = yc - self.dy / 2 + (j + 0.5) * self.conductor.dy
                     self.conductor.plot(xc=xc_c, yc=yc_c, ax=ax)
@@ -190,8 +205,8 @@ class WindingPack(metaclass=RegistrableMeta):
             ),
             "name": self.name,
             "conductor": self.conductor.to_dict(),
-            "nx": self.nx,
-            "ny": self.ny,
+            "nx": self.params.nx.value,
+            "ny": self.params.ny.value,
         }
 
     @classmethod
