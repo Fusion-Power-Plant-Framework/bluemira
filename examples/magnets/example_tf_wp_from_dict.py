50f6fd21041a05542772a3734dd1471865fadf89
matti <matti.coleman@gmail.com>
2025-08-21 14:50:45 +0200

    clean up

--- examples/magnets/example_tf_wp_from_dict.py
+++ examples/magnets/example_tf_wp_from_dict.py
@@ -9,22 +9,7 @@ from eurofusion_materials.library.magnet_branch_mats import (
 )
 from matproplib import OperationalConditions
 
-from bluemira.magnets.cable import DummyRectangularCableLTS
-from bluemira.magnets.strand import SuperconductingStrand
-
 op_cond = OperationalConditions(temperature=5.7, magnetic_field=10.0, strain=0.0055)
-
-print("Nb3Sn resistivity:", NB3SN_MAG.electrical_resistivity(op_cond))
-print("Copper 100 resistivity:", COPPER_100.electrical_resistivity(op_cond))
-print("Copper 300 resistivity:", COPPER_300.electrical_resistivity(op_cond))
-print("SS316 LN resistivity:", SS316_LN_MAG.electrical_resistivity(op_cond))
-print(
-    "Dummy insulator resistivity:", DUMMY_INSULATOR_MAG.electrical_resistivity(op_cond)
-)
-print("Nb3Sn specific heat:", NB3SN_MAG.specific_heat_capacity(op_cond))
-print("Copper 100 specific heat:", COPPER_100.specific_heat_capacity(op_cond))
-print("Copper 300 specific heat:", COPPER_300.specific_heat_capacity(op_cond))
-
 
 from bluemira.base.constants import MU_0, MU_0_2PI, MU_0_4PI
 from bluemira.magnets.case_tf import create_case_tf_from_dict
@@ -125,24 +110,6 @@ case_tf_dict = {
     ],
 }
 
-strand_cls = SuperconductingStrand
-strand = strand_cls.from_dict(case_tf_dict["WPs"][0]["conductor"]["cable"]["sc_strand"])
-
-print(f"Strand erho: {strand.erho(temperature=5.7, B=10.0)}")
-print(f"Strand Cp: {strand.Cp(temperature=5.7, B=10.0)}")
-print(f"Strand rho: {strand.rho(temperature=5.7, B=10.0)}")
-print(f"Strand E: {strand.E(temperature=5.7, B=10.0)}")
-
-cable_cls = DummyRectangularCableLTS
-
-cable = cable_cls.from_dict(case_tf_dict["WPs"][0]["conductor"]["cable"])
-
-print(f"Cable erho: {cable.erho(temperature=5.7, B=10.0)}")
-print(f"Cable Cp: {cable.Cp(temperature=5.7, B=10.0)}")
-print(f"Cable rho: {cable.rho(temperature=5.7, B=10.0)}")
-print(f"Cable E: {cable.E(temperature=5.7, B=10.0)}")
-
-
 case_tf = create_case_tf_from_dict(case_tf_dict)
 
 case_tf.plot(show=True, homogenized=False)
@@ -201,8 +168,6 @@ T_for_hts = T_op
 I_fun = delayed_exp_func(Iop, Tau_discharge, t_delay)
 B_fun = delayed_exp_func(B_TF_i, Tau_discharge, t_delay)
 
-print("cable")
-print(case_tf.WPs[0].conductor.cable)
 
 import time
 
