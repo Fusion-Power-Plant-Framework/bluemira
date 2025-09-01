92404058cc252c2b9ad8741637d0a345091e218c
ocean <OceanWongUK@gmail.com>
2025-09-01 17:59:00 +0100

    Added test for neutron source created from PlasmaParameters.

--- tests/codes/openmc/test_source_params.py
+++ tests/codes/openmc/test_source_params.py
@@ -6,7 +6,12 @@
 import json
 from pathlib import Path
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
+import pytest
+from matplotlib.cm import coolwarm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from bluemira.base.file import get_bluemira_path
 from bluemira.base.parameter_frame import make_parameter_frame
@@ -14,6 +19,7 @@ from bluemira.codes.openmc.params import (
     OpenMCNeutronicsSolverParams,
     PlasmaSourceParameters,
 )
+from bluemira.codes.openmc.sources import make_tokamak_source
 
 PARAMS_PATH = Path(get_bluemira_path("codes/openmc", subfolder="tests"), "params.json")
 
@@ -95,3 +101,104 @@ def test_separatrix_plasma_electron_temperature():
 
 def test_shafranov_shift_of_plasma():
     assert pms_params.shaf_shift == 50.0  # [cm]
+
+
+def test_source_plot():
+    tokamak_source = make_tokamak_source(pms_params)
+    dd = tokamak_source[:10000]
+    tt = tokamak_source[10000:20000]
+    dt = tokamak_source[20000:]
+
+    rz, energy, intensity = [], [], []
+    for src in dd:
+        # position distribution: discrete distribution
+        rz.append([src.space.r.x, src.space.z.x])
+        intensity.append(src.strength)
+    rz = np.array(rz).reshape([len(dd), 2])
+    intensity = np.array(intensity)
+    fig, ax = plot_one_reaction(*rz.T, intensity)
+    ax.set_title("Purported neutron rates from distribution DD reaction")
+    plt.show()
+
+    rz, energy, intensity = [], [], []
+    for src in tt:
+        # position distribution: discrete distribution
+        rz.append([src.space.r.x, src.space.z.x])
+        intensity.append(src.strength)
+    rz = np.array(rz).reshape([len(tt), 2])
+    intensity = np.array(intensity)
+    fig, ax = plot_one_reaction(*rz.T, intensity)
+    ax.set_title("Purported neutron rates from distribution TT reaction")
+    plt.show()
+
+    rz, energy, intensity = [], [], []
+    for src in dt:
+        # position distribution: discrete distribution
+        rz.append([src.space.r.x, src.space.z.x])
+        intensity.append(src.strength)
+    rz = np.array(rz).reshape([len(dt), 2])
+    intensity = np.array(intensity)
+    fig, ax = plot_one_reaction(*rz.T, intensity)
+    ax.set_title("Purported neutron rates from distribution DT reaction")
+    plt.show()
+
+
+@pytest.mark.xfail(reason="The openmc_source is poorly")
+def test_unique_energy_distributions():
+    """
+    Issue: the energy distribution of the DD, TT, and DT are all IDENTICAL.
+    This is an issue on the side of openmc_plasma_source
+    """
+    tokamak_source = make_tokamak_source(pms_params)
+    dd_sources = tokamak_source[:10000]
+    tt_sources = tokamak_source[10000:20000]
+    dt_sources = tokamak_source[20000:30000]
+    # These parameters we're expecting them to be different in-general.
+    weights = []
+    peak_parameters = []
+    table_energy, table_prob = [], []
+
+    for dd, tt, dt in zip(dd_sources, tt_sources, dt_sources, strict=False):
+        dd_dist = dd.energy
+        tt_dist = tt.energy
+        dt_dist = dt.energy
+
+        weights.append([dd_dist.probability, tt_dist.probability, dt_dist.probability])
+
+        dd_pk1, dd_pk2 = dd_dist.distribution[:2]
+        tt_pk1, tt_pk2 = tt_dist.distribution[:2]
+        dt_pk1, dt_pk2 = dt_dist.distribution[:2]
+        peak_parameters.append([
+            [dd_pk1.mean_value, dd_pk1.std_dev, dd_pk2.mean_value, dd_pk2.std_dev],
+            [tt_pk1.mean_value, tt_pk1.std_dev, tt_pk2.mean_value, tt_pk2.std_dev],
+            [dt_pk1.mean_value, dt_pk1.std_dev, dt_pk2.mean_value, dt_pk2.std_dev],
+        ])
+
+        dd_table = dd_dist.distribution[2]
+        tt_table = tt_dist.distribution[2]
+        dt_table = dt_dist.distribution[2]
+        table_energy.append([dd_table.x, tt_table.x, dt_table.x])
+        table_prob.append([dd_table.p, tt_table.p, dt_table.p])
+    weights = np.array(weights)
+    peak_parameters = np.array(peak_parameters)
+    table_energy, table_prob = np.array(table_energy), np.array(table_prob)
+    # compare how different the energy distributions are from each other.
+    same_weights = np.diff(weights, axis=1) == 0.0
+    same_peaks = np.diff(peak_parameters, axis=1) == 0.0
+    same_table_energy = np.diff(table_energy, axis=1) == 0.0
+    same_table_prob = np.diff(table_prob, axis=1) == 0.0
+    assert (not same_weights.all()) or (not same_peaks.all())
+    assert (not same_table_energy.all()) or (not same_table_prob.all())
+
+
+def plot_one_reaction(r, z, intensity):
+    fig, ax = plt.subplots()
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    # scaled_intensity = 0.2 * intensity/intensity.max() + 0.8
+    scatter = ax.scatter(r, z, marker=".", c=intensity, cmap=coolwarm)
+    mpl.colorbar.ColorbarBase(ax=cax, cmap=coolwarm)
+    ax.figure.colorbar(scatter, cax=cax, orientation="vertical")
+    ax.set_aspect(1.0)
+    ax.set_xlabel("r (cm)"), ax.set_ylabel("z (cm)")
+    return fig, ax
