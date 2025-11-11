721092c41c83af20734bc790a3a5762a1755e667
matti <matti.coleman@gmail.com>
2025-11-11 13:56:42 +0100

    FW tweaks

--- eudemo/eudemo/reactor.py
+++ eudemo/eudemo/reactor.py
@@ -711,6 +711,7 @@ if __name__ == "__main__":
         zero_d_params = ZeroDNeutronicsModel(reactor_config.global_params).run()
         reactor_config.global_params.update_from_frame(zero_d_params)
         if reactor_config.config_for("Neutronics", "CSG").get("enabled", False):
+            neutronics_start = time.time()
             neutronics_csg = run_csg_neutronics(
                 reactor_config.params_for("Neutronics", "CSG").global_params,
                 reactor_config.config_for("Neutronics", "CSG"),
@@ -720,9 +721,6 @@ if __name__ == "__main__":
                 eq=reference_eq,
                 op_cond=OperationalConditions(temperature=298, pressure=101325),
             )
-            print(f"TBR = {reactor.neutronics.results.tbr}")
-            print(f"e_mult = {reactor.neutronics.results.e_mult}")
-            raise ValueError
             neutronics_end = time.time()
             run_time_track["CSG neutronics"] = neutronics_end - neutronics_start
 
