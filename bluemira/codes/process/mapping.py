22b7fdb290dccb8cc11feb852e626c0a1d77e7c7
matti <matti.coleman@gmail.com>
2025-12-17 18:16:21 +0100

    update PROCESS mappings

--- bluemira/codes/process/mapping.py
+++ bluemira/codes/process/mapping.py
@@ -38,8 +38,9 @@ IN_mappings = {
     "T_profile_alpha": ("alphat", "dimensionless"),
     "T_profile_beta": ("tbeta", "dimensionless"),
     "n_profile_alpha": ("alphan", "dimensionless"),
-    # Note: PROCESS has both rhopedt and rhopedn - they are usually the same...
-    "profile_rho_ped": ("rhopedt", "dimensionless"),
+    # Note: PROCESS has both radius_plasma_pedestal_temp_norm and
+    # radius_plasma_pedestal_density_norm - they are usually the same...
+    "profile_rho_ped": ("radius_plasma_pedestal_temp_norm", "dimensionless"),
 }
 
 OUT_mappings = {
@@ -64,8 +65,8 @@ OUT_mappings = {
     "P_sync": ("pden_plasma_sync_mw*vol", "MW"),
     "P_brehms": ("pbrempv*plasma_volume", "MW"),
     "f_bs": ("f_c_plasma_bootstrap", "dimensionless"),
-    "beta_p": ("beta_poloidal", "dimensionless"),
-    "beta": ("beta", "dimensionless"),
+    "beta_p": ("beta_poloidal_vol_avg", "dimensionless"),
+    "beta_total_vol_avg": ("beta_total_vol_avg", "dimensionless"),
     "j_cs_critical": ("j_cs_critical_pulse_start", "A/m^2"),
     "tau_e": ("t_energy_confinement", "s"),
     "v_burn": ("v_plasma_loop_burn", "V"),
@@ -103,13 +104,13 @@ OUT_mappings = {
     "n_e": ("nd_plasma_electrons_vol_avg", "1/m^3"),
     "n_e_ped": ("nd_plasma_pedestal_electron", "1/m^3"),
     "n_e_sep": ("nd_plasma_separatrix_electron", "1/m^3"),
-    "n_i_fuel": ("nd_fuel_ions", "1/m^3"),
+    "n_i_fuel": ("nd_plasma_fuel_ions_vol_avg", "1/m^3"),
     "T_ie_ratio": ("f_temp_plasma_ion_electron", ""),
 }
 
 IO_mappings = {
     "A": ("aspect", "dimensionless"),
-    "tau_flattop": (("t_burn_min", "t_burn"), "s"),
+    "tau_flattop": (("t_burn_min", "t_plant_pulse_burn"), "s"),
     "P_el_net": (("p_plant_electric_net_required_mw", "p_plant_electric_net_mw"), "MW"),
     "tk_bb_ib": ("dr_blkt_inboard", "m"),
     "tk_bb_ob": ("dr_blkt_outboard", "m"),
