d7ed472c4854f29431059f2439da65012fc1a9e6
Georgina Graham <g.a.graham09@gmail.com>
2026-01-13 11:28:43 +0000

    corrections from comments

--- bluemira/equilibria/find.py
+++ bluemira/equilibria/find.py
@@ -25,6 +25,7 @@ from bluemira.equilibria.error import EquilibriaError
 from bluemira.geometry.coordinates import (
     Coordinates,
     get_area_2d,
+    get_intersect,
     in_polygon,
 )
 from bluemira.utilities.tools import floatify
@@ -826,8 +827,8 @@ def find_LCFS_separatrix(
         middle = low + 0.5 * delta
         flux_surface = get_flux_loop(middle)
 
-        intersection_count, _ = x_point_check(flux_surface, primary_op, primary_xp)
-        if flux_surface.closed and intersection_count == 0:
+        intersection_count = x_point_check(flux_surface, primary_op, primary_xp)
+        if flux_surface.closed and intersection_count <= 1:
             # Middle flux surface is still closed, shift search bounds
             low = middle
         else:
@@ -873,7 +874,12 @@ def find_LCFS_separatrix(
     return lcfs, separatrix
 
 
-def x_point_check(flux_surface: Coordinates, op: Opoint, xp: Xpoint):
+def x_point_check(
+    flux_surface: Coordinates,
+    op: Opoint,
+    xp: Xpoint,
+    tangent_length: float | None = None,
+):
     """
     Check if there are intersections of a flux surface with the
     line tangent to the o-point-x-point vector at the x-point.
@@ -887,16 +893,23 @@ def x_point_check(flux_surface: Coordinates, op: Opoint, xp: Xpoint):
         Primary o-point for an equilibrium
     xp:
         X-point of interest, usually primary
+    tangent_length:
+        Set tangent length, otherwise small default value used
 
     Returns
     -------
     arg_inters:
         Intersection indices
     """
-    length = np.hypot(np.max(flux_surface.x), np.max(np.abs(flux_surface.z)))
-    tanget_line = two_point_angled_line(op, xp, length)  # default theta is tangent
-    _, _, intersection_count = join_intersect(flux_surface, tanget_line, get_arg=True)
-    return (intersection_count, tanget_line)
+    if tangent_length is None:
+        tangent_length = (
+            np.hypot(np.max(flux_surface.x), np.min(flux_surface.x)) * 1e-2
+        )  # small and accounts for plasma size
+    tanget_line = two_point_angled_line(
+        op, xp, tangent_length
+    )  # default theta is tangent
+    inter = np.shape(get_intersect(flux_surface.xz, tanget_line.xz))
+    return inter[1]
 
 
 def two_point_angled_line(
