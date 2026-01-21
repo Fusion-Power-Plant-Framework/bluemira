05c04c123a82a55f363c38c1bc5b9b2e442efb92
Josephine Peters <josephine.peters@ukaea.uk>
2026-01-21 10:55:56 +0000

    Changing nb.np.extensions.cross2d to cross_2d

--- eudemo/eudemo_tests/maintenance/test_upper_port.py
+++ eudemo/eudemo_tests/maintenance/test_upper_port.py
@@ -10,7 +10,6 @@ Tests for EU-DEMO Maintenance
 
 import numpy as np
 import pytest
-from numba.np.extensions import cross2d
 
 from bluemira.base.error import BuilderError
 from bluemira.base.parameter_frame import Parameter
@@ -24,6 +23,7 @@ from bluemira.geometry.tools import (
     make_polygon,
 )
 from bluemira.geometry.wire import BluemiraWire
+from bluemira.utilities.tools import cross_2d
 from eudemo.maintenance.duct_connection import (
     VVUpperPortDuctBuilder,
     VVUpperPortDuctBuilderParams,
@@ -147,7 +147,7 @@ class TestDuctConnection:
             # Are the edges of the internal and external wires parallel
             v1 = (e1.end_point().xy - e1.start_point().xy).flatten()
             v2 = (e2.end_point().xy - e2.start_point().xy).flatten()
-            assert np.isclose(cross2d(v1, v2), 0)
+            assert np.isclose(cross_2d(v1, v2), 0)
 
             # Are they the right distance apart
             if no in {0, 3}:
