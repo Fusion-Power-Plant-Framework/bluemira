fix to mutual inductance matrix calculation

bluemira/equilibria/coils/_tools.py
@@ -74,18 +74,15 @@ def make_mutual_inductance_matrix(
     n_coils = len(coils)
 
     M = np.zeros((n_coils, n_coils))  # noqa: N806
-
     itri, jtri = np.triu_indices(n_coils, k=1)
-
-    for i in itri:
-        for j in jtri:
-            coil1, coil2 = coils[i], coils[j]
-            for xi1, zi1 in zip(coil1._quad_x, coil1._quad_z, strict=True):
-                for xi2, zi2 in zip(coil2._quad_x, coil2._quad_z, strict=True):
-                    M[i, j] += greens_psi(xi1, zi1, xi2, zi2)
-            M[i, j] *= (
-                coil1.n_turns * coil2.n_turns / (len(coil1._quad_x) * len(coil2._quad_x))
-            )
+    for i, j in zip(itri, jtri, strict=True):
+        coil1, coil2 = coils[i], coils[j]
+        for xi1, zi1 in zip(coil1._quad_x, coil1._quad_z, strict=True):
+            for xi2, zi2 in zip(coil2._quad_x, coil2._quad_z, strict=True):
+                M[i, j] += greens_psi(xi1, zi1, xi2, zi2)
+        M[i, j] *= (
+            coil1.n_turns * coil2.n_turns / (len(coil1._quad_x) * len(coil2._quad_x))
+        )
 
     M[jtri, itri] = M[itri, jtri]
 