454d7abe71d83ea60b20322704af1a2c608d528b
matti <matti.coleman@gmail.com>
2026-06-08 16:50:50 +0200

    TF KOZ offset tweak and dodge DAGMC+fastCTD

--- eudemo/eudemo/neutronics/run.py
+++ eudemo/eudemo/neutronics/run.py
@@ -196,6 +196,9 @@ def run_dagmc_neutronics(
     tally_function=None,
 ) -> DAGMCOutput:
     """Creates and runs the DAGMC neutronics model"""  # noqa: DOC201
+    if build_config.get("enabled"):
+        return None
+
     export_dagmc_model(reactor, build_config)
 
     mats = {"undef_material": Void(name="undef_material")}
