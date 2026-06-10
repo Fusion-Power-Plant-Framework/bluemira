0b93aed6cccc7d7b40018e081620f1ceb4d04f2c
matti <matti.coleman@gmail.com>
2026-06-10 11:28:18 +0200

    switch back to LCFS wire

--- eudemo/eudemo/reactor.py
+++ eudemo/eudemo/reactor.py
@@ -51,7 +51,6 @@ from bluemira.geometry.face import BluemiraFace
 from bluemira.geometry.tools import (
     distance_to,
     interpolate_bspline,
-    make_polygon,
     offset_wire,
     save_cad,
 )
@@ -788,7 +787,7 @@ if __name__ == "__main__":
         reactor.tf_coils, peak_opt_ripple = build_tf_coils(
             reactor_config.params_for("TF coils"),
             reactor_config.config_for("TF coils"),
-            make_polygon(lcfs_coords),
+            reactor.plasma.lcfs(),
             vv_thermal_shield.xz_boundary,
         )
         reactor_config.global_params.TF_peak_ripple_opt.set_value(
