fc732ea188f5bf41e1cc4c8b0b2822e25c07c8b7
Georgina Graham <g.a.graham09@gmail.com>
2026-06-17 10:42:54 +0100

    fix wrong message index

--- tests/geometry/test_coordinates.py
+++ tests/geometry/test_coordinates.py
@@ -450,13 +450,13 @@ class TestShortCoordinates:
         idx = c._get_loc_idx(x=0.0, y=0.0, z=0.0)
         assert idx[0] == 0
         _ = c._get_loc_idx(x=0.5, y=0.5, z=0.5)
-        assert "There is no point at this location." in caplog.messages[1]
+        assert "There is no point at this location." in caplog.messages[0]
         new_c = Coordinates({"x": [0, 1, 0], "y": [0, 1, 0], "z": [0, 1, 0]})
         idx = new_c._get_loc_idx(x=0.0, y=0.0, z=0.0)
         assert idx[0] == 0
         assert (
             "There is more than one point at this location, returning first value."
-            in caplog.messages[2]
+            in caplog.messages[1]
         )
 
     @pytest.mark.parametrize("c", [deepcopy(line)])
