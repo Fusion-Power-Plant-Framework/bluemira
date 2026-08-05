bf7705707eeef288d359aebdfc3cfbd96e03eade
Josephine Peters <josephine.peters@ukaea.uk>
2026-08-05 11:01:18 +0100

    correction

--- examples/optimisation/geometry_optimisation_marimo.py
+++ examples/optimisation/geometry_optimisation_marimo.py
@@ -5,13 +5,14 @@
 #     "marimo>=0.23.16",
 #     "pyside6==6.11.1",
 # ]
+#
 # [[tool.uv.index]]
 # name = "bluemira-index"
 # url = "https://github.com/Fusion-Power-Plant-Framework/bluemira"
 # explicit = true
 #
 # [tool.uv.sources]
-# bluemira = { path = "../../", editable = true, index = bluemira-index }
+# bluemira = { path = "../../", editable = true, index = "bluemira-index" }
 # ///
 import marimo
 
