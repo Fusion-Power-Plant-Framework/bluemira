353d36675df69dd5520dec6447e8d4e269905d21
athoy.nilima@ukaea.uk <athoy.nilima@ukaea.uk>
2026-08-19 09:34:10 +0100

    test BSplineSurface as well

--- tests/geometry/test_despliner.py
+++ tests/geometry/test_despliner.py
@@ -3,6 +3,7 @@
 # SPDX-FileCopyrightText: 2021-present J. Morris, D. Short
 #
 # SPDX-License-Identifier: LGPL-2.1-or-later
+import numpy as np
 import pytest
 
 from bluemira.geometry.despliner import has_splines
@@ -11,6 +12,7 @@ from bluemira.geometry.tools import (
     extrude_shape,
     interpolate_bspline,
     make_bezier,
+    make_bsplinesurface,
     make_polygon,
 )
 
@@ -26,6 +28,27 @@ def make_bspline_solid(points, vec, closed):
     return extrude_shape(BluemiraFace(interpolate_bspline(points, closed)), vec)
 
 
+def make_bspline_surface():
+    poles = np.array([
+        [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0]],
+        [[0.0, 1.0, 0.0], [1.0, 1.0, 0.0]],
+    ])
+
+    surface = make_bsplinesurface(
+        poles,
+        mults_u=[2, 2],
+        mults_v=[2, 2],
+        knot_vector_u=[0.0, 1.0],
+        knot_vector_v=[0.0, 1.0],
+        degree_u=1,
+        degree_v=1,
+        weights=np.ones((2, 2)),
+        periodic=True,
+        check_rational=True,
+    )
+    return extrude_shape(surface, [0, 1, 0])
+
+
 def make_bezier_solid(points, vec, closed):
     return extrude_shape(BluemiraFace(make_bezier(points, closed)), vec)
 
@@ -34,6 +57,7 @@ def make_bezier_solid(points, vec, closed):
     ("shape_func", "args", "expected"),
     [
         (make_unit_box, (), False),
+        (make_bspline_surface, (), True),
         (
             make_bspline_solid,
             (
