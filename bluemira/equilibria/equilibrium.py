a691a792eb5d82e14861560234be9d4adb22b4d1
james <81617086+je-cook@users.noreply.github.com>
2026-09-14 11:41:06 +0100

    🏷️ Profiles

--- bluemira/equilibria/equilibrium.py
+++ bluemira/equilibria/equilibrium.py
@@ -65,6 +65,7 @@ from bluemira.equilibria.plotting import (
 from bluemira.equilibria.profiles import (
     BetaLiIpProfile,
     CustomProfile,
+    IpProfile,
     OPointCalcOptions,
     Profile,
 )
@@ -1150,7 +1151,7 @@ class Equilibrium(CoilSetMHDState):  # noqa: PLR0904
         self,
         coilset: CoilSet,
         grid: Grid,
-        profiles: Profile,
+        profiles: IpProfile,
         *,
         force_symmetry: bool = False,
         vcontrol: str | None = None,
