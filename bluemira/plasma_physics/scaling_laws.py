🐛 Fix scaling law test

bluemira/plasma_physics/scaling_laws.py
@@ -125,7 +125,12 @@ class PowerLawScaling:
         min_terms = np.zeros(len(self))
         max_terms = np.zeros(len(self))
         for i, (arg, exp, err) in enumerate(
-            zip(args, self.exponents, self.errors or (), strict=False)
+            zip(
+                args,
+                self.exponents,
+                () if self.errors is None else self.errors,
+                strict=False,
+            )
         ):
             term_values = [arg ** (exp - err), arg ** (exp + err)]
             min_terms[i] = min(term_values)